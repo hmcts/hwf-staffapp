f735dbf8a9c39108a2ac648f179e18669addee2f
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-02-12 10:37:39 +0000

    Fix all pending tests

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe "home/index.html.slim", type: :view do
-  pending it 'should contain our header' do
+  it 'should contain our header' do
     render
-    expect(rendered).to include('Your home page here')
+    expect(rendered).to include('Welcome to the Fee remission application')
   end
 end
