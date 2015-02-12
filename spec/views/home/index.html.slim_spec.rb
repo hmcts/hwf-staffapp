2072d16d40fd5dedc5a035e4a68ff30efe12f095
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-12 15:00:50 +0000

    update tests for home page view

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe "home/index.html.slim", type: :view do
-  it 'should contain our header' do
+  it 'should contain our dashboard header' do
     render
-    expect(rendered).to include('Welcome to the Fee remission application')
+    expect(rendered).to include('Dashboard')
   end
 end
