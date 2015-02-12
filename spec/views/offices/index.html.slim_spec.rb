f69ce67a7934f51e8000e57e1a32a5e8791b5e34
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-12 15:01:12 +0000

    Improve specs around office creation

--- spec/views/offices/index.html.slim_spec.rb
+++ spec/views/offices/index.html.slim_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "offices/index", type: :view do
   include Devise::TestHelpers
 
+  let(:user)          { FactoryGirl.create :user }
   let(:admin_user)    { FactoryGirl.create :admin_user }
+
   before(:each) do
     assign(:offices, [
       Office.create!(
@@ -15,9 +17,26 @@ RSpec.describe "offices/index", type: :view do
     ])
   end
 
-  it "renders a list of offices" do
-    sign_in admin_user
-    render
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+  context 'logged in user' do
+
+    before(:each) { sign_in user }
+
+    it 'does not see New Office link' do
+      render
+      expect(rendered).to_not include('New Office')
+    end
   end
+
+  context 'logged in as admin' do
+
+    before(:each) { sign_in admin_user }
+
+    it 'sees New office list' do
+      render
+      expect(rendered).to include('New Office')
+    end
+
+  end
+
 end
+
