7703ffa2b14a4fe5138c3281b99a25cafac0eb38
Stephen Richards <stephen.richards@digital.justice.gov.uk>
2015-02-12 10:35:18 +0000

    Office

--- spec/controllers/offices_controller_spec.rb
+++ spec/controllers/offices_controller_spec.rb
@@ -27,11 +27,11 @@ RSpec.describe OfficesController, type: :controller do
   # Office. As you add validations to Office, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { name: 'Test Office' }
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    { name: nil }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -43,131 +43,200 @@ RSpec.describe OfficesController, type: :controller do
   let(:user)          { FactoryGirl.create :user }
   let(:admin_user)    { FactoryGirl.create :admin_user }
 
-  describe "GET #index" do
-    it "assigns all offices as @offices" do
-      office = Office.create! valid_attributes
-      get :index, {}, valid_session
-      expect(assigns(:offices)).to eq([office])
-    end
-  end
+  context 'standard user' do
 
-  describe "GET #show" do
-    it "assigns the requested office as @office" do
-      office = Office.create! valid_attributes
-      get :show, {:id => office.to_param}, valid_session
-      expect(assigns(:office)).to eq(office)
-    end
-  end
+    before(:each) { sign_in user }
 
-  describe "GET #new" do
-    it "assigns a new office as @office" do
-      sign_in admin_user
-      get :new, {}, valid_session
-      expect(assigns(:office)).to be_a_new(Office)
-    end
-
-    it 'raises if user not admin' do
-      sign_in user
-      expect {
-        get :new, {}, valid_session
-      }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
-    end
-  end
-
-  describe "GET #edit" do
-    it "assigns the requested office as @office" do
-      office = Office.create! valid_attributes
-      get :edit, {:id => office.to_param}, valid_session
-      expect(assigns(:office)).to eq(office)
-    end
-  end
-
-  describe "POST #create" do
-    context "with valid params" do
-      it "creates a new Office" do
-        expect {
-          post :create, {:office => valid_attributes}, valid_session
-        }.to change(Office, :count).by(1)
-      end
-
-      it "assigns a newly created office as @office" do
-        post :create, {:office => valid_attributes}, valid_session
-        expect(assigns(:office)).to be_a(Office)
-        expect(assigns(:office)).to be_persisted
-      end
-
-      it "redirects to the created office" do
-        post :create, {:office => valid_attributes}, valid_session
-        expect(response).to redirect_to(Office.last)
-      end
-    end
-
-    context "with invalid params" do
-      it "assigns a newly created but unsaved office as @office" do
-        post :create, {:office => invalid_attributes}, valid_session
-        expect(assigns(:office)).to be_a_new(Office)
-      end
-
-      it "re-renders the 'new' template" do
-        post :create, {:office => invalid_attributes}, valid_session
-        expect(response).to render_template("new")
-      end
-    end
-  end
-
-  describe "PUT #update" do
-    context "with valid params" do
-      let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
-      }
-
-      it "updates the requested office" do
+    describe "GET #index" do
+      it "assigns all offices as @offices" do
         office = Office.create! valid_attributes
-        put :update, {:id => office.to_param, :office => new_attributes}, valid_session
-        office.reload
-        skip("Add assertions for updated state")
+        get :index, {}, valid_session
+        expect(assigns(:offices)).to eq([office])
       end
+    end
 
+    describe "GET #show" do
       it "assigns the requested office as @office" do
         office = Office.create! valid_attributes
-        put :update, {:id => office.to_param, :office => valid_attributes}, valid_session
+        get :show, {:id => office.to_param}, valid_session
         expect(assigns(:office)).to eq(office)
-      end
-
-      it "redirects to the office" do
-        office = Office.create! valid_attributes
-        put :update, {:id => office.to_param, :office => valid_attributes}, valid_session
-        expect(response).to redirect_to(office)
       end
     end
 
-    context "with invalid params" do
-      it "assigns the office as @office" do
-        office = Office.create! valid_attributes
-        put :update, {:id => office.to_param, :office => invalid_attributes}, valid_session
-        expect(assigns(:office)).to eq(office)
+    describe "GET #new" do
+      it "assigns a new office as @office" do
+        expect {
+          get :new, {}, valid_session
+        }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
       end
+    end
 
-      it "re-renders the 'edit' template" do
+    describe "GET #edit" do
+      it "assigns the requested office as @office" do
         office = Office.create! valid_attributes
-        put :update, {:id => office.to_param, :office => invalid_attributes}, valid_session
-        expect(response).to render_template("edit")
+        expect {
+          get :edit, {:id => office.to_param}, valid_session
+        }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Office" do
+          expect {
+            post :create, {:office => valid_attributes}, valid_session
+          }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
+        end
+      end
+    end
+
+    describe "PUT #update" do
+      context "with valid params" do
+        let(:new_attributes) {
+          { name: 'New Office' }
+        }
+
+        it "is unauthorised" do
+          office = Office.create! valid_attributes
+          expect {
+            put :update, {:id => office.to_param, :office => new_attributes}, valid_session
+          }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
+        end
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "is unauthorised" do
+        office = Office.create! valid_attributes
+        expect {
+          delete :destroy, {:id => office.to_param}, valid_session
+        }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
       end
     end
   end
 
-  describe "DELETE #destroy" do
-    it "destroys the requested office" do
-      office = Office.create! valid_attributes
-      expect {
-        delete :destroy, {:id => office.to_param}, valid_session
-      }.to change(Office, :count).by(-1)
+
+  context 'admin_user' do
+
+    before(:each) { sign_in admin_user }
+
+    describe "GET #index" do
+      it "assigns all offices as @offices" do
+        office = Office.create! valid_attributes
+        get :index, {}, valid_session
+        expect(assigns(:offices)).to eq([office])
+      end
     end
 
-    it "redirects to the offices list" do
-      office = Office.create! valid_attributes
-      delete :destroy, {:id => office.to_param}, valid_session
-      expect(response).to redirect_to(offices_url)
+    describe "GET #show" do
+      it "assigns the requested office as @office" do
+        office = Office.create! valid_attributes
+        get :show, {:id => office.to_param}, valid_session
+        expect(assigns(:office)).to eq(office)
+      end
+    end
+
+    describe "GET #new" do
+      it "assigns a new office as @office" do
+        get :new, {}, valid_session
+        expect(assigns(:office)).to be_a_new(Office)
+      end
+    end
+
+    describe "GET #edit" do
+      it "assigns the requested office as @office" do
+        office = Office.create! valid_attributes
+        get :edit, {:id => office.to_param}, valid_session
+        expect(assigns(:office)).to eq(office)
+      end
+    end
+
+    describe "POST #create" do
+      context "with valid params" do
+        it "creates a new Office" do
+          expect {
+            post :create, {:office => valid_attributes}, valid_session
+          }.to change(Office, :count).by(1)
+        end
+
+        it "assigns a newly created office as @office" do
+          post :create, {:office => valid_attributes}, valid_session
+          expect(assigns(:office)).to be_a(Office)
+          expect(assigns(:office)).to be_persisted
+        end
+
+        it "redirects to the created office" do
+          post :create, {:office => valid_attributes}, valid_session
+          expect(response).to redirect_to(Office.last)
+        end
+      end
+
+      context "with invalid params" do
+        it "assigns a newly created but unsaved office as @office" do
+          post :create, {:office => invalid_attributes}, valid_session
+          expect(assigns(:office)).to be_a_new(Office)
+        end
+
+        it "re-renders the 'new' template" do
+          post :create, {:office => invalid_attributes}, valid_session
+          expect(response).to render_template("new")
+        end
+      end
+    end
+
+    describe "PUT #update" do
+      context "with valid params" do
+        let(:new_attributes) {
+          { name: 'New Office' }
+        }
+
+        it "updates the requested office" do
+          office = Office.create! valid_attributes
+          put :update, {:id => office.to_param, :office => new_attributes}, valid_session
+          office.reload
+        end
+
+        it "assigns the requested office as @office" do
+          office = Office.create! valid_attributes
+          put :update, {:id => office.to_param, :office => valid_attributes}, valid_session
+          expect(assigns(:office)).to eq(office)
+        end
+
+        it "redirects to the office" do
+          office = Office.create! valid_attributes
+          put :update, {:id => office.to_param, :office => valid_attributes}, valid_session
+          expect(response).to redirect_to(office)
+        end
+      end
+
+      context "with invalid params" do
+        it "assigns the office as @office" do
+          office = Office.create! valid_attributes
+          put :update, {:id => office.to_param, :office => invalid_attributes}, valid_session
+          expect(assigns(:office)).to eq(office)
+        end
+
+        it "re-renders the 'edit' template" do
+          office = Office.create! valid_attributes
+          put :update, {:id => office.to_param, :office => invalid_attributes}, valid_session
+          expect(response).to render_template("edit")
+        end
+      end
+    end
+
+    describe "DELETE #destroy" do
+      it "destroys the requested office" do
+        office = Office.create! valid_attributes
+        expect {
+          delete :destroy, {:id => office.to_param}, valid_session
+        }.to change(Office, :count).by(-1)
+      end
+
+      it "redirects to the offices list" do
+        office = Office.create! valid_attributes
+        delete :destroy, {:id => office.to_param}, valid_session
+        expect(response).to redirect_to(offices_url)
+      end
     end
   end
 
