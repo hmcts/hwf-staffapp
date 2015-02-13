2490f3327b55263a8111bff20b3a32c4c0ae5d7a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-13 11:38:36 +0000

    Added user controller and specs

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   devise_for :users
+  resources :users
 
   
   # The priority is based upon order of creation: first created -> highest priority.
