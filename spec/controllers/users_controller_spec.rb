79529c96736512db899d20d9a71ef6a82375cb97
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-13 15:38:40 +0000

    correct user specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -17,7 +17,11 @@ RSpec.describe UsersController, type: :controller do
   }
 
   let(:invalid_attributes) {
-    { name: nil }
+    {
+        email: nil,
+        password: 'short',
+        role: 'student'
+    }
   }
 
   # This should return the minimal set of values that should be in the session
@@ -29,22 +33,16 @@ RSpec.describe UsersController, type: :controller do
   let(:admin_user)    { FactoryGirl.create :admin_user }
 
   context 'logged out user' do
-    describe "GET #index" do
-      it "redirects to login page" do
+    describe 'GET #index' do
+      it 'redirects to login page' do
         get :index, {}, valid_session
         expect(response).to redirect_to(user_session_path)
       end
     end
-    describe "GET #show" do
-      it "redirects to login page" do
-        office = User.create! valid_attributes
-        get :show, {:id => office.to_param}, valid_session
-        expect(response).to redirect_to(user_session_path)
-      end
-    end
-    describe "GET #new" do
-      xit "redirects to login page" do
-        get :index, {}, valid_session
+    describe 'GET #show' do
+      it 'redirects to login page' do
+        user = User.create! valid_attributes
+        get :show, {:id => user.to_param}, valid_session
         expect(response).to redirect_to(user_session_path)
       end
     end
@@ -52,31 +50,38 @@ RSpec.describe UsersController, type: :controller do
 
   context 'standard user' do
     before(:each) { sign_in user }
-    describe "GET #index" do
-      it "generates access denied error" do
+    describe 'GET #index' do
+      it 'generates access denied error' do
         expect {
           get :index, {}, valid_session
         }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
       end
     end
-    describe "GET #show" do
-      it "generates access denied error" do
-        office = User.create! valid_attributes
+    describe 'GET #show' do
+      it 'generates access denied error' do
+        user = User.create! valid_attributes
         expect {
-          get :show, {:id => office.to_param}, valid_session
-        }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
-      end
-    end
-    describe "GET #new" do
-      xit "generates access denied error" do
-        expect {
-          get :new, valid_session
+          get :show, {:id => user.to_param}, valid_session
         }.to raise_error CanCan::AccessDenied, 'You are not authorized to access this page.'
       end
     end
   end
 
   context 'admin user' do
-
+    before(:each) { sign_in admin_user }
+    describe 'GET #index' do
+      it 'shows user list' do
+        user = User.create! valid_attributes
+        get :index, {}, valid_session
+        expect(assigns(:users).first).to eq(user)
+      end
+    end
+    describe 'GET #show' do
+      it 'shows user details' do
+        user = User.create! valid_attributes
+        get :show, {:id => user.to_param}, valid_session
+        expect(assigns(:user)).to eq(user)
+      end
+    end
   end
 end
