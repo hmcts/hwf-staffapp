503f8d2479bd1b8587e3add4e906f96fed49e988
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-23 12:57:27 +0000

    removed obsolete commented route from routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   get 'dwp_checks' => 'dwp_checks#new', as: 'new_dwp_checks'
   post 'dwp_checks/lookup'
-  # get 'dwp_checks/:id' => 'dwp_checks#show', as: 'dwp_checks'
   get 'dwp_checks/:unique_number' => 'dwp_checks#show', as: 'dwp_checks'
 
 
