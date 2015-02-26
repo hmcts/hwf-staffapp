17d231bb1d6f7e14702d57fe56ce04e705df0302
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-02-26 09:24:15 +0000

    added feature test for failed check

--- spec/features/perform_benefit_check_spec.rb
+++ spec/features/perform_benefit_check_spec.rb
@@ -8,18 +8,34 @@ RSpec.feature 'Undertake benefit check', type: :feature do
   let(:user)          { FactoryGirl.create :user }
 
   context 'as user' do
-    scenario 'generates a result page' do
 
-      login_as user
-      visit new_dwp_checks_path
+    context 'with valid data' do
+      scenario 'generates a result page' do
 
-      fill_in 'dwp_check_last_name', with: 'bruce'
-      fill_in 'dwp_check_dob', with: '01/01/2001'
-      fill_in 'dwp_check_ni_number', with: 'AB123456A'
-      click_button 'Check'
+        login_as user
+        visit new_dwp_checks_path
 
-      expect(page).to have_css('div.dwp-value', text: 'AB123456A', count: 1)
-      expect(page).to have_css('div.callout > span.number', text: /[Bb]enefits/, count: 1)
+        fill_in 'dwp_check_last_name', with: 'bruce'
+        fill_in 'dwp_check_dob', with: '01/01/2001'
+        fill_in 'dwp_check_ni_number', with: 'AB123456A'
+        click_button 'Check'
+
+        expect(page).to have_css('div.dwp-value', text: 'AB123456A', count: 1)
+        expect(page).to have_css('div.callout > span.number', text: /[Bb]enefits/, count: 1)
+      end
+    end
+    context 'with invalid data' do
+      scenario 'returns to input page' do
+        login_as user
+        visit new_dwp_checks_path
+
+        fill_in 'dwp_check_last_name', with: 'bruce'
+        fill_in 'dwp_check_dob', with: '01/01/2001'
+        fill_in 'dwp_check_ni_number', with: 'AB123'
+        click_button 'Check'
+        expect(page).to have_css('div#error_explanation > h3', text: /1 error prevented/, count: 1)
+        expect(page).to have_selector('input[type="text"][value="AB123"]')
+      end
     end
   end
 end
