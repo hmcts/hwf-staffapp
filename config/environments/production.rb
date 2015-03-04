27de817a7c7cb8f4d620e83537ea66761ae67c7c
Colin Bruce <colin.bruce@digital.justice.gov.uk>
2015-03-04 14:00:45 +0000

    Update production.rb
    Switched to login authentication

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
     ActionMailer::Base.smtp_settings = {
       address:              'smtp.sendgrid.net',
       port:                 '587',
-      authentication:       :plain,
+      authentication:       :login,
       user_name:            ENV['SENDGRID_USERNAME'],
       password:             ENV['SENDGRID_PASSWORD'],
       domain:               ENV['SMTP_DOMAIN'],
