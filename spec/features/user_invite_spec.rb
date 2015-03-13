df3bcdb810c729665d98a0cc901d590689f9d414
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-03-13 16:11:45 +0000

    fixed feature to include name changes

--- spec/features/user_invite_spec.rb
+++ spec/features/user_invite_spec.rb
@@ -12,15 +12,17 @@ RSpec.feature 'Office management', type: :feature do
     scenario 'invites a user' do
 
       new_email = 'test@email.com'
-
+      new_name = 'Test'
       login_as(admin_user, scope: :user)
       visit new_user_invitation_path
 
       fill_in 'user_email', with: new_email
+      fill_in 'user_name', with: new_name
       select('User', from: 'user_role')
+
       click_button 'Send an invitation'
 
-      expect(page).to have_css('a', text: new_email, count: 1)
+      expect(page).to have_css('a', text: new_name, count: 1)
 
     end
   end
