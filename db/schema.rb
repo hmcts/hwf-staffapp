e0216a8980166a8fe345136d6e1e5d7c44ed89f2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-03-26 13:22:14 +0000

    Add DB fields
    To store dwp response
    * dwp_result yes/no//deceased/etc
    * dwp_id - unique downstream result

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313151421) do
+ActiveRecord::Schema.define(version: 20150326131421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
