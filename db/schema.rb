851752dc83f05106b9b39f67606ebde0f612accf
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-16 14:53:56 +0100

    Add type to r2_calc db table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150409132800) do
+ActiveRecord::Schema.define(version: 20150416130439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 20150409132800) do
     t.integer  "created_by_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "type"
   end
 
   add_index "r2_calculators", ["created_by_id"], name: "index_r2_calculators_on_created_by_id", using: :btree
