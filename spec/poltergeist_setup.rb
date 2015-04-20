c30e9137b203a94064305ca76f01035e64bd0919
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-20 13:28:05 +0100

    increased Capybara timeout

--- spec/poltergeist_setup.rb
+++ spec/poltergeist_setup.rb
@@ -3,3 +3,9 @@ require 'capybara/poltergeist'
 Capybara.save_and_open_page_path = 'tmp'
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 5
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app,
+    timeout: 60,
+    phantomjs_options: ['--ignore-ssl-errors=yes', '--ssl-protocol=tlsv1']
+  )
+end
