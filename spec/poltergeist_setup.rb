e713c8ee1308fcf7491bfeae39d948195b4ff3a6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-21 10:47:58 +0100

    Increased Capybara wait time
    * upped to 30 seconds to see if travis works

--- spec/poltergeist_setup.rb
+++ spec/poltergeist_setup.rb
@@ -2,7 +2,7 @@ require 'capybara/poltergeist'
 
 Capybara.save_and_open_page_path = 'tmp'
 Capybara.javascript_driver = :poltergeist
-Capybara.default_wait_time = 5
+Capybara.default_wait_time = 30
 Capybara.register_driver :poltergeist do |app|
   Capybara::Poltergeist::Driver.new(app,
     timeout: 60,
