8369d00372bb592815c981e57739c6f3ce2255c9
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-21 11:29:32 +0100

    Added DWPCheck scope
    Allows for grouping results by type on graphs

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     if user_signed_in?
-      @checks_by_day = DwpCheck.by_office(current_user.office_id).checks_by_day
+      @checks_by_day = DwpCheck.by_office_grouped_by_type(current_user.office_id).checks_by_day
     end
   end
 end
