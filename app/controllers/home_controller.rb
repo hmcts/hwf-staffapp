25015e7a6ff1013bcdb22b12d78c4b872049c64b
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-04-21 10:00:53 +0100

    Examples added for home page
    Checking the behavior when the users are signed in and not signed in, as
    they have different behavior.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    @checks_by_day = DwpCheck.by_office(current_user.office_id).checks_by_day
+    if user_signed_in?
+      @checks_by_day = DwpCheck.by_office(current_user.office_id).checks_by_day
+    end
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,26 @@ require 'spec_helper'
 
 RSpec.describe HomeController, type: :controller do
 
+  include Devise::TestHelpers
+
   describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    let(:user)          { FactoryGirl.create :user }
+
+    context 'when the user is authenticated' do
+      before { sign_in user }
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'when the user is not authenticated' do
+      before { sign_out user }
+
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
