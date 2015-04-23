3776ee875bd35d8e278fdf36b690007ae40a151e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-23 10:49:28 +0100

    Added user seed data for local use

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
 
-Office.create([{ name: 'Bristol' },
-               { name: 'Digital' }])
+Office.create([{ name: 'Digital' },
+               { name: 'Bristol' }])
+
+User.create([{
+              name: 'Admin',
+              email: 'fee-remission@digital.justice.gov.uk',
+              password: '123456789',
+              role: 'admin',
+              office_id: 1
+            },
+            {
+              name: 'User',
+              email: 'bristol.user@hmcts.gsi.gov.uk',
+              password: '987654321',
+              role: 'user',
+              office_id: 2
+            }
+            ])
