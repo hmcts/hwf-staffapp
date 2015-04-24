096696b4182e98e5c77be39d0ea46c97b0f196b4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-04-24 11:09:05 +0100

    Refactored rspec
    * removed duplicated helpers and pointed everything to rails_helper
    * changed selectors to
    * consolidated webmock disable to allow localhost, codeclimate and google for chartkick

--- spec/views/dwp_checks/new.html.slim_spec.rb
+++ spec/views/dwp_checks/new.html.slim_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe 'dwp_checks/new.html.slim', type: :view do
   include Devise::TestHelpers
--- spec/views/layouts/_toolbar.html.slim_spec.rb
+++ spec/views/layouts/_toolbar.html.slim_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe "layouts/_toolbar.html.slim", type: :view do
 
