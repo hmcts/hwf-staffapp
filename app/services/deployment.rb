f439083e7ae0707f553107439f2acf51f99b78dd
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-04-29 12:54:55 +0100

    Changed the module to class
    There is no need for this to be a module, as I don't expect it to be
    included into any other classes.

--- app/services/deployment.rb
+++ app/services/deployment.rb
@@ -1,29 +1,12 @@
-module Deployment
-  module_function
+class Deployment
 
-  def info
+  def self.info
     {
-      version_number: version_number,
-      build_date: build_date,
-      commit_id: commit_id,
-      build_tag: build_tag
+      version_number: ENV['APPVERSION'] || 'unknown',
+      build_date: ENV['APP_BUILD_DATE'] || 'unknown',
+      commit_id: ENV['APP_GIT_COMMIT'] || 'unknown',
+      build_tag: ENV['APP_BUILD_TAG'] || 'unknown'
     }
-  end
-
-  def version_number
-    ENV['APPVERSION'] || 'unknown'
-  end
-
-  def build_date
-    ENV['APP_BUILD_DATE'] || 'unknown'
-  end
-
-  def commit_id
-    ENV['APP_GIT_COMMIT'] || 'unknown'
-  end
-
-  def build_tag
-    ENV['APP_BUILD_TAG'] || 'unknown'
   end
 
 end
