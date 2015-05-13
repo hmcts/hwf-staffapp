505d476eb097f539464f3de3286b2fcc7edb9dba
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-13 11:52:11 +0100

    Updated migrate script
    * copy user office id into dwp_checks table

--- db/migrate/20150508131347_add_fields_to_dwp_check.rb
+++ db/migrate/20150508131347_add_fields_to_dwp_check.rb
@@ -2,5 +2,6 @@ class AddFieldsToDwpCheck < ActiveRecord::Migration
   def change
     add_column :dwp_checks, :our_api_token, :string
     add_reference :dwp_checks, :office, index: true, foreign_key: true
+    DwpCheck.connection.execute('UPDATE dwp_checks SET office_id = users.office_id FROM users WHERE users.id = dwp_checks.created_by_id;')
   end
 end
