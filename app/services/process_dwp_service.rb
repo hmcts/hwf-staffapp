f4a68cb08deb58f09f5e8db27144724feb4d6f4a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-22 15:27:02 +0100

    refactored process dwp service
    * return a json object with three attributes
    	- success
    	- dwp_check
    	- message
    
    * handle that in the dwp_checks controller

--- app/services/process_dwp_service.rb
+++ app/services/process_dwp_service.rb
@@ -1,11 +1,20 @@
 class ProcessDwpService
+
+  attr_accessor :result
+  attr_accessor :message
+
   def initialize(dwp_check)
+    @result = false
     @dwp_checker = dwp_check
     check_remote_api
   end
 
   def result
-    {'key':'value'}.to_json
+    {
+      success: @result,
+      dwp_check: @dwp_checker,
+      message: @message
+    }.to_json
   end
 
 private
@@ -18,16 +27,31 @@ private
   end
 
   def query_proxy_api
-    params = {
+    params = build_params
+    response = JSON.parse(RestClient.post "#{ENV['DWP_API_PROXY']}/api/benefit_checks", params)
+    @dwp_checker.dwp_result = response['benefit_checker_status']
+    @dwp_checker.dwp_id = response['confirmation_ref']
+    @result = true
+    rescue Errno::ECONNREFUSED
+      log_error I18n.t('error_messages.dwp_checker.unavailable'), 'Server unavailable'
+    rescue => e
+      log_error(e.message, 'Unspecified error')
+  end
+
+  def build_params
+    {
       id: @dwp_checker.our_api_token,
       ni_number: @dwp_checker.ni_number,
       surname: @dwp_checker.last_name.upcase,
       birth_date: @dwp_checker.dob.strftime('%Y%m%d'),
       entitlement_check_date: process_check_date
     }
-    response = JSON.parse(RestClient.post "#{ENV['DWP_API_PROXY']}/api/benefit_checks", params)
-    @dwp_checker.dwp_result = response['benefit_checker_status']
-    @dwp_checker.dwp_id = response['confirmation_ref']
+  end
+
+  def log_error(message, result)
+    @message = message
+    @dwp_checker.update!(dwp_result: result)
+    LogStuff.log "dwp lookup", @message
   end
 
   def process_check_date
