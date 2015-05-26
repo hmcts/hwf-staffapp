60ebe9de08cabeec75aa272c37b5c9309fc326f0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-26 15:42:06 +0100

    Implement Manager view
    * populate paged list of dwp_checks for passing to view
    * render partial view for managers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
     if user_signed_in?
       @checks_by_day = DwpCheck.by_office_grouped_by_type(current_user.office_id).checks_by_day
       @r2_checks = R2Calculator.by_office_grouped_by_type(current_user.office_id).checks_by_day
+      if current_user.manager?
+        @dwpchecks=DwpCheck.by_office(current_user.office_id).page(params[:page]).order('created_at DESC')
+      end
     end
   end
 end
