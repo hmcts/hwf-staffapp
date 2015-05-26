958c83cddb824d9fe68d4bc584a53272b69353bc
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-26 15:42:06 +0100

    Implement Manager view
    * populate paged list of dwp_checks for passing to view
    * render partial view for managers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,4 @@
 class HomeController < ApplicationController
   def index
-    if user_signed_in? && current_user.admin?
-      @report_data = []
-      Office.non_digital.each do |office|
-        @report_data << {
-          name: office.name,
-          dwp_checks: DwpCheck.by_office_grouped_by_type(office.id).checks_by_day
-        }
-      end
-    end
   end
 end
