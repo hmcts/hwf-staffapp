d8bf0a681a0a1fedd702fd67b0cff9a909997d04
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-26 12:38:45 +0100

    Updated user with manager role
    * added to ability class
    * Re-sequenced roles to make user default
     [starts #93738888]
    * Update ability for manager user creation

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,13 +6,22 @@ class Ability
 
     if user.admin?
       can :manage, :all
+    elsif user.manager?
+      can [:read, :create, :show], User, office_id: user.office_id
+      users_can
     else
-      can :read, Office
-      can :new, DwpCheck
-      can :lookup, DwpCheck
-      can :show, DwpCheck
-      can :create, R2Calculator
-      can :create, Feedback
+      users_can
     end
+  end
+
+private
+
+  def users_can
+    can :read, Office
+    can :new, DwpCheck
+    can :lookup, DwpCheck
+    can :show, DwpCheck
+    can :create, R2Calculator
+    can :create, Feedback
   end
 end
