159ad555443fc77ead5716565d3261dd943962fd
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-05-26 06:03:29 +0100

    Updated the specs
    So that they work with the new behaviour

--- spec/services/process_dwp_service_spec.rb
+++ spec/services/process_dwp_service_spec.rb
@@ -21,7 +21,7 @@ describe ProcessDwpService do
                "confirmation_ref": "T1426267181940",
                "@xmlns": "https://lsc.gov.uk/benefitchecker/service/1.0/API_1.0_Check"}'
         stub_request(:post, "#{ENV['DWP_API_PROXY']}/api/benefit_checks").
-          with(body: { id: check.our_api_token, birth_date: '19800101', entitlement_check_date: Date.today.strftime('%Y%m%d'), ni_number: 'AB123456A', surname: 'LAST_NAME' }).
+          with(body: { id: check.unique_token, birth_date: '19800101', entitlement_check_date: Date.yesterday.strftime('%Y%m%d'), ni_number: 'AB123456A', surname: 'LAST_NAME' }).
           to_return(status: 200, body: json, headers: {})
         described_class.new(check)
       end
