09f5d692895329217e43f4c5bd1a767430823fae
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-27 13:58:23 +0100

    Refactored loading dwp_checks
    * implemented tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,19 @@
 class HomeController < ApplicationController
+  before_action :load_dwp_data, only: [:index], if: 'user_signed_in? && current_user.manager?'
+
   def index
     if user_signed_in?
       @checks_by_day = DwpCheck.by_office_grouped_by_type(current_user.office_id).checks_by_day
       @r2_checks = R2Calculator.by_office_grouped_by_type(current_user.office_id).checks_by_day
-      if current_user.manager?
-        @dwpchecks=DwpCheck.by_office(current_user.office_id).page(params[:page]).order('created_at DESC')
-      end
     end
+  end
+
+private
+
+  def load_dwp_data
+    @dwpchecks = DwpCheck.
+                 by_office(current_user.office_id).
+                 page(params[:page]).
+                 order('created_at DESC')
   end
 end
