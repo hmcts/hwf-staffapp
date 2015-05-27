ff1cbb9c3a11440883d4db1b5e726ab73e35c9e1
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-05-27 13:58:23 +0100

    Refactored loading dwp_checks
    * implemented tests

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,15 @@
 class HomeController < ApplicationController
+  before_action :load_dwp_data, only: [:index], if: 'user_signed_in? && current_user.manager?'
+
   def index
+  end
+
+private
+
+  def load_dwp_data
+    @dwpchecks = DwpCheck.
+                 by_office(current_user.office_id).
+                 page(params[:page]).
+                 order('created_at DESC')
   end
 end
