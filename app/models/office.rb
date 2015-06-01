8997eeb9ba5f2d0334a3450005ffd1352cb4ea39
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-06-01 11:23:27 +0100

    Added non_digital scope to offices
    For reporting

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,6 +2,7 @@ class Office < ActiveRecord::Base
   has_many :users
 
   scope :sorted, -> {  all.order(:name) }
+  scope :non_digital, -> { where('name != ?', 'Digital') }
 
   validates :name, presence: true
 
