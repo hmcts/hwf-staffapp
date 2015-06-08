e9125d2663cdf29a6b26c491784d70a8786dd52b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-06-08 10:08:18 +0100

    refactored home_controller tests

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,42 +4,64 @@ RSpec.describe HomeController, type: :controller do
 
   include Devise::TestHelpers
 
-  describe "GET #index" do
-    let(:user)          { FactoryGirl.create :user }
-    let(:manager)       { FactoryGirl.create :manager }
+  describe 'GET #index' do
+    let(:user)      { create :user }
+    let(:manager)   { create :manager }
 
     context 'when the user is authenticated' do
       context 'as a user' do
-        before { sign_in user }
-        it "returns http success" do
-          get :index
-          expect(response).to have_http_status(:success)
-        end
-      end
-      context 'as a manager' do
+
         before(:each) do
-          DwpCheck.delete_all
-          FactoryGirl.create_list :dwp_check, 2, created_by: manager, office: manager.office
-          sign_in manager
+          sign_in user
           get :index
         end
+
         it 'returns http success' do
           expect(response).to have_http_status(:success)
         end
-        it 'populates a list of dwp_checks' do
-          expect(assigns(:dwpchecks).count).to eql(2)
+
+        it 'renders the index view' do
+          expect(response).to render_template :index
         end
+      end
+    end
+
+    context 'as a manager' do
+
+      before(:each) do
+        DwpCheck.delete_all
+        create_list :dwp_check, 2, created_by: manager, office: manager.office
+        sign_in manager
+        get :index
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'populates a list of dwp_checks' do
+        expect(assigns(:dwpchecks).count).to eql(2)
+      end
+
+      it 'renders the index view' do
+        expect(response).to render_template :index
       end
     end
 
     context 'when the user is not authenticated' do
-      before { sign_out user }
 
-      it "returns http success" do
+      before(:each) do
+        sign_out user
         get :index
+      end
+
+      it 'returns http success' do
         expect(response).to have_http_status(:success)
+      end
+
+      it 'renders the index view' do
+        expect(response).to render_template :index
       end
     end
   end
-
 end
