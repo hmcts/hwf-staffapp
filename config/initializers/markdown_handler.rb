fe5980a8035a87ce806dcf68bbd104cf30988fa7
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-06-10 12:44:13 +0100

    Updated markdown handler
    - remove string injection
    - replace with ruby object builder

--- config/initializers/markdown_handler.rb
+++ config/initializers/markdown_handler.rb
@@ -1,22 +1,31 @@
-class MarkdownTemplateHandler
-  def self.call(template)
-    erb = ActionView::Template.registered_template_handler(:erb)
-    source = erb.call(template)
-    <<-SOURCE
-    renderer = ::Redcarpet::Render::HTML.new(hard_wrap: true)
-    options = {
-      autolink: true,
-      no_intra_emphasis: true,
-      fenced_code_blocks: true,
-      lax_html_blocks: true,
-      strikethrough: true,
-      superscript: true,
-      tables: true
-    }
-    ::Redcarpet::Markdown.new(renderer, options).render(begin;#{source};end).html_safe
-    SOURCE
+module ActionView
+  module Template::Handlers
+    # Rails template handler for Markdown
+    class Markdown
+      class_attribute :default_format
+      self.default_format = Mime::HTML
+
+      # @param template [ActionView::Template]
+      # @return [String] Ruby code that when evaluated will return the rendered
+      #   content
+      def call(template)
+        @markdown ||= Redcarpet::Markdown.new(
+          Redcarpet::Render::HTML.new(
+            hard_wrap:          true,
+            with_toc_data:      true
+          ),
+          autolink:             true,
+          no_intra_emphasis:    true,
+          fenced_code_blocks:   true,
+          lax_html_blocks:      true,
+          strikethrough:        true,
+          superscript:          true,
+          tables:               true
+        )
+        "#{@markdown.render(template.source).inspect}.html_safe"
+      end
+    end
   end
 end
 
-ActionView::Template.register_template_handler(:md, MarkdownTemplateHandler)
-ActionView::Template.register_template_handler(:markdown, MarkdownTemplateHandler)
+ActionView::Template.register_template_handler(:md, ActionView::Template::Handlers::Markdown.new)
