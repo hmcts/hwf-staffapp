052aef465bdfd183128d7df119a1967c6a729a83
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-06-12 14:34:34 +0100

    Added route example for '/healthcheck'

--- spec/routing/health_status_spec.rb
+++ spec/routing/health_status_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe HealthStatusController, type: :routing do
     it 'routes to #ping' do
       expect(get: '/ping', format: :json).to route_to('health_status#ping')
     end
+
+    it 'routes to #healthcheck' do
+      expect(get: '/healthcheck', format: :json).to route_to('health_status#healthcheck')
+    end
   end
 end
