ab3225b583f50ead85ef871668d87f4b83da7a2e
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-06-18 17:03:54 +0100

    link user to jurisdiction

--- spec/factories/jurisdictions.rb
+++ spec/factories/jurisdictions.rb
@@ -3,5 +3,8 @@ FactoryGirl.define do
     name { Faker::Company.name }
     abbr { |n| "#{Faker::Hacker.abbreviation} #{n}" }
     active true
+    factory :invalid_jurisdiction do
+      name nil
+    end
   end
 end
