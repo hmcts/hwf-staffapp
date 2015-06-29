8ac5951b70c97fd564a536e510b3a44af6ffda4a
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-06-29 12:02:03 +0100

    RuboCop fix
    Style fix due to RuboCop's complaint

--- app/helpers/common_scopes.rb
+++ app/helpers/common_scopes.rb
@@ -4,8 +4,9 @@ module CommonScopes
   def self.included(model)
     model.instance_eval do
       scope :checks_by_day, lambda {
-        group_by_day("#{name.pluralize.underscore}.created_at", format: "%d %b %y").
-          where("#{name.pluralize.underscore}.created_at > ?", (Time.zone.today.-6.days)).count
+        model_name = name.pluralize.underscore
+        group_by_day("#{model_name}.created_at", format: "%d %b %y").
+          where("#{model_name}.created_at > ?", (Time.zone.today.-6.days)).count
       }
     end
   end
