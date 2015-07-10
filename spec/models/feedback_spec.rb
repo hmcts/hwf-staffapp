6944222cba61b99c4f09fba60aeecf39185889b2
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-07-10 15:15:36 +0100

    Added whitespace
    Added whitespace for legibility

--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe Feedback, type: :model do
   it 'passes factory build' do
     expect(feedback).to be_valid
   end
+
   context 'validations' do
     it 'requires a user to stored' do
       feedback.user = nil
       expect(feedback).to be_invalid
     end
+
     it 'requires an office to be stored' do
       feedback.office = nil
       expect(feedback).to be_invalid
