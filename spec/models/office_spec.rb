1d2dfca7ef38f91d2bf0d8c1c5fef9599739072e
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-07-10 11:45:36 +0100

    Fixed a fickle example
    It cared about the order the manager emails were displayed, but it isn't
    relevant for this example.

--- spec/models/office_spec.rb
+++ spec/models/office_spec.rb
@@ -77,11 +77,15 @@ RSpec.describe Office, type: :model do
     end
     context 'managers are populated' do
       before(:each) { User.delete_all }
+      let(:manager1) { FactoryGirl.create :manager, office: office }
+      let(:manager2) { FactoryGirl.create :manager, office: office }
+      let(:manager_emails) { office.managers_email }
+
       it 'returns an html string of emails of users in the manager role' do
-        manager1 = FactoryGirl.create :manager, office: office
-        manager2 = FactoryGirl.create :manager, office: office
-        expected = "<a href=\"mailto:#{manager1.email}\">#{manager1.name}</a>, <a href=\"mailto:#{manager2.email}\">#{manager2.name}</a>"
-        expect(office.managers_email).to eql(expected)
+        [manager1, manager2].each do |manager|
+          expect(office.managers_email).to include "#{manager.email}"
+          expect(office.managers_email).to include "#{manager.name}"
+        end
       end
     end
   end
