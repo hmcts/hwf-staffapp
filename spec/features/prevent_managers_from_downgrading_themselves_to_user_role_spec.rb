349576080e3498a987b2b9fb4ec93b09d33ffa97
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-07-13 14:24:41 +0100

    Prevent the manager from changing their role
    Don't allow a manager to change their role to user.
    
    Added the examples for the scenario and removed the previous scenario
    which did allow the manager to change his role.

---
+++ spec/features/prevent_managers_from_downgrading_themselves_to_user_role_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature 'Prevent managers from downgrading themselves to user role', type: :feature do
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  let(:manager) { create :manager }
+
+  before(:each) do
+    login_as manager
+    visit edit_user_path(manager.id)
+  end
+
+  context 'show view' do
+    it 'shows the role' do
+      expect(page).to have_text 'Role'
+    end
+
+    it "doens't allow editing of the role" do
+      expect(page).to_not have_xpath '//*[@id="user_role_user"]'
+    end
+  end
+end
