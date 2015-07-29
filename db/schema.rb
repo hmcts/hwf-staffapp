4d4bbabfad954be29f326482ba81ffc2d2ec1c9a
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-07-29 16:07:04 +0100

    added threshold_exceeded to application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150721114357) do
+ActiveRecord::Schema.define(version: 20150729150135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20150721114357) do
     t.boolean  "married"
     t.decimal  "fee"
     t.string   "status"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "jurisdiction_id"
     t.date     "date_received"
     t.string   "form_name"
@@ -38,6 +38,8 @@ ActiveRecord::Schema.define(version: 20150721114357) do
     t.date     "date_fee_paid"
     t.integer  "user_id"
     t.integer  "office_id"
+    t.boolean  "threshold_exceeded"
+    t.boolean  "over_61"
   end
 
   add_index "applications", ["office_id"], name: "index_applications_on_office_id", using: :btree
@@ -148,9 +150,9 @@ ActiveRecord::Schema.define(version: 20150721114357) do
   add_index "users", ["invited_by_id"], name: "index_users_on_invited_by_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "applications", "jurisdictions"
   add_foreign_key "applications", "offices"
   add_foreign_key "applications", "users"
-  add_foreign_key "applications", "jurisdictions"
   add_foreign_key "feedbacks", "offices"
   add_foreign_key "feedbacks", "users"
   add_foreign_key "office_jurisdictions", "jurisdictions"
