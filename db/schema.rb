9e0a589369894aa386aa1e33fe7fb172ceec0b45
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-07-30 10:57:02 +0100

    Added migration for benefits column to application
    New column added for the 4th step of the state machine, 'Benefits' page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150729150135) do
+ActiveRecord::Schema.define(version: 20150730091719) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150729150135) do
     t.decimal  "threshold"
     t.boolean  "threshold_exceeded"
     t.boolean  "over_61"
+    t.boolean  "benefits"
   end
 
   add_index "applications", ["office_id"], name: "index_applications_on_office_id", using: :btree
