95ed797938a09aebbfe67919586bb1e267183552
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-07-30 15:11:15 +0100

    Added methods
    * known_over_61?
    * savings_investment_result?
    * applicant_age
    
    Called known_over_61 when setting fee to set threshold

--- spec/models/applications/step_3_spec.rb
+++ spec/models/applications/step_3_spec.rb
@@ -10,21 +10,92 @@ RSpec.describe Application, type: :model do
   describe 'Step 3 - Savings and investments' do
     before { application.status = 'savings_investments' }
 
-    describe 'threshold' do
-      describe 'is set' do
-        it 'to 3000 when the fee is less than 1000' do
-          application.fee = '999.99'
-          expect(application.threshold).to eq 3000
+    describe 'methods' do
+      describe 'savings_investment_result' do
+        context 'threshold_exceeded is true' do
+          before { application.threshold_exceeded = true }
+
+          it 'returns failed' do
+            expect(application.savings_investment_result?).to eq false
+          end
+
+          context 'over_61 is true' do
+            before { application.over_61 = true }
+
+            it 'returns failed' do
+              expect(application.savings_investment_result?).to eq false
+            end
+          end
         end
 
-        it 'to 3000 then the fee is equal to 1000' do
-          application.fee = '1000'
-          expect(application.threshold).to eq 3000
+        context 'threshold_exceeded is false' do
+          before { application.threshold_exceeded = false }
+
+          it 'returns passed' do
+            expect(application.savings_investment_result?).to eq true
+          end
+        end
+        context 'threshold_exceeded is true' do
+          before { application.threshold_exceeded = true }
+
+          context 'over_61 is false' do
+            before { application.over_61 = false }
+
+            it 'returns passed' do
+              expect(application.savings_investment_result?).to eq true
+            end
+          end
+        end
+      end
+
+      describe 'threshold' do
+        describe 'is set' do
+          it 'to 3000 when the fee is less than 1000' do
+            application.fee = '999.99'
+            expect(application.threshold).to eq 3000
+          end
+
+          it 'to 3000 when the fee is equal to 1000' do
+            application.fee = '1000'
+            expect(application.threshold).to eq 3000
+          end
+
+          it 'to 4000 when the fee is greater than 1000' do
+            application.fee = '1001.00'
+            expect(application.threshold).to eq 4000
+          end
+
+          context 'when the applicant is over 61' do
+            before { application.date_of_birth = Time.zone.today - 62.years }
+            it 'to 16000 regardless of fee amount' do
+              ['999', '1000', '1001'].each do |fee|
+                application.fee = fee
+                expect(application.threshold).to eq 16000
+              end
+            end
+          end
+        end
+      end
+
+      describe 'known_over_61?' do
+        it 'is recognised' do
+          expect(application).to respond_to :known_over_61?
         end
 
-        it 'to 4000 then the fee is greater than 1000' do
-          application.fee = '1001.00'
-          expect(application.threshold).to eq 4000
+        context 'when applicant is over 61' do
+          before { application.date_of_birth = Time.zone.today - 61.years }
+
+          it 'returns true' do
+            expect(application.known_over_61?).to eq true
+          end
+        end
+
+        context 'when applicant is under 61' do
+          before { application.date_of_birth = Time.zone.today - 01.years }
+
+          it 'returns false' do
+            expect(application.known_over_61?).to eq false
+          end
         end
       end
     end
