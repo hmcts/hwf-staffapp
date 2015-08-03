5717f225b3a8b6ffa336639052ff42c8f30402c2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-08-03 15:53:12 +0100

    Update locale to en-GB

--- config/application.rb
+++ config/application.rb
@@ -33,6 +33,8 @@ module FrStaffapp
 
     # Custom directories with classes and modules you want to be autoloadable.
     config.autoload_paths += %W[#{config.root}/scrapers #{config.root}/lib]
+
+    config.i18n.default_locale = 'en-GB'
   end
 
   WillPaginate.per_page = 20
