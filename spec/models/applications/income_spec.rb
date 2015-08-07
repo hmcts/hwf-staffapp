44788592792520da9765f335cccb0ec1daf616a4
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-08-07 14:32:42 +0100

    Added income
    Added the income attribute (i.e. migration), view and all the required
    tests.

--- spec/models/applications/income_spec.rb
+++ spec/models/applications/income_spec.rb
@@ -14,6 +14,23 @@ RSpec.describe Application, type: :model do
       describe 'income' do
         describe 'presence' do
           before do
+            application.income = nil
+            application.valid?
+          end
+
+          it 'must be entered' do
+            expect(application).to be_invalid
+          end
+
+          it 'returns an error if missing' do
+            expect(application.errors[:income]).to eq ['is not a number']
+          end
+        end
+      end
+
+      describe 'children' do
+        describe 'presence' do
+          before do
             application.children = nil
             application.valid?
           end
