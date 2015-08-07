5b9fc35643cba0c351f15fbbff1f90e53861e6f4
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-08-07 12:44:18 +0100

    Add view and tests for benefit results

--- spec/controllers/applications/build_controller_spec.rb
+++ spec/controllers/applications/build_controller_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe Applications::BuildController, type: :controller do
   render_views
 
   include Devise::TestHelpers
+  before { WebMock.disable_net_connect!(allow: 'codeclimate.com') }
 
   let(:user)          { create :user }
 
@@ -59,6 +60,45 @@ RSpec.describe Applications::BuildController, type: :controller do
 
         it 'displays the income view' do
           expect(response).to render_template :income
+        end
+      end
+
+      context 'benefits result' do
+        context 'user has selected "yes" to benefits' do
+          before do
+            stub_request(:post, "#{ENV['DWP_API_PROXY']}/api/benefit_checks").with(body:
+            {
+              birth_date: (Time.zone.today - 20.years).strftime('%Y%m%d'),
+              entitlement_check_date: (Time.zone.today).strftime('%Y%m%d'),
+              id: "#{user.name.gsub(' ', '').downcase.truncate(27)}@#{application.created_at.strftime('%y%m%d%H%M%S')}.#{application.id}",
+              ni_number: 'AB123456A',
+              surname: application.last_name.upcase
+            }).to_return(status: 200, body: '', headers: {})
+            application.benefits = true
+            application.ni_number = 'AB123456A'
+            application.save
+            get :show, application_id: application.id, id: :benefits_result
+          end
+
+          it 'displays the benefits result view' do
+            expect(response).to render_template :benefits_result
+          end
+        end
+
+        context 'user has selected "no" to benefits' do
+          before do
+            application.benefits = false
+            application.save
+            get :show, application_id: application.id, id: :benefits_result
+          end
+
+          it 'redirects' do
+            expect(response).to have_http_status(:redirect)
+          end
+
+          it 'redirects to the summary page' do
+            expect(response).to redirect_to redirect_to(application_build_path(application_id: assigns(:application).id, id: :summary))
+          end
         end
       end
 
---
+++ spec/features/applications/benefit_results_are_rendered_spec.rb
@@ -0,0 +1,163 @@
+require 'rails_helper'
+
+RSpec.feature 'Benefit Results', type: :feature do
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  let(:user) { create :user, office: create(:office) }
+  let(:application) { create :application, user_id: user.id, ni_number: 'AB123456A', benefits: true, status: 'benefits_result' }
+  context 'as a signed in user' do
+
+    before do
+      WebMock.disable_net_connect!(allow: 'codeclimate.com')
+      json = '{"original_client_ref": "unique", "benefit_checker_status": "Yes",
+                  "confirmation_ref": "T1426267181940",
+                  "@xmlns": "https://lsc.gov.uk/benefitchecker/service/1.0/API_1.0_Check"}'
+      stub_request(:post, "#{ENV['DWP_API_PROXY']}/api/benefit_checks").with(body:
+        {
+          birth_date: application.date_of_birth.strftime('%Y%m%d'),
+          entitlement_check_date: application.date_received.strftime('%Y%m%d'),
+          id: "#{user.name.gsub(' ', '').downcase.truncate(27)}@#{application.created_at.strftime('%y%m%d%H%M%S')}.#{application.id}",
+          ni_number: application.ni_number,
+          surname: application.last_name.upcase
+        }).to_return(status: 200, body: json, headers: {})
+    end
+
+    before do
+      login_as user
+    end
+
+    context 'after user selects yes to benefits' do
+      before do
+        application.last_benefit_check.update_attributes(dwp_result: 'yes', error_message: nil)
+        visit application_build_path(application_id: application.id, id: 'benefits_result')
+      end
+
+      scenario 'the correct view is rendered' do
+        expect(page).to have_xpath('//h2', text: 'Benefits')
+        expect(page).to have_xpath('//div[contains(@class,"callout")]/h3[@class="bold"]')
+      end
+
+      scenario 'the next button is rendered' do
+        expect(page).to have_xpath('//input[@type="submit"]')
+      end
+
+      context 'the result is deceased' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'deceased', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "deceased")]/h3[@class="bold"]')
+        end
+
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "deceased")]/h3[@class="bold"]', text: I18n.t('benefit_checks.deceased.heading'))
+        end
+      end
+
+      context 'the result is no' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'no', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "no")]/h3[@class="bold"]')
+        end
+
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "no")]/h3[@class="bold"]', text: '✗ The applicant is not receiving benefits')
+        end
+      end
+
+      context 'the result is server unavailable' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'server_unavailable', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "server_unavailable")]/h3[@class="bold"]')
+        end
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "server_unavailable")]/h3[@class="bold"]', text: I18n.t('benefit_checks.server_unavailable.heading'))
+        end
+      end
+
+      context 'the result is superseded' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'superseded', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "superseded")]/h3[@class="bold"]')
+        end
+
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "superseded")]/h3[@class="bold"]', text: I18n.t('benefit_checks.superseded.heading'))
+        end
+      end
+
+      context 'the result is undetermined' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'Undetermined', error_message: nil)
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+        end
+
+        scenario 'the next button is not rendered' do
+          expect(page).not_to have_xpath('//input[@type="submit"]')
+        end
+
+        scenario 'a link to personal details is rendered' do
+          expect(page).to have_xpath('//a', text: 'Check personal details')
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "undetermined")]/h3[@class="bold"]')
+        end
+
+        scenario 'the view is rendered with the correct information' do
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "undetermined")]/h3[@class="bold"]', text: I18n.t('benefit_checks.undetermined.heading'))
+        end
+      end
+
+      context 'the result is unspecified error' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'unspecified_error', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "unspecified_error")]/h3[@class="bold"]')
+        end
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "unspecified_error")]/h3[@class="bold"]', text: I18n.t('benefit_checks.unspecified_error.heading'))
+        end
+      end
+
+      context 'the result is yes' do
+        before do
+          application.last_benefit_check.update_attributes(dwp_result: 'yes', error_message: nil)
+        end
+
+        scenario 'the view is rendered with the correct style' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "yes")]/h3[@class="bold"]')
+        end
+        scenario 'the view is rendered with the correct information' do
+          visit application_build_path(application_id: application.id, id: 'benefits_result')
+          expect(page).to have_xpath('//div[contains(@class,"callout")][contains(@class, "yes")]/h3[@class="bold"]', text: '✓ The applicant is receiving the correct benefits')
+        end
+      end
+    end
+  end
+end
