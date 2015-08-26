e28cd4644df90bb70224c4fec25273e908cac954
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-08-26 12:46:09 +0100

    Updated OOBE password access
    * created helper to get resource values
    * removed explicit setting of @user from controller
    * reset form on password view to use resource variables

---
+++ app/helpers/out_of_box_helper.rb
@@ -0,0 +1,15 @@
+module OutOfBoxHelper
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= current_user
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+end
