6faaffcf72a8ae0670c3335e4b553f820e487212
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2015-08-26 11:27:55 +0100

    rough outline of views done

--- app/controllers/out_of_box_controller.rb
+++ app/controllers/out_of_box_controller.rb
@@ -4,11 +4,18 @@ class OutOfBoxController < ApplicationController
   respond_to :html
 
   def password
+    @user = current_user
   end
 
   def office
+     @office = current_user.office
+     @jurisdictions = Jurisdiction.all
   end
 
   def details
+    @user = current_user
+    @jurisdictions = Jurisdiction.all
+    @roles = User::ROLES
+    @offices = Office.all
   end
 end
