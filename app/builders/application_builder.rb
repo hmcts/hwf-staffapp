94e978277dbf7cbabb32c41498dabc88ceea0c99
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-08-27 15:25:41 +0100

    Implemented reference generation
    ApplicationBuilder now handles generating & populating of the reference
    number.

--- app/builders/application_builder.rb
+++ app/builders/application_builder.rb
@@ -13,6 +13,17 @@ class ApplicationBuilder
   end
 
   def create_reference
-    @reference = Reference.create(application_id: @application.id)
+    @reference = Reference.create(application_id: @application.id,
+                                  reference: format_reference)
+  end
+
+  private
+
+  def format_reference
+    entity_code = @user.office.entity_code
+    current_year = Time.zone.now.strftime('%y')
+    code_and_year = "#{entity_code}-#{current_year}"
+    counter = Reference.where("reference like ?", "#{code_and_year}-%").count + 1
+    "#{code_and_year}-#{counter}"
   end
 end
--- spec/builders/application_builder_spec.rb
+++ spec/builders/application_builder_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe ApplicationBuilder do
 
   let(:user) { create :user }
   let(:application_builder) { described_class.new(user) }
+  let(:entity_code) { user.office.entity_code }
+  let(:current_year) { Time.zone.now.strftime('%y') }
+  let(:counter) do
+    Reference.where("reference like ?", "#{entity_code}-#{current_year}-%").count + 1
+  end
 
   describe '#create_application' do
     let(:application_params) do
@@ -25,7 +30,8 @@ RSpec.describe ApplicationBuilder do
 
     let(:reference_params) do
       {
-        application_id: application_builder.application.id
+        application_id: application_builder.application.id,
+        reference: "#{entity_code}-#{current_year}-#{counter}"
       }
     end
 
