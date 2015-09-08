2e7e20aa70dbed8359fa16edac8ef001b123c579
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-08 11:59:59 +0100

    Added no remission confirmation page example
    Added the test to confirm the behaviour of when there is no remission on
    how the confirmation page should look

--- spec/features/applications/remission_confirmation_spec.rb
+++ spec/features/applications/remission_confirmation_spec.rb
@@ -37,14 +37,18 @@ RSpec.feature 'Confirmation page for remission', type: :feature do
         ['Write to the applicant with details of how much they have to pay',
          'Complete the remission register with the application details',
          'Write the reference number on the top right corner of the paper form',
-         'Copy the reference number into the case management system ']
+         'Copy the reference number into the case management system']
       end
 
       context 'after user continues to confirmation page' do
         before { visit_confirmation_page }
 
+        scenario 'they have part remission' do
+          expect(application.application_outcome).to eq 'part'
+        end
+
         scenario 'the part remission copy is show' do
-          part_remission_copy.each {|line| expect(page).to have_text line }
+          part_remission_copy.each { |line| expect(page).to have_text line }
         end
       end
     end
@@ -60,8 +64,34 @@ RSpec.feature 'Confirmation page for remission', type: :feature do
       context 'after user continues to confirmation page' do
         before { visit_confirmation_page }
 
+        scenario 'they have full remission' do
+          expect(application.application_outcome).to eq 'full'
+        end
+
         scenario 'the full remission copy is shown' do
-          full_remission_copy.each {|line| expect(page).to have_text line }
+          full_remission_copy.each { |line| expect(page).to have_text line }
+        end
+      end
+    end
+
+    context 'who has no remission' do
+      let(:application) { create(:application_no_remission) }
+      let(:no_remission_copy) do
+        ['Complete the remission register with the application details',
+         'Write the reference number on the top right corner of the paper form',
+         'Copy the reference number into the case management system',
+         'Write to the applicant and send back all the documents']
+      end
+
+      context 'after user continues to confirmation page' do
+        before { visit_confirmation_page }
+
+        scenario 'they have no remission' do
+          expect(application.application_outcome).to eq 'none'
+        end
+
+        scenario 'the no remission copy is shown' do
+          no_remission_copy.each { |line| expect(page).to have_text line }
         end
       end
     end
