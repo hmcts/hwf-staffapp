f8274ac50fc997bbc1296cdcf244da14111117a6
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-09-14 16:02:24 +0100

    remove un-needed @dwpchecks collection
    The dwp list is no longer shown on the manager dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,5 @@
 class HomeController < ApplicationController
   before_action :authenticate_user!, only: [:index]
-  before_action :load_dwp_data, only: [:index], if: 'user_signed_in? && current_user.manager?'
   before_action :load_graph_data, only: [:index], if: 'user_signed_in? && current_user.admin?'
 
   def index
@@ -9,13 +8,6 @@ class HomeController < ApplicationController
   end
 
   private
-
-  def load_dwp_data
-    @dwpchecks = DwpCheck.
-                 by_office(current_user.office_id).
-                 page(params[:page]).
-                 order('created_at DESC')
-  end
 
   def load_graph_data
     @report_data = []
