6b40f702d2867df74d892e9a9772ffe9fd24011c
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-15 15:31:29 +0100

    Trimmed down DWP API response
    Trimmed it so that we don't have to paste in a big blurb of text,
    repeating the same thing all over the place.
    
    With this change in place, you concentrate on the test at hand rather
    than at pasting 'spaghetti'.

---
+++ spec/support/dwp_response.rb
@@ -0,0 +1,9 @@
+
+def dwp_response(response)
+  json = { "original_client_ref": "unique",
+           "benefit_checker_status": "#{response}",
+           "confirmation_ref": "T1426267181940",
+           "@xmlns": "https://lsc.gov.uk/benefitchecker/service/1.0/API_1.0_Check" }.to_json
+  stub_request(:post, "#{ENV['DWP_API_PROXY']}/api/benefit_checks").
+    to_return(status: 200, body: json, headers: {})
+end
