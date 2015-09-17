c1a27a1f1c68d6be3647f8ac2aa65ecd5454acaf
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-17 14:29:49 +0100

    Moved attributes to a constant
    We did it so that we can include all the fields, in order to get the
    Application model to render correctly in the view. This change is to
    facilitate that.

--- spec/models/forms/personal_details_spec.rb
+++ spec/models/forms/personal_details_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Forms::PersonalDetails do
+  PARAMS_LIST = %i[last_name date_of_birth married title first_name ni_number]
 
-  subject { described_class.new }
+  let(:application) { create :application }
+  subject { described_class.new(application) }
 
-  describe '#permitted_attributes' do
-    let(:params_list) { %i[title first_name last_name date_of_birth ni_number married] }
-
+  describe 'PERMITTED_ATTRIBUTES' do
     it 'returns a list of attributes' do
-      expect(subject.permitted_attributes).to match_array(params_list)
+      expect(described_class::PERMITTED_ATTRIBUTES).to match_array(PARAMS_LIST)
     end
   end
 
@@ -37,6 +37,16 @@ RSpec.describe Forms::PersonalDetails do
       it 'is required' do
         subject.married = nil
         expect(subject).to be_invalid
+      end
+    end
+  end
+
+  describe 'construct form object from application' do
+    let(:form) { described_class.new(application) }
+
+    PARAMS_LIST.each do |attr_name|
+      it "assigns #{attr_name}" do
+        expect(form.send(attr_name)).to eq application.send(attr_name)
       end
     end
   end
