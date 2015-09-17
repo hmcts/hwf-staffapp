88e1ece0705b59612c8982803c89d6444a491e38
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-17 13:08:37 +0100

    Added validations for married attribute
    Because it's mandatory field in the form

--- app/models/forms/personal_details.rb
+++ app/models/forms/personal_details.rb
@@ -5,9 +5,12 @@ module Forms
 
     attr_accessor :last_name
     attr_accessor :date_of_birth
+    attr_accessor :married
 
     validates :last_name, presence: true, length: { minimum: 2 }
     validates :date_of_birth, presence: true
+    validates :married, inclusion: { in: [true, false] }
+
     def permitted_attributes
       %i[title first_name last_name date_of_birth ni_number married]
     end
