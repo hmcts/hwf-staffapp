93d22a4e6a3b60dd9a156b96b4392c76d5b85610
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-17 14:29:49 +0100

    Moved attributes to a constant
    We did it so that we can include all the fields, in order to get the
    Application model to render correctly in the view. This change is to
    facilitate that.

--- app/models/forms/personal_details.rb
+++ app/models/forms/personal_details.rb
@@ -1,18 +1,21 @@
 module Forms
   class PersonalDetails
+    include ActiveModel::Model
 
-    include ActiveModel::Validations
+    PERMITTED_ATTRIBUTES = %i[last_name date_of_birth married title ni_number first_name]
 
-    attr_accessor :last_name
-    attr_accessor :date_of_birth
-    attr_accessor :married
+    attr_accessor *PERMITTED_ATTRIBUTES
+
+    def initialize(application)
+      attrs = application.attributes.select do |key, _|
+        PERMITTED_ATTRIBUTES.include?(key.to_sym)
+      end
+
+      super(attrs)
+    end
 
     validates :last_name, presence: true, length: { minimum: 2 }
     validates :date_of_birth, presence: true
     validates :married, inclusion: { in: [true, false] }
-
-    def permitted_attributes
-      %i[title first_name last_name date_of_birth ni_number married]
-    end
   end
 end
