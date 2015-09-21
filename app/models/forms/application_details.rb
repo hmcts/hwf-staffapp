c03cfb0e8eba0e279555fe7143854c366d5b09ed
Aleksandar Simić & Colin Bruce <alex.simic+colin.bruce@digital.justice.gov.uk>
2015-09-21 12:06:50 +0100

    Probate handling added to ApplicationDetails
    Mimicking the beahviour on the application_details page, by adding it to
    ApplicationDetails form object.

--- app/models/forms/application_details.rb
+++ app/models/forms/application_details.rb
@@ -1,17 +1,45 @@
 module Forms
   class ApplicationDetails
     include ActiveModel::Model
-    PERMITTED_ATTRIBUTES = %i[fee jurisdiction_id date_received]
+    PERMITTED_ATTRIBUTES = %i[fee jurisdiction_id date_received probate date_of_death deceased_name refund date_fee_paid]
 
     attr_accessor *PERMITTED_ATTRIBUTES
+
+    def initialize(object)
+      attrs = extract_params(object)
+      super(attrs)
+    end
 
     validates :fee, numericality: { allow_blank: true }
     validates :fee, presence: true
     validates :jurisdiction_id, presence: true
 
     validates :date_received, date: {
-      after: proc { Time.zone.today - 3.months },
-      before: proc { Time.zone.today + 1.day }
-    }
+                after: proc { Time.zone.today - 3.months },
+                before: proc { Time.zone.today + 1.day }
+              }
+
+    with_options if: :probate? do
+      validates :deceased_name, presence: true
+      validates :date_of_death, date: {
+                  before: proc { Time.zone.today + 1.day }
+                }
+    end
+
+    private
+
+    def probate?
+      probate
+    end
+
+    def extract_params(object)
+      get_attribs(object).select do |key, _|
+        PERMITTED_ATTRIBUTES.include?(key.to_sym)
+      end
+    end
+
+    def get_attribs(object)
+      object.is_a?(Application) ? object.attributes : object
+    end
   end
 end
