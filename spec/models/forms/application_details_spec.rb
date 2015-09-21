2cc7ab3e7875695752f5eaf95b8b4bf94d9471bd
Aleksandar Simić & Colin Bruce <alex.simic+colin.bruce@digital.justice.gov.uk>
2015-09-21 12:06:50 +0100

    Probate handling added to ApplicationDetails
    Mimicking the beahviour on the application_details page, by adding it to
    ApplicationDetails form object.

--- spec/models/forms/application_details_spec.rb
+++ spec/models/forms/application_details_spec.rb
@@ -1,15 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe Forms::ApplicationDetails do
-  PARAMS_LIST = %i[fee jurisdiction_id date_received]
+  PARAMS_LIST = %i[fee jurisdiction_id date_received probate date_of_death deceased_name refund date_fee_paid]
 
-  subject { described_class.new }
+  let(:application) { create :application }
+
+  subject { described_class.new(application) }
 
   describe 'PERMITTED_ATTRIBUTES' do
     it 'returns a list of attributes' do
       expect(described_class::PERMITTED_ATTRIBUTES).to match_array(PARAMS_LIST)
     end
   end
+
+  describe 'when Application object is passed in' do
+    let(:form) { described_class.new(application) }
+
+    PARAMS_LIST.each do |attr_name|
+      it "assigns #{attr_name}" do
+        expect(form.send(attr_name)).to eq application.send(attr_name)
+      end
+    end
+  end
+
+  describe 'when a Hash is passed in' do
+    let(:hash) { application.attributes }
+    let(:form) { described_class.new(hash) }
+
+    PARAMS_LIST.each do |attr_name|
+      it "assigns #{attr_name}" do
+        expect(form.send(attr_name)).to eq hash[attr_name.to_s]
+      end
+    end
+  end
+
+
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:fee) }
@@ -83,6 +108,67 @@ RSpec.describe Forms::ApplicationDetails do
         end
       end
     end
-  end
 
+    describe 'probate' do
+      let(:probate) do
+        described_class.new({ jurisdiction_id: 1,
+                              fee: 500,
+                              date_received: Time.zone.yesterday,
+                              probate: true,
+                              deceased_name: 'Bob the builder',
+                              date_of_death: Time.zone.yesterday })
+      end
+
+      it 'has a valid factory build' do
+        expect(probate).to be_valid
+      end
+
+      it 'passes if probate unchecked' do
+        probate.probate = false
+        probate.deceased_name = nil
+        expect(probate).to be_valid
+      end
+
+      describe 'requires' do
+        describe 'date of death' do
+          describe 'presence' do
+            before do
+              probate.date_of_death = nil
+              probate.valid?
+            end
+
+            it 'must be entered' do
+              expect(probate).to be_invalid
+            end
+
+            it 'returns an error if missing' do
+              expect(probate.errors[:date_of_death]).to eq ['Enter the date in this format 01/01/2015']
+            end
+          end
+
+          describe 'range' do
+            it 'must be prior to today' do
+              probate.date_of_death = Time.zone.tomorrow
+              expect(probate).to be_invalid
+            end
+          end
+        end
+
+        describe 'deceased name' do
+          before do
+            probate.deceased_name = nil
+            probate.valid?
+          end
+
+          it 'must be entered' do
+            expect(probate).to be_invalid
+          end
+
+          it 'returns an error if missing' do
+            expect(probate.errors[:deceased_name]).to eq ["The deceased's name should be entered"]
+          end
+        end
+      end
+    end
+  end
 end
