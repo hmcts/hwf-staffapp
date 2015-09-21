39480b7deeaeb09961addeb5f17dc9e76202c73b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-09-21 10:39:04 +0100

    Add timestamps to benefit check model

---
+++ db/migrate/20150921084620_add_timestamps_to_benefit_check.rb
@@ -0,0 +1,19 @@
+class AddTimestampsToBenefitCheck < ActiveRecord::Migration
+  def up
+    change_table(:benefit_checks) { |t| t.timestamps }
+    migrate_sql = <<SQL
+UPDATE benefit_checks
+SET
+  created_at = "applications"."created_at",
+  updated_at = "applications"."updated_at"
+FROM "applications"
+WHERE "applications"."id" = "benefit_checks"."application_id";
+SQL
+    execute(migrate_sql)
+  end
+
+  def down
+    remove_column :benefit_checks, :created_at
+    remove_column :benefit_checks, :updated_at
+  end
+end
