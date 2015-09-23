860bbae327d6758bef1f6d85ebe3f46f4b5ba5c5
Jakub Novotny <novotny.jakub@gmail.com>
2015-09-23 17:37:33 +0100

    Refactor out of the box feature tests
    Using the existing approach didn't make sense as the result were not
    readable scenarios. Also the next few tests need to be clear what
    they do

--- spec/features/manager_out_of_the_box_experience_spec.rb
+++ spec/features/manager_out_of_the_box_experience_spec.rb
@@ -1,42 +1,59 @@
 require 'rails_helper'
 
+# I'm disabling this Rubocop check to allow writing readable scenarios
+# rubocop:disable RSpec/InstanceVariable
+
 RSpec.feature 'Manager has to setup their office and preferences', type: :feature do
-  include Warden::Test::Helpers
-  Warden.test_mode!
 
-  let(:office) { create :office }
-  let(:manager) { create :manager, office: office, sign_in_count: sign_in_count }
-
-  before do
-    manager_login
+  scenario 'Manager signing in for second time or later and their office is not setup is redirected to office setup' do
+    given_the_office_is_not_setup
+    and_manager_has_signed_in_before
+    when_they_sign_in
+    they_are_redirected_to_the_office_setup
   end
 
-  context 'Manager logging in for the first time' do
-    let(:sign_in_count) { 0 }
+  scenario 'Manager signing in for second time or later and their office is already setup is redirected to dashboard' do
+    given_the_office_is_setup
+    and_manager_has_signed_in_before
+    when_they_sign_in
+    they_are_redirected_to_the_dashboard
   end
 
-  context 'Manager logging in for second time or later' do
-    let(:sign_in_count) { 1 }
-
-    context 'when the office is already setup' do
-      let(:office) { create :office_with_jurisdictions }
-
-      scenario 'the dashboard is displayed' do
-        expect(page.current_path).to eql '/'
-      end
-    end
-
-    context 'when the office is not setup' do
-      scenario 'the office setup page is displayed', focus: true do
-        expect(page.current_path).to eql "/offices/#{office.id}/edit"
-      end
-    end
+  scenario 'Manager signing in for the first time is redirected to office setup' do
+    given_the_office_is_not_setup
+    and_manager_has_not_signed_in_before
+    when_they_sign_in
+    they_are_redirected_to_the_office_setup
   end
 
-  def manager_login
+  def given_the_office_is_not_setup
+    @office = create :office
+  end
+
+  def given_the_office_is_setup
+    @office = create :office_with_jurisdictions
+  end
+
+  def and_manager_has_signed_in_before
+    @manager = create :manager, office: @office, sign_in_count: 2
+  end
+
+  def and_manager_has_not_signed_in_before
+    @manager = create :manager, office: @office, sign_in_count: 0
+  end
+
+  def when_they_sign_in
     visit new_user_session_path
-    fill_in 'user_email', with: manager.email
-    fill_in 'user_password', with: manager.password
+    fill_in 'user_email', with: @manager.email
+    fill_in 'user_password', with: @manager.password
     click_button 'Sign in'
+  end
+
+  def they_are_redirected_to_the_office_setup
+    expect(page.current_path).to eql "/offices/#{@office.id}/edit"
+  end
+
+  def they_are_redirected_to_the_dashboard
+    expect(page.current_path).to eql '/'
   end
 end
