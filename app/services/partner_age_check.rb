f44dfb5a7d5cac616e50c995739af67b3dd11862
Александар Симић <asimic@gmail.com>
2015-09-25 22:24:00 +0100

    Revealing the intention of PartnerAgeCheck
    Refactored it further to make the reading of the #verify method obvious
    what the class is meant to do.

--- app/services/partner_age_check.rb
+++ app/services/partner_age_check.rb
@@ -6,18 +6,28 @@ class PartnerAgeCheck
   end
 
   def verify
-    if @record.threshold_exceeded?
-      if married_and_under_61
-        [true, false].include?(@record.over_61)
-      else
-        true
-      end
-    end
+    process_marital_status_and_age if threshold_exceeded?
   end
 
   private
 
+  def threshold_exceeded?
+    @record.threshold_exceeded?
+  end
+
+  def process_marital_status_and_age
+    if married_and_under_61
+      only_boolean_values_present?
+    else
+      true
+    end
+  end
+
   def married_and_under_61
     @application.married? && @application.applicant_age < 61
+  end
+
+  def only_boolean_values_present?
+    [true, false].include?(@record.over_61)
   end
 end
