45b9e7f81d1f71437e9d1816754dace0cc33c300
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-09-28 17:17:35 +0100

    Improve the layout of the sql migration script

--- db/migrate/20150921084620_add_timestamps_to_benefit_check.rb
+++ db/migrate/20150921084620_add_timestamps_to_benefit_check.rb
@@ -1,14 +1,14 @@
 class AddTimestampsToBenefitCheck < ActiveRecord::Migration
   def up
     change_table(:benefit_checks) { |t| t.timestamps }
-    migrate_sql = <<SQL
-UPDATE benefit_checks
-SET
-  created_at = "applications"."created_at",
-  updated_at = "applications"."updated_at"
-FROM "applications"
-WHERE "applications"."id" = "benefit_checks"."application_id";
-SQL
+    migrate_sql = <<-SQL.gsub(/^\s+\|/, '')
+      |UPDATE benefit_checks
+      |SET
+      |  created_at = "applications"."created_at",
+      |  updated_at = "applications"."updated_at"
+      |FROM "applications"
+      |WHERE "applications"."id" = "benefit_checks"."application_id";
+    SQL
     execute(migrate_sql)
   end
 
