dc94523fb0bd9c538214ef85e4a73e4588afffb3
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-29 21:47:19 +0100

    Added more examples for income model
    Trying to cover all possible combinations of dependants and their number
    for validations.
    
    I've not added the proper locale message, which I plan on doing
    tomorrow, since I'm just keen on getting this model to pass the tests.

--- app/models/forms/income.rb
+++ app/models/forms/income.rb
@@ -11,5 +11,16 @@ module Forms
     define_attributes
 
     validates :dependents, inclusion: { in: [true, false] }
+    validates :children, numericality: { greater_than: 0, only_integer: true }, if: :dependents?
+    validate :number_of_children_when_no_dependents
+
+    private
+
+    def number_of_children_when_no_dependents
+      errors.add(
+        :children,
+        "you assign children if you don't have any dependants"
+      ) if !dependents && children > 0
+    end
   end
 end
--- spec/models/forms/income_spec.rb
+++ spec/models/forms/income_spec.rb
@@ -12,28 +12,64 @@ RSpec.describe Forms::Income do
   end
 
   describe 'validation' do
+    let(:income) { described_class.new(hash) }
+
     describe 'dependents' do
-      hash = {}
-      let(:income) { described_class.new(hash) }
+      let(:hash) { { dependents: dependents, children: 1 } }
 
       context 'when true' do
-        before { income[:dependents] = true }
+        let(:dependents) { true }
 
         it { expect(income.valid?).to be true }
       end
 
       context 'when false' do
-        before { income[:dependents] = false }
+        let(:dependents) { false }
 
-        it { expect(income.valid?).to be true }
+        it { expect(income.valid?).to be false }
       end
 
       context 'when not a boolean value' do
-        before { income[:dependents] = 'string' }
+        let(:dependents) { 'string' }
 
         it { expect(income.valid?).to be false }
       end
     end
 
+    describe 'children' do
+      let(:hash) { { dependents: dependents, children: children } }
+
+      context 'when there are dependents' do
+        let(:dependents) { true }
+
+        context 'and the number of children is valid' do
+          let(:children) { 1 }
+
+          it { expect(income.valid?).to be true }
+        end
+
+        context 'and the number of children is invalid' do
+          let(:children) { 0 }
+
+          it { expect(income.valid?).to be false }
+        end
+      end
+
+      context 'when there are no dependents' do
+        let(:dependents) { false }
+
+        context 'and the number of children is bigger than zero' do
+          let(:children) { 1 }
+
+          it { expect(income.valid?).to be false }
+        end
+
+        context 'and the number of children is zero' do
+          let(:children) { 0 }
+
+          it { expect(income.valid?).to be true }
+        end
+      end
+    end
   end
 end
