b82d8edc9eed85b92fa2e9312b9f234bbad8fa2f
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-09-30 16:44:00 +0100

    Add Email link to feedback display
    This will allow admins to contact users directly
    without having to search for users and copy/paste
    email addresses from the user table.

---
+++ spec/views/feedback/display.html.slim_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'feedback/index', type: :view do
+  include Devise::TestHelpers
+
+  let(:admin) { create :admin_user }
+
+  before do
+    assign(:feedback, create_list(:feedback, 2, user: admin, office: admin.office))
+    sign_in admin
+    render
+  end
+
+  it 'identifys the user' do
+    expect(rendered).to have_xpath("//td[contains(., '#{admin.name}')]")
+  end
+
+  it 'provides the users email' do
+    expect(rendered).to have_xpath("//a[@href='mailto:#{admin.email}']")
+  end
+
+end
