d48594be9c24ca03cdfe61d46341bcdb0b300096
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-09-30 12:08:28 +0100

    Cleaner user of error message for locale
    I've copied the way the locale was done in the previous approach in the
    monolithic Application model. Using it this way is much cleaner as it
    takes less screen real estate and it also happens to be the proper
    'Rails way' of doing localization error messages.

--- app/models/forms/income.rb
+++ app/models/forms/income.rb
@@ -19,8 +19,12 @@ module Forms
     def number_of_children_when_no_dependents
       errors.add(
         :children,
-        t('activemodel.errors.models.forms/income.attributes.children.cant_have_children_assigned')
-      ) if !dependents && children.to_i > 0
+        :cant_have_children_assigned
+      ) if children_declared_but_dependents_arent?
+    end
+
+    def children_declared_but_dependents_arent?
+      !dependents && children.to_i > 0
     end
   end
 end
