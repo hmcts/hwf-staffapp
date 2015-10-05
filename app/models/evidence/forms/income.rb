8f84b0b5097828165e934c850e4e3f836efa9696
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-05 17:33:58 +0100

    Renamed attribute to amount
    Seeing as the model is already called Income it made it clear what is
    being used, tested and expected from the user, so renamed it to amount.

--- app/models/evidence/forms/income.rb
+++ app/models/evidence/forms/income.rb
@@ -4,14 +4,14 @@ module Evidence
 
       def self.permitted_attributes
         {
-          income: String
+          amount: String
         }
       end
 
       define_attributes
 
-      validates :income, presence: true
-      validates :income, numericality: { greater_than_or_equal_to: 0 }
+      validates :amount, presence: true
+      validates :amount, numericality: { greater_than_or_equal_to: 0 }
     end
   end
 end
--- spec/models/evidence/forms/income_spec.rb
+++ spec/models/evidence/forms/income_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Evidence::Forms::Income do
-  params_list = %i[income]
+  params_list = %i[amount]
 
-  let(:income) { { income: '500' } }
+  let(:income) { { amount: '500' } }
   subject { described_class.new(income) }
 
   describe '.permitted_attributes' do
@@ -13,22 +13,22 @@ RSpec.describe Evidence::Forms::Income do
   end
 
   describe 'validation' do
-    it { is_expected.to validate_presence_of(:income) }
+    it { is_expected.to validate_presence_of(:amount) }
 
     context 'when the income is 0' do
-      let(:income) { { income: '0' } }
+      let(:income) { { amount: '0' } }
 
       it { expect(subject.valid?).to be true }
     end
 
     context 'when the income is negative' do
-      let(:income) { { income: '-1' } }
+      let(:income) { { amount: '-1' } }
 
       it { expect(subject.valid?).to be false }
     end
 
     context 'when the income is string' do
-      let(:income) { { income: 'a string' } }
+      let(:income) { { amount: 'a string' } }
 
       it { expect(subject.valid?).to be false }
     end
