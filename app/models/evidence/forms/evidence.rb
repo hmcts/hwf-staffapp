261e54e1cbcd7a75c75a87f82c1627908b745ed2
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-05 15:45:46 +0100

    Added validation for reason on Evidence model
    Prevent users from submitting the reasons for invalid evidence when the
    evidence is correct.
    
    Added it to prevent any potentially corrupt data.

--- app/models/evidence/forms/evidence.rb
+++ app/models/evidence/forms/evidence.rb
@@ -4,14 +4,27 @@ module Evidence
 
       def self.permitted_attributes
         {
-          correct: Boolean
+          correct: Boolean,
+          reason: String
         }
       end
 
       define_attributes
 
       validates :correct, inclusion: { in: [true, false] }
+      validate :no_reason_when_correct
 
+      private
+
+      def no_reason_when_correct
+        if evidence_correct
+          errors.add(:reason) unless reason.blank?
+        end
+      end
+
+      def evidence_correct
+        correct.equal?(true)
+      end
     end
   end
 end
--- spec/models/evidence/forms/evidence_spec.rb
+++ spec/models/evidence/forms/evidence_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Evidence::Forms::Evidence do
-  params_list = %i[correct]
+  params_list = %i[correct reason]
 
   let(:evidence) { { correct: true } }
   subject { described_class.new(evidence) }
@@ -12,21 +12,27 @@ RSpec.describe Evidence::Forms::Evidence do
     end
   end
 
-  describe 'correct' do
+  describe 'validations' do
     context 'when true' do
-      let(:evidence) { { correct: true }}
+      let(:evidence) { { correct: true } }
 
       it { expect(subject.valid?).to be true }
+
+      describe 'the reason' do
+        let(:evidence) { { correct: true, reason: 'some reason' } }
+
+        it { expect(subject.valid?).to be false }
+      end
     end
 
     context 'when false' do
-      let(:evidence) { { correct: false }}
+      let(:evidence) { { correct: false } }
 
       it { expect(subject.valid?).to be true }
     end
 
     context 'when not a boolean value' do
-      let(:evidence) { { correct: 'some string' }}
+      let(:evidence) { { correct: 'some string' } }
 
       it { expect(subject.valid?).to be false }
     end
