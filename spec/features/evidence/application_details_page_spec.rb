197a23abe03cb795ac6bb94cebd18c7bf76f27a0
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-06 14:54:27 +0100

    Adde application details section
    Added application details section to application details page.
    
    Removed the view specst for the page and used the features specs as they
    were easier to set up than to extend the view specs.
    
    Added the application details attributes to the view object.

---
+++ spec/features/evidence/application_details_page_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature 'Application details page', type: :feature do
+
+  include Warden::Test::Helpers
+  Warden.test_mode!
+
+  let(:user) { create :user }
+  let(:application) { create :application_full_remission, user: user }
+  let!(:evidence) { create :evidence_check, application_id: application.id }
+  headings = ['Waiting for evidence', 'Process evidence', 'Processing details', 'Personal details', 'Application details']
+
+  before do
+    login_as user
+    visit evidence_path(id: evidence.id)
+  end
+
+  headings.each do |heading_title|
+    it "has a heading titled #{heading_title}" do
+      expect(page).to have_content heading_title
+    end
+  end
+end
--- spec/models/evidence/views/overview_spec.rb
+++ spec/models/evidence/views/overview_spec.rb
@@ -1,13 +1,16 @@
+# coding: utf-8
 require 'rails_helper'
 
 RSpec.describe Evidence::Views::Overview do
 
   let(:user) { create(:user) }
-  let(:application) { create(:application, user: user, office: user.office) }
+  let(:fee_amount) { '100' }
+  let(:application) { create(:application, user: user, office: user.office, fee: fee_amount) }
   let(:evidence) { application.build_evidence_check(expires_at: expiration_date) }
-  let(:overview) { Evidence::Views::Overview.new(evidence) }
+  let(:overview) { described_class.new(evidence) }
+  symbols = %i[reference processed_by expires date_of_birth full_name ni_number status fee jurisdiction date_received form_name]
 
-  %i[reference processed_by expires date_of_birth full_name ni_number status].each do |symbol|
+  symbols.each do |symbol|
     let(:expiration_date) { Time.zone.today + 14.days }
     it 'has the attribute #{symbol} for "Processing details" section' do
       expect(overview.methods).to include(symbol)
@@ -34,4 +37,25 @@ RSpec.describe Evidence::Views::Overview do
     end
   end
 
+  describe '#jurisdiction' do
+    it { expect(overview.jurisdiction).to eq application.jurisdiction.name }
+  end
+
+  describe '#fee' do
+    context 'rounds down' do
+      let(:fee_amount) { '100.49'}
+
+      it 'formats the fee amount correctly' do
+        expect(overview.fee).to eq 100
+      end
+    end
+
+    context 'when its under £1' do
+      let(:fee_amount) { '0.49'}
+
+      it 'formats the fee amount correctly' do
+        expect(overview.fee).to eq 0
+      end
+    end
+  end
 end
