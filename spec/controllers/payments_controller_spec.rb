e812f1b4468d0a3104d8c3127c212a25060e4f59
Jakub Novotny <novotny.jakub@gmail.com>
2015-10-15 16:21:56 +0100

    Let accuracy flow to PaymentsController
    Feature tests missing for the moment.

--- spec/controllers/payments_controller_spec.rb
+++ spec/controllers/payments_controller_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe PaymentsController, type: :controller do
   let(:processing_details) { double }
   let(:application_overview) { double }
   let(:application_result) { double }
+  let(:accuracy_form) { double }
 
   before do
     allow(Payment).to receive(:find).with(payment.id.to_s).and_return(payment)
     allow(Views::ProcessingDetails).to receive(:new).with(payment).and_return(processing_details)
     allow(Views::ApplicationOverview).to receive(:new).with(payment.application).and_return(application_overview)
     allow(Views::ApplicationResult).to receive(:new).with(payment.application).and_return(application_result)
+    allow(Forms::Accuracy).to receive(:new).with(payment).and_return(accuracy_form)
   end
 
   describe 'GET #show' do
@@ -26,7 +28,7 @@ RSpec.describe PaymentsController, type: :controller do
     end
 
     it 'renders the correct template' do
-      expect(response).to render_template('show')
+      expect(response).to render_template(:show)
     end
 
     it 'assigns the view models' do
@@ -36,4 +38,64 @@ RSpec.describe PaymentsController, type: :controller do
     end
   end
 
+  describe 'GET #accuracy' do
+    before do
+      get :accuracy, id: payment.id
+    end
+
+    it 'returns the correct status code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template(:accuracy)
+    end
+
+    it 'assigns the form object' do
+      expect(assigns(:form)).to eql(accuracy_form)
+    end
+  end
+
+  describe 'POST #accuracy_save', focus: true do
+    let(:expected_form_params) { { correct: true, incorrect_reason: 'reason' } }
+
+    before do
+      allow(accuracy_form).to receive(:update_attributes).with(expected_form_params)
+      allow(accuracy_form).to receive(:save).and_return(form_save)
+
+      post :accuracy_save, id: payment.id, payment: expected_form_params
+    end
+
+    context 'when the form can be saved' do
+      let(:form_save) { true }
+
+      it 'redirects to the summary page' do
+        expect(response).to redirect_to(summary_payment_path(payment))
+      end
+    end
+
+    context 'when the form can not be saved' do
+      let(:form_save) { false }
+
+      it 'assigns the form' do
+        expect(assigns(:form)).to eql(accuracy_form)
+      end
+
+      it 'renders the accuracy template again' do
+        expect(response).to render_template(:accuracy)
+      end
+    end
+  end
+
+  describe 'GET #summary' do
+    before { get :summary, id: payment }
+
+    it 'returns the correct status code' do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the correct template' do
+      expect(response).to render_template :summary
+    end
+  end
 end
--- spec/routing/payment_routing_spec.rb
+++ spec/routing/payment_routing_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe PaymentsController, type: :routing do
     it 'routes to #show' do
       expect(get: '/payments/1').to route_to('payments#show', id: '1')
     end
+
+    it 'routes to #accuracy' do
+      expect(get: '/payments/1/accuracy').to route_to('payments#accuracy', id: '1')
+    end
+
+    it 'routes to #accuracy_save' do
+      expect(post: '/payments/1/accuracy_save').to route_to('payments#accuracy_save', id: '1')
+    end
+
+    it 'routes to #summary' do
+      expect(get: '/payments/1/summary').to route_to('payments#summary', id: '1')
+    end
   end
 end
