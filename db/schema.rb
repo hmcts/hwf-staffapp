82e48e90850fc17b71b58f53bd70d724ef5e8fab
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-19 11:54:35 +0100

    Renamed the migration
    In order to make sure that it does get picked up for sure and not clash
    with any of the previous PRs.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016153328) do
+ActiveRecord::Schema.define(version: 20151018115056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
