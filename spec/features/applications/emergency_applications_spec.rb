f86365710f91a8662407662c5d78073190d1f6ab
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-19 13:46:09 +0100

    Added the display of emergency reason on summary
    Implemented showing of the emergency reason on the application summary
    page.
    
    Using the activerecord path to the locale as the field is retreived from
    the activerecord instance. This will be changed once the Application
    model is broken down into smaller models.

--- spec/features/applications/emergency_applications_spec.rb
+++ spec/features/applications/emergency_applications_spec.rb
@@ -8,6 +8,7 @@ RSpec.feature 'Emergency application', type: :feature do
   let!(:jurisdictions) { create_list :jurisdiction, 3 }
   let!(:office)        { create(:office, jurisdictions: jurisdictions) }
   let!(:user)          { create(:user, jurisdiction_id: jurisdictions[1].id, office: office) }
+  let(:reason)         { 'A really good reason' }
 
   before do
     login_as user
@@ -27,13 +28,25 @@ RSpec.feature 'Emergency application', type: :feature do
 
     context 'when the emergency case option is chosen' do
       before { check 'application_emergency' }
-      reason = 'A really good reason'
       label_text = 'Reason for emergency'
 
       scenario 'there will be a way to add the explanation for emergency reason' do
         expect(page).to have_content label_text
         fill_in 'application_emergency_reason', with: reason
         expect(page).to have_field('Reason for emergency', with: reason)
+      end
+    end
+  end
+
+  context 'when on application summary page' do
+    let(:application) { create :application_full_remission, emergency_reason: reason }
+
+    before { visit application_build_path(application_id: application.id, id: 'summary') }
+
+    scenario 'there will be the reason for emergency application' do
+      ['Application details',
+       reason].each do |content|
+        expect(page).to have_content content
       end
     end
   end
