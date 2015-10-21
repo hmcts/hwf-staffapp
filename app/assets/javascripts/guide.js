7bf056947b8b88b24e1d03145303ec1f9cdcad61
Fazal Khan <fazal.khan@digital.justice.gov.uk>
2015-10-21 14:04:51 +0100

    minor fixes as per kellies amends

--- app/assets/javascripts/guide.js
+++ app/assets/javascripts/guide.js
@@ -1,19 +1,22 @@
 var FR = {};
 
 FR.equalHeightColumns = function() {
-  var cols = $('.guide-cols');
+  var wrapper = $('.guide-cols-container');
+  var cols = wrapper.find('.guide-cols');
   var max = 0;
+  var arr = [];
 
   cols.each(function(i, el) {
-    var height = Math.max($(el).height());
+    var height = Math.max($(el).outerHeight());
     if(height >= max) {
       max = height;
     }
   });
+  cols.height(max);
 };
 
 $(function(){
-  if($('.guide-cols'.length)){
+  if($('.guide-cols-container'.length)){
     FR.equalHeightColumns();
   }
 });
