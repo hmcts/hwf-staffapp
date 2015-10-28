e35b15389988f371ed4d91ff42aefd78ce33a5a9
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-10-28 16:07:54 +0000

    Going for 100% coverage
    Added this in order to satisfy Code Climate's code coverage metrics.
    
    Used a class because it was easier to test it that way.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,7 @@
-FrStaffapp::Application.config.secret_key_base =
-  if Rails.env.production?
-    ENV['SECRET_TOKEN']
-  else
-    ('a' * 30)
+class SecretToken
+  def self.generate
+    Rails.env.production? ? ENV['SECRET_TOKEN'] : ('a' * 30)
   end
+end
+
+FrStaffapp::Application.config.secret_key_base = SecretToken.generate
