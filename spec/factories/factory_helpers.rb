86ae0af0b7f2988e1d00946225f033a75dd0fbff
Jakub Novotny <novotny.jakub@gmail.com>
2015-11-06 13:09:32 +0000

    Cleanup factories and improve stubbing
    This may not seem as an improvent, but it allows us to build stubbed
    Application with Applicant / Detail. Before it was possible but without
    being able to inject any of those when stubbing.

---
+++ spec/factories/factory_helpers.rb
@@ -0,0 +1,23 @@
+def around_stub(obj)
+  id = obj.id
+  obj.id = nil
+  yield
+  obj.id = id
+end
+
+def build_related_for_application(scope, method, application, evaluator)
+  application.applicant ||= scope.send(method,
+    evaluator.applicant_factory, *evaluator.applicant_traits,
+    application: application, ni_number: evaluator.ni_number)
+
+  application.detail ||= begin
+    overrides = { application: application }
+    %i[fee date_received refund date_fee_paid probate jurisdiction emergency_reason].each do |field|
+      value = evaluator.send(field)
+      overrides[field] = value if value.present?
+    end
+
+    scope.send(method, evaluator.detail_factory,
+      *evaluator.detail_traits, overrides)
+  end
+end
