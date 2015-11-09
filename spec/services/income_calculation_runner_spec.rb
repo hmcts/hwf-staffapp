46251e17d2021896736df70ecbbe14559817a79a
Jakub Novotny <novotny.jakub@gmail.com>
2015-11-09 01:14:56 +0000

    Add service to wrap income calculation

---
+++ spec/services/income_calculation_runner_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe IncomeCalculationRunner do
+  let(:application) { create :application, application_type: nil, application_outcome: nil }
+
+  subject(:runner) { described_class.new(application) }
+
+  describe '#run' do
+    let(:calculation) { double(calculate: result) }
+
+    before do
+      allow(IncomeCalculation).to receive(:new).with(application).and_return(calculation)
+
+      runner.run
+
+      application.reload
+    end
+
+    context 'when result is not nil' do
+      let(:result) { { outcome: 'part', amount: 100 } }
+
+      it 'sets application type to income' do
+        expect(application.application_type).to eql('income')
+      end
+
+      it 'sets application outcome as per result' do
+        expect(application.application_outcome).to eql('part')
+      end
+
+      it 'sets amount_to_pay as per result' do
+        expect(application.amount_to_pay).to eq(100)
+      end
+    end
+
+    context 'when result is nil' do
+      let(:result) { nil }
+
+      it 'does not set application type' do
+        expect(application.application_type).to be nil
+      end
+
+      it 'does not set application outcome' do
+        expect(application.application_outcome).to be nil
+      end
+    end
+
+  end
+end
