b82872f9483befce69af2cd55f218525c12851bc
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2015-11-16 12:58:42 +0000

    Fix for all the applications without outcome
    Fix for all the applications that should have their outcome overridden
    by BenefitOverride, but didn't.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112110146) do
+ActiveRecord::Schema.define(version: 20151116125301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
