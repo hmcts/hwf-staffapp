45a81db1ea3d4e0874b86927560a4fe70764c4b1
Jakub Novotny <novotny.jakub@gmail.com>
2015-12-03 17:25:55 +0000

    Add feature test for removing application

--- spec/features/processed_applications/list_and_delete_processed_applications_spec.rb
+++ spec/features/processed_applications/list_and_delete_processed_applications_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature 'User can access processed applications,', type: :feature do
+RSpec.feature 'List and delete processed applications', type: :feature do
   include Warden::Test::Helpers
   Warden.test_mode!
 
@@ -40,5 +40,35 @@ RSpec.feature 'User can access processed applications,', type: :feature do
 
     expect(page).to have_content('Processed application')
     expect(page).to have_content("Full name#{application1.applicant.full_name}")
+  end
+
+  describe 'User removes application' do
+    before do
+      visit '/processed_applications'
+
+      click_link application1.applicant.full_name
+    end
+
+    scenario 'With reason provided the application is removed and does not show in the list' do
+      fill_in 'application_removed_reason', with: 'Reason'
+      click_button 'Remove application'
+
+      expect(page).to have_content('Processed applications')
+      expect(page).to have_content('The application has been removed')
+      within 'table.processed-applications tbody' do
+        expect(page).to have_no_content(application1.applicant.full_name)
+        expect(page).to have_content(application2.applicant.full_name)
+      end
+    end
+
+    scenario 'With reason not provided the application shows an error' do
+      click_button 'Remove application'
+
+      expect(page).to have_content('Processed application')
+      expect(page).to have_content("Full name#{application1.applicant.full_name}")
+      within '.remove-form' do
+        expect(page).to have_content('can\'t be blank')
+      end
+    end
   end
 end
