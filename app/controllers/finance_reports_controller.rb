90bf535e6b0360b3d82cc093c3705c10071d4c72
colinbruce <colin.bruce@digital.justice.gov.uk>
2015-12-06 19:08:16 +0000

    Updated finance reports controller
    Set respond_to to always, and only, respond to xlsx

--- app/controllers/finance_reports_controller.rb
+++ app/controllers/finance_reports_controller.rb
@@ -1,20 +1,15 @@
 class FinanceReportsController < ApplicationController
+  respond_to :xlsx
 
   def index
   end
 
   def output_to_excel
-    respond_to do |format|
-      format.xlsx
-    end
   end
 
   def processed_applications
     @applications = Query::ProcessedApplications.new(current_user).find.map do |application|
       Views::ApplicationList.new(application)
-    end
-    respond_to do |format|
-      format.xlsx
     end
   end
 end
