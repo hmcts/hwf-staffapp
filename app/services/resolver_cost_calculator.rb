7495b30f3f43d04f18b792dfbb20581cadef0be7
Jakub Novotny <novotny.jakub@gmail.com>
2015-12-17 08:53:02 +0000

    Refactor ResolverCostCalculator

--- app/services/resolver_cost_calculator.rb
+++ app/services/resolver_cost_calculator.rb
@@ -1,35 +1,32 @@
 class ResolverCostCalculator
   def initialize(source)
     @source = source
+    @application = @source.is_a?(Application) ? @source : @source.application
   end
 
   def cost
-    @source.outcome == 'none' ? 0 : incurred_cost(@source)
+    @source.outcome == 'none' ? 0 : incurred_cost
   end
 
   private
 
-  def incurred_cost(source)
-    if source.is_a?(PartPayment)
-      if source.application.evidence_check.present?
-        fee(source) - amount_to_pay(source.application.evidence_check)
-      else
-        fee(source) - amount_to_pay(source.application)
-      end
+  def incurred_cost
+    if @source.is_a?(PartPayment)
+      fee - amount_to_pay
     else
-      fee(source)
+      fee
     end
   end
 
-  def fee(source)
-    if source.is_a?(Application)
-      source.detail.fee
-    else
-      source.application.detail.fee
-    end
+  def fee
+    @application.detail.fee
   end
 
-  def amount_to_pay(source)
-    source.try(:amount_to_pay) || 0
+  def amount_to_pay
+    if @application.evidence_check.present?
+      @application.evidence_check.try(:amount_to_pay) || 0
+    else
+      @application.try(:amount_to_pay) || 0
+    end
   end
 end
