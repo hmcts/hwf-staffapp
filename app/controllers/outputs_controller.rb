c9abcf8b7fe0dc0df39b7cb433cee556071f3ad2
ColinBruce <colin.bruce@digital.justice.gov.uk>
2015-12-18 15:48:01 +0000

    Create CSV route for the output controller

--- app/controllers/outputs_controller.rb
+++ app/controllers/outputs_controller.rb
@@ -9,4 +9,18 @@ class OutputsController < ApplicationController
     authorize! :access, :outputs
     @form = Forms::FinanceReport.new
   end
+
+  def finance_report_generator
+    authorize! :access, :outputs
+    @date_from = Date.parse(report_params[:date_from])
+    @date_to = Date.parse(report_params[:date_to])
+    @data = FinanceReportBuilder.new(report_params[:date_from], report_params[:date_to])
+    send_data @data.to_csv, filename: "finance-report-#{@date_from}-#{@date_to}.csv"
+  end
+
+  private
+
+  def report_params
+    params.require(:forms_finance_report).permit(:date_from, :date_to)
+  end
 end
