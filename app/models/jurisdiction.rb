1d51947bdb2917b855297cc08ac55d0a4db63f77
Jakub Novotny <novotny.jakub@gmail.com>
2016-01-14 20:05:37 +0000

    Add available_for_office scope to Jurisdiction
    The scope only returns jurisdictions, which have a BusinessEntity
    created for the same combination of Office + Jurisdiction.

--- app/models/jurisdiction.rb
+++ app/models/jurisdiction.rb
@@ -2,9 +2,14 @@ class Jurisdiction < ActiveRecord::Base
 
   has_many :office_jurisdictions
   has_many :offices, through: :office_jurisdictions
+  has_many :business_entities
 
   validates :name, uniqueness: true, presence: true
   validates :abbr, uniqueness: { allow_nil: true }
+
+  scope :available_for_office, lambda { |office|
+    joins(:business_entities).where(business_entities: { office: office })
+  }
 
   def display
     self.abbr ||= name
