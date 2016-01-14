4a931a999a57fdd787dce8fee828ff045a41399b
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-01-14 14:54:15 +0000

    Update business controller and routing

--- spec/routing/business_entities_routing_spec.rb
+++ spec/routing/business_entities_routing_spec.rb
@@ -10,5 +10,9 @@ RSpec.describe BusinessEntitiesController, type: :routing do
     it 'routes to #new' do
       expect(get: '/offices/1/business_entities/new').to route_to('business_entities#new', office_id: '1')
     end
+
+    it 'routes to #create' do
+      expect(post: '/offices/1/business_entities').to route_to('business_entities#create', office_id: '1')
+    end
   end
 end
