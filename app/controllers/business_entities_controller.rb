1312185e85705216cd8a7b5ca55c89ec938dc7e8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-01-14 14:54:15 +0000

    Update business controller and routing

--- app/controllers/business_entities_controller.rb
+++ app/controllers/business_entities_controller.rb
@@ -1,5 +1,5 @@
+# rubocop:disable ClassLength
 class BusinessEntitiesController < ApplicationController
-
   def index
     authorize :business_entity
     office
@@ -7,13 +7,32 @@ class BusinessEntitiesController < ApplicationController
   end
 
   def new
+    redirect_to office_business_entities_path unless jurisdiction
+    office
     @business_entity = BusinessEntity.new
     authorize @business_entity
+  end
+
+  def create
+    @business_entity = BusinessEntity.new(add_valid_from_param(Time.zone.now).merge(create_params))
+    authorize @business_entity
+    if @business_entity.save
+      redirect_to office_business_entities_path
+    else
+      office
+      jurisdiction
+      render :new
+    end
+  end
+
+  def create_params
+    { office_id: params[:office_id], jurisdiction_id: params[:jurisdiction_id] }
   end
 
   def edit
     authorize business_entity
     business_entity
+    office
   end
 
   def update
@@ -35,6 +54,16 @@ class BusinessEntitiesController < ApplicationController
     @business_entity ||= BusinessEntity.find(params[:id])
   end
 
+  def jurisdiction
+    if params[:jurisdiction_id].nil?
+      flash[:alert] = 'Please select a jurisdiction to add'
+      false
+    else
+      @jurisdiction ||= Jurisdiction.find(params[:jurisdiction_id])
+      true
+    end
+  end
+
   def business_entity_update
     change_time = Time.zone.now
     new_be = BusinessEntity.new(build_new_params(change_time))
@@ -49,22 +78,26 @@ class BusinessEntitiesController < ApplicationController
   end
 
   def build_new_params(change_time)
-    business_entity.attributes.merge(business_entity_params.merge(id: nil, valid_from: change_time))
+    business_entity.attributes.merge(add_valid_from_param(change_time))
+  end
+
+  def add_valid_from_param(change_time)
+    business_entity_params.merge(id: nil, valid_from: change_time)
   end
 
   def list_jurisdictions
-    @jurisdictions = Jurisdiction.joins(join_command).order(order_sequence).pluck(return_fields)
+    @jurisdictions = Jurisdiction.joins(join(@office.id)).order(order_sequence).pluck(return_fields)
   end
 
   def order_sequence
     'business_entities.code IS NOT NULL DESC, jurisdictions.id'
   end
 
-  def join_command
+  def join(office_id)
     <<-JOIN
       LEFT OUTER JOIN business_entities
         ON business_entities.jurisdiction_id = jurisdictions.id
-        AND business_entities.office_id = 1
+        AND business_entities.office_id = #{office_id}
         AND business_entities.valid_to IS NULL
       LEFT OUTER JOIN office_jurisdictions
         ON business_entities.jurisdiction_id = office_jurisdictions.jurisdiction_id
@@ -80,7 +113,7 @@ class BusinessEntitiesController < ApplicationController
       'business_entities.id',
       'business_entities.code',
       'business_entities.name',
-      "CASE WHEN office_jurisdictions.office_id IS NOT NULL THEN NULL
+      "CASE WHEN office_jurisdictions.office_id IS NULL THEN 'delete'
        ELSE CASE WHEN business_entities.code IS NOT NULL THEN 'edit' ELSE 'new' END
        END AS state"
     ].join(',')
