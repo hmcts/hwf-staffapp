88c679ae121122c891aad8c8fda546e14ea1b28c
Aleksandar Simić <alex.simic@digital.justice.gov.uk>
2016-01-15 10:33:19 +0000

    Allow users to edit their own passwords
    Since the introduction of Pundit, the users were unable to change their
    own passwords. This change allows them to do that again.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,12 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
+
+    before_action :authorize_for_current_user
+
+    def edit
+      super
+    end
+
     def update
       if account_update_params[:password].present?
         super
@@ -8,6 +15,16 @@ module Users
         resource.errors.add(:password, error_message)
         respond_with resource
       end
+    end
+
+    private
+
+    def their_own?
+      current_user.id.eql?(params[:id].to_i)
+    end
+
+    def authorize_for_current_user
+      authorize current_user if their_own?
     end
   end
 end
