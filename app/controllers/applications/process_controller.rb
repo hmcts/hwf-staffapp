3f175a19e403a8086dda271956dee5051f0169dd
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-02-11 15:31:18 +0000

    Fix redirect after completion
    Because the pages were stored in the browser history the users could
    still use the browser back button.
    
    This was designed to set the application processed page to expire and
    force a reload/redirect if trying to view in the same browser session.

--- app/controllers/applications/process_controller.rb
+++ app/controllers/applications/process_controller.rb
@@ -128,7 +128,14 @@ module Applications
     end
 
     def check_completed_redirect
+      set_cache_headers
       redirect_to CompletedApplicationRedirect.new(application).path unless application.created?
+    end
+
+    def set_cache_headers
+      response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+      response.headers['Pragma'] = 'no-cache'
+      response.headers['Expires'] = 3.hours.ago.to_formatted_s(:rfc822)
     end
 
     def form_params(type)
