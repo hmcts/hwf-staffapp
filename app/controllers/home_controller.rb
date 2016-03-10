2ce1b72157ff6f1498c52b464c6db69798200669
Jakub Novotny <novotny.jakub@gmail.com>
2016-03-10 11:44:14 +0000

    Create a basic search method on HomeController
    It's just an early version so frontend can start being integrated.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,18 @@ class HomeController < ApplicationController
     load_waiting_applications
   end
 
+  def search
+    @search_form = Forms::Search.new(search_params)
+
+    if @search_form.valid? && @search_form.reference == 'exists'
+      flash[:notice] = 'Online submission found'
+      redirect_to(home_index_path)
+    else
+      load_waiting_applications
+      render :index
+    end
+  end
+
   private
 
   def manager_setup_progress
@@ -46,5 +58,9 @@ class HomeController < ApplicationController
 
   def waiting_for_part_payment
     Query::WaitingForPartPayment.new(current_user).find
+  end
+
+  def search_params
+    params.permit(:reference)
   end
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe HomeController, type: :controller do
 
   include Devise::TestHelpers
 
-  describe 'GET #index' do
-    let(:user)    { create :user }
-    let(:manager) { create :manager }
-    let(:admin)   { create :admin_user }
+  let(:staff)   { create :staff }
+  let(:manager) { create :manager }
+  let(:admin)   { create :admin_user }
 
+  describe 'GET #index' do
     context 'when the user is authenticated' do
       context 'as a user' do
 
         before(:each) do
-          sign_in user
+          sign_in staff
           get :index
         end
 
@@ -58,13 +58,70 @@ RSpec.describe HomeController, type: :controller do
     context 'when the user is not authenticated' do
 
       before(:each) do
-        sign_out user
+        sign_out staff
         get :index
       end
 
       it 'redirects to sign in page' do
         expect(response).to redirect_to(:new_user_session)
       end
+    end
+  end
+
+  describe 'POST #search' do
+    before do
+      sign_in(user)
+      post :search, search_params
+    end
+
+    context 'as a staff' do
+      let(:user) { staff }
+
+      context 'when reference parameter is missing' do
+        let(:search_params) { {} }
+
+        it 'renders the index template' do
+          expect(response).to render_template(:index)
+        end
+
+        it 'assigns the search form' do
+          expect(assigns(:search_form)).to be_a(Forms::Search)
+        end
+      end
+
+      context 'when reference parameter is present' do
+        let(:search_params) { { reference: reference } }
+
+        context 'when no online submission is found with that reference' do
+          let(:reference) { 'wrong' }
+
+          it 'renders the index template' do
+            expect(response).to render_template(:index)
+          end
+
+          it 'assigns the search form' do
+            expect(assigns(:search_form)).to be_a(Forms::Search)
+          end
+        end
+
+        context 'when an online submission is found with that reference' do
+          let(:reference) { 'exists' }
+
+          it 'redirects to the homepage action' do
+            expect(response).to redirect_to(home_index_path)
+          end
+
+          it 'sets success flash message' do
+            expect(flash[:notice]).to eql('Online submission found')
+          end
+        end
+      end
+    end
+
+    context 'as a manager' do
+    end
+
+    context 'as an admin' do
     end
   end
 end
