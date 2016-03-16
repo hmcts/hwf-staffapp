f9c4646e6998d72557ee3e5c2fdd20cbde0b31d8
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-03-16 13:30:41 +0000

    Update online_application builder tests

--- spec/builders/online_application_builder_spec.rb
+++ spec/builders/online_application_builder_spec.rb
@@ -13,31 +13,5 @@ describe OnlineApplicationBuilder do
       is_expected.to be_a(OnlineApplication)
       is_expected.not_to be_persisted
     end
-
-    describe 'email contact' do
-      subject(:email) { build_submission.email_address }
-      describe 'when applicant responds `no`' do
-        it { is_expected.to eql nil }
-      end
-
-      describe 'when applicant responds `no`' do
-        let(:submission) { attributes_for :public_app_submission, :email_contact }
-
-        it { is_expected.to eql 'foo@bar.com' }
-      end
-    end
-
-    describe 'children field' do
-      subject(:children) { build_submission.children }
-      describe 'when applicant responds `no`' do
-        it { is_expected.to eql 0 }
-      end
-
-      describe 'when applicant responds `yes`' do
-        let(:submission) { attributes_for :public_app_submission, :with_children }
-
-        it { is_expected.to eql 1 }
-      end
-    end
   end
 end
