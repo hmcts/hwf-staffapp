a840e5a4cce60f073da5b6c868c2a55141b8d134
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-03-31 19:20:07 +0100

    refactor standard attachments

--- app/mailers/online_mailer.rb
+++ app/mailers/online_mailer.rb
@@ -1,14 +1,18 @@
 class OnlineMailer < ApplicationMailer
 
   def confirmation(application)
-    attachments.inline['icon-important.png'] = File.read('app/assets/images/icon-important.png')
-    attachments.inline['crest.png'] = File.read('app/assets/images/crest.png')
-    attachments.inline['moj_logo.png'] = File.read('app/assets/images/moj_logotype_email.png')
+    standard_attachments
     @application = application
     mail_with_subject @application.email_address, 'Help with Fees confirmation'
   end
 
   private
+
+  def standard_attachments
+    attachments.inline['icon-important.png'] = File.read('app/assets/images/icon-important.png')
+    attachments.inline['crest.png'] = File.read('app/assets/images/crest.png')
+    attachments.inline['moj_logo.png'] = File.read('app/assets/images/moj_logotype_email.png')
+  end
 
   def mail_with_subject(to, subject)
     mail to: to, subject: subject
