56bedba2c10126f6f6494a489d2d28b2f553b5c8
axemonkey <hi+github@clivemurray.com>
2016-04-01 12:11:12 +0100

    nest those tables, oh boy

--- app/mailers/online_mailer.rb
+++ app/mailers/online_mailer.rb
@@ -3,7 +3,7 @@ class OnlineMailer < ApplicationMailer
   def confirmation(application)
     attachments.inline['icon-important.png'] = File.read('app/assets/images/icon-important.png')
     attachments.inline['crest.png'] = File.read('app/assets/images/crest.png')
-    attachments.inline['moj_logo.png'] = File.read('app/assets/images/moj_logo_small_png24.png')
+    attachments.inline['moj_logo.png'] = File.read('app/assets/images/moj_logotype_email.png')
     @application = application
     mail_with_subject @application.email_address, 'Help with Fees confirmation'
   end
