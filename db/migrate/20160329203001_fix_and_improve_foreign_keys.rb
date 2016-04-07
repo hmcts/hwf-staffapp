6a1e6f3d6abca720f727590cb638acff84ba2bb2
Jakub Novotny <novotny.jakub@gmail.com>
2016-04-07 14:41:46 +0100

    Merge the 2 foreign keys migrations to 1

--- db/migrate/20160329203001_fix_and_improve_foreign_keys.rb
+++ db/migrate/20160329203001_fix_and_improve_foreign_keys.rb
@@ -29,6 +29,20 @@ class FixAndImproveForeignKeys < ActiveRecord::Migration
 
     change_foreign_key :users, :jurisdictions
     change_foreign_key :users, :offices
+
+    # some extra foreign references to the users table
+
+    add_user_foreign_key(:applications, :completed_by_id)
+    add_user_foreign_key(:applications, :deleted_by_id)
+
+    add_user_foreign_key(:benefit_overrides, :completed_by_id)
+
+    add_user_foreign_key(:evidence_checks, :completed_by_id)
+
+    add_user_foreign_key(:part_payments, :completed_by_id)
+
+    # This foreign key can't be setup now as it prevents the primary key to be incremented
+    # add_user_foreign_key(:users, :invited_by_id)
   end
 
   def change_foreign_key(from_table, to_table)
@@ -43,5 +57,9 @@ class FixAndImproveForeignKeys < ActiveRecord::Migration
         add_foreign_key from_table, to_table
       end
     end
+  end
+
+  def add_user_foreign_key(from_table, column)
+    add_foreign_key from_table, :users, column: column, on_update: :cascade
   end
 end
