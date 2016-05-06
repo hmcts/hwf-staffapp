5f1b0b6b8d9211254ab37fba908b3bb5d67fdd15
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-05-06 15:27:29 +0100

    refactored scope of tests

--- spec/models/views/reports/public_submission_data_spec.rb
+++ spec/models/views/reports/public_submission_data_spec.rb
@@ -3,63 +3,34 @@ require 'rails_helper'
 
 RSpec.describe Views::Reports::PublicSubmissionData do
 
+  before do
+    Timecop.freeze(8.days.ago) do
+      create_list :online_application, 8, :completed, :with_reference, convert_to_application: true
+    end
+    Timecop.freeze(2.days.ago) do
+      create_list :online_application, 2, :completed, :with_reference, convert_to_application: true
+    end
+  end
+
   subject(:data) { described_class.new }
 
-  describe '#submission_all_time_total' do
-    subject { data.submission_all_time_total }
+  it 'returns the expected data' do
+    # #submission_all_time_total returns the expected count of processed online submissions'
+    expect(data.submission_all_time_total).to eq 10
 
-    it { is_expected.to be_a Integer }
-  end
+    # #submission_all_time returns a collection of courts and a total count of the applications processed
+    expect(data.submission_all_time.count).to eq 10
 
-  describe '#submission_all_time' do
-    subject { data.submission_all_time }
+    # #submission_seven_day_total returns the expected count of processed online submissions in the last 7 days
+    expect(data.submission_seven_day_total).to eq 2
 
-    it { is_expected.to be_a Hash }
-  end
+    # #submission_seven_day returns a collection of courts and a count of the applications processed in the last 7 days
+    expect(data.submission_seven_day.count).to eq 2
 
-  describe '#submission_seven_day_total' do
-    subject { data.submission_seven_day_total }
+    # #submission_total_time_taken returns a collection of courts and an average of the time taken to process applications
+    expect(data.submission_total_time_taken.count).to eq 10
 
-    it { is_expected.to be_a Integer }
-  end
-
-  describe '#submission_seven_day' do
-    subject { data.submission_seven_day }
-
-    it { is_expected.to be_a Hash }
-  end
-
-  describe '#submission_total_time_taken' do
-    subject { data.submission_total_time_taken }
-
-    it { is_expected.to be_a Array }
-  end
-
-  describe '#submission_seven_day_time_taken' do
-    subject { data.submission_seven_day_time_taken }
-
-    it { is_expected.to be_a Array }
-  end
-
-  describe 'when populated with data' do
-    let(:user) { create :staff }
-
-    before do
-      Timecop.freeze(8.days.ago) do
-        create_list :online_application, 8, :completed, :with_reference, convert_to_application: true
-      end
-      Timecop.freeze(2.days.ago) do
-        create_list :online_application, 2, :completed, :with_reference, convert_to_application: true
-      end
-    end
-
-    it 'returns expected values' do
-      expect(data.submission_all_time_total).to eq 10
-      expect(data.submission_seven_day_total).to eq 2
-      expect(data.submission_all_time.count).to eq 10
-      expect(data.submission_seven_day.count).to eq 2
-      expect(data.submission_total_time_taken.count).to eq 10
-      expect(data.submission_seven_day_time_taken.count).to eq 2
-    end
+    # #submission_seven_day_time_taken returns a collection of courts and an average of the time taken to process applications in the last 7 days
+    expect(data.submission_seven_day_time_taken.count).to eq 2
   end
 end
