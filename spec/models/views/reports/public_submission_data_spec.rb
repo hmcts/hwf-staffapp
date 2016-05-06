84384c8856b0222c06c6bbb7bf7837afb47fb03c
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-05-06 13:00:20 +0100

    Update tests for public data
    * update factories to serve the new tests

--- spec/models/views/reports/public_submission_data_spec.rb
+++ spec/models/views/reports/public_submission_data_spec.rb
@@ -40,4 +40,26 @@ RSpec.describe Views::Reports::PublicSubmissionData do
 
     it { is_expected.to be_a Array }
   end
+
+  describe 'when populated with data' do
+    let(:user) { create :staff }
+
+    before do
+      Timecop.freeze(8.days.ago) do
+        create_list :online_application, 8, :completed, :with_reference, convert_to_application: true
+      end
+      Timecop.freeze(2.days.ago) do
+        create_list :online_application, 2, :completed, :with_reference, convert_to_application: true
+      end
+    end
+
+    it 'returns expected values' do
+      expect(data.submission_all_time_total).to eq 10
+      expect(data.submission_seven_day_total).to eq 2
+      expect(data.submission_all_time.count).to eq 10
+      expect(data.submission_seven_day.count).to eq 2
+      expect(data.submission_total_time_taken.count).to eq 10
+      expect(data.submission_seven_day_time_taken.count).to eq 2
+    end
+  end
 end
