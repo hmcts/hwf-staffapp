2e4338cc8f2b021c1a63a27a5ec4c0b4d1b4aea7
Jakub Novotny <novotny.jakub@gmail.com>
2016-05-10 13:58:45 +0100

    Add delayed_job and its data structure

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160509081902) do
+ActiveRecord::Schema.define(version: 20160509152138) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -114,6 +114,22 @@ ActiveRecord::Schema.define(version: 20160509081902) do
   add_index "business_entities", ["name"], name: "index_business_entities_on_name", using: :btree
   add_index "business_entities", ["office_id", "jurisdiction_id", "valid_to"], name: "unique_active_office_jurisdiction", unique: true, using: :btree
   add_index "business_entities", ["office_id"], name: "index_business_entities_on_office_id", using: :btree
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer  "priority",   default: 0, null: false
+    t.integer  "attempts",   default: 0, null: false
+    t.text     "handler",                null: false
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string   "locked_by"
+    t.string   "queue"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "details", force: :cascade do |t|
     t.integer  "application_id",   null: false
