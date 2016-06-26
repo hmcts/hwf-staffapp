d1fc652bd8056b1e045750d563ef8b4fbe04c3fa
Jakub Novotny <novotny.jakub@gmail.com>
2016-06-26 22:31:53 +0100

    Remove temporarily commented specs

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -28,39 +28,4 @@ RSpec.describe Application, type: :model do
   it { is_expected.to validate_uniqueness_of(:reference).allow_blank }
 
   it { is_expected.to define_enum_for(:state).with([:created, :waiting_for_evidence, :waiting_for_part_payment, :processed, :deleted]) }
-
-  # it { is_expected.to delegate_method(:applicant_age).to(:applicant).as(:age) }
-
-  describe 'temporary methods delegation to sliced models' do
-    let(:param) { true }
-
-    # describe '-> Applicant' do
-    #   described_class::APPLICANT_GETTERS.each do |getter|
-    #     it { is_expected.to delegate_method(getter).to(:applicant) }
-    #   end
-    #
-    #   described_class::APPLICANT_SETTERS.each do |setter|
-    #     it "should delegate #{setter} to #applicant object" do
-    #       expect(applicant).to receive(setter).with(param)
-    #       application.send(setter, param)
-    #     end
-    #   end
-    # end
-
-    # describe '-> Detail' do
-    #   described_class::DETAIL_GETTERS.each do |getter|
-    #     it { is_expected.to delegate_method(getter).to(:detail) }
-    #   end
-    #
-    #   described_class::DETAIL_SETTERS.each do |setter|
-    #     it "should delegate #{setter} to #detail object" do
-    #       # this is a hack to make sure the bellow expectation is not tested when the factories are being created
-    #       application
-    #
-    #       expect(detail).to receive(setter).with(param)
-    #       application.send(setter, param)
-    #     end
-    #   end
-    # end
-  end
 end
