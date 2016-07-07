1b56d8ecc3725d90a4919b0dd2ac145f25e86c54
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-07-07 15:07:49 +0100

    Add `override` tests to controller

--- spec/controllers/applications/process_controller_spec.rb
+++ spec/controllers/applications/process_controller_spec.rb
@@ -468,6 +468,51 @@ RSpec.describe Applications::ProcessController, type: :controller do
     end
   end
 
+  describe 'PUT #override' do
+    let!(:application) { create(:application, office: user.office) }
+    let(:override_reason) { nil }
+    let(:params) { { value: override_value, reason: override_reason, created_by_id: user.id } }
+
+    before { put :override, application_id: application.id, application: params }
+
+    context 'when the parameters are valid' do
+      context 'by selecting a radio button' do
+        let(:override_value) { 1 }
+
+        it 'redirects to the confirmation page' do
+          expect(response).to redirect_to(application_confirmation_path(application))
+        end
+      end
+
+      context 'by selecting `other` and providing a reason' do
+        let(:override_value) { 'other' }
+        let(:override_reason) { 'foo bar' }
+
+        it 'redirects to the confirmation page' do
+          expect(response).to redirect_to(application_confirmation_path(application))
+        end
+      end
+    end
+
+    context 'when the parameters are invalid' do
+      context 'because they are missing' do
+        let(:override_value) { nil }
+
+        it 're-renders the confirmation page' do
+          expect(response).to render_template(:confirmation)
+        end
+      end
+
+      context 'because a reason is not supplied' do
+        let(:override_value) { 'other' }
+
+        it 're-renders the confirmation page' do
+          expect(response).to render_template(:confirmation)
+        end
+      end
+    end
+  end
+
   context 'GET #confirmation' do
     before { get :confirmation, application_id: application.id }
 
