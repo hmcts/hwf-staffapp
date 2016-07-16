f99aee6d02e25ed66de67811ba1e35df58120afb
Jakub Novotny <novotny.jakub@gmail.com>
2016-07-16 11:21:37 +0100

    Add income threshold fields to Application

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627135552) do
+ActiveRecord::Schema.define(version: 20160716101626) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20160627135552) do
     t.datetime "decision_date"
     t.decimal  "decision_cost"
     t.integer  "online_application_id"
+    t.boolean  "income_min_threshold_exceeded"
+    t.boolean  "income_max_threshold_exceeded"
   end
 
   add_index "applications", ["business_entity_id"], name: "index_applications_on_business_entity_id", using: :btree
