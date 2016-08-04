444cded5dfefc7219d30cd1ff2cc2a995f188007
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-08-04 12:10:42 +0100

    fix typo in migration script

--- db/migrate/20160803121730_add_type_to_evidence_check.rb
+++ db/migrate/20160803121730_add_type_to_evidence_check.rb
@@ -1,6 +1,6 @@
 class AddTypeToEvidenceCheck < ActiveRecord::Migration
   def change
     add_column :evidence_checks, :check_type, :string
-    EvidenceCheck.connection.execute("UPDATE evidence_checks SET check_çtype='random';")
+    EvidenceCheck.connection.execute("UPDATE evidence_checks SET check_type='random';")
   end
 end
