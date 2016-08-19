b8ff1b770d11224c9e7fdaa11c87708ea1ac49e0
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-08-19 14:24:32 +0100

    Update DB with UpdateMissingBusinessEntities

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818114238) do
+ActiveRecord::Schema.define(version: 20160819130824) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
