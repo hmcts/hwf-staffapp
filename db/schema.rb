21fb225adb64242423e3bec5f173a89e5ae20194
ColinBruce <colin.bruce@digital.justice.gov.uk>
2016-12-12 14:46:50 +0000

    Create migration for moving from bec to sop

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207104343) do
+ActiveRecord::Schema.define(version: 20161208123710) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,15 +106,16 @@ ActiveRecord::Schema.define(version: 20161207104343) do
   create_table "business_entities", force: :cascade do |t|
     t.integer  "office_id",       null: false
     t.integer  "jurisdiction_id", null: false
-    t.string   "code",            null: false
+    t.string   "be_code",         null: false
     t.string   "name",            null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.datetime "valid_from"
     t.datetime "valid_to"
+    t.string   "sop_code"
   end
 
-  add_index "business_entities", ["code"], name: "index_business_entities_on_code", using: :btree
+  add_index "business_entities", ["be_code"], name: "index_business_entities_on_be_code", using: :btree
   add_index "business_entities", ["jurisdiction_id"], name: "index_business_entities_on_jurisdiction_id", using: :btree
   add_index "business_entities", ["name"], name: "index_business_entities_on_name", using: :btree
   add_index "business_entities", ["office_id", "jurisdiction_id", "valid_to"], name: "unique_active_office_jurisdiction", unique: true, using: :btree
