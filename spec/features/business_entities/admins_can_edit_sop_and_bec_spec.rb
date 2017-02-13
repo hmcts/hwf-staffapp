51c6daa8d3b2cbccbd9c90db2228a13d94a9ba5a
Petr Zaparka <petr@zaparka.cz>
2017-02-13 16:28:15 +0000

    Removing switch from Views

--- spec/features/business_entities/admins_can_edit_sop_and_bec_spec.rb
+++ spec/features/business_entities/admins_can_edit_sop_and_bec_spec.rb
@@ -7,23 +7,8 @@ RSpec.feature 'Business entity management around SOP and BEC switchover ', type:
   let(:business_entity) { office.business_entities.first }
 
   before do
-    Timecop.freeze(current_time)
     login_as admin
     visit "offices/#{office.id}/business_entities/#{business_entity.id}/edit"
-  end
-  after { Timecop.return }
-
-  context 'before the switchover date' do
-    let(:current_time) { reference_change_date - 1.day }
-
-    scenario 'user can edit the be_code' do
-      expect(page).to have_xpath("//input[@name='business_entity[be_code]' and @value='#{business_entity.be_code}']")
-    end
-
-    scenario 'user can edit the sop_code' do
-      expect(page).to have_xpath("//input[@name='business_entity[sop_code]' and @value='#{business_entity.sop_code}']")
-    end
-
   end
 
   context 'after the switchover date' do
