65c8b270e1745ea98ca3c22bca4a8bac43d3bcf6
Petr Zaparka <petr@zaparka.cz>
2017-04-25 12:18:52 +0100

    Adding tests for first 3 scenarios
    Given I have submitted a benefits-based application
    Then I should not have to provide evidence
    Given I have submitted a income-based application
    Then I should have a random 10% chance of being selected to provide evidence
    Given I have submitted a refund and income based application
    Then I should have a random “?” chance of being selected to provide evidence

---
+++ spec/support/admin_application_steps.rb
@@ -0,0 +1,76 @@
+def fill_personal_details
+  expect(page).to have_text 'Personal details'
+  fill_in 'Title', with: 'Mr.'
+  fill_in 'First and middle names', with: 'Johny'
+  fill_in 'Last name', with: 'Mnemonick'
+  fill_in 'Date of birth', with: '01/01/2000'
+  fill_in 'National Insurance number', with: 'SN123456C'
+  choose 'Single'
+  click_button 'Next'
+end
+
+def fill_application_details
+  expect(page).to have_text 'Application details'
+  fill_in 'Fee', with: '1000'
+  choose Jurisdiction.first.display_full.to_s
+  fill_in 'Date application received', with: Date.yesterday.to_s
+  click_button 'Next'
+end
+
+def fill_application_refund_details
+  expect(page).to have_text 'Application details'
+  fill_in 'Fee', with: '1000'
+  choose Jurisdiction.first.display_full.to_s
+  fill_in 'Date application received', with: Date.yesterday.to_s
+  check 'This is a refund case'
+  fill_in 'Date fee paid', with: 2.days.ago.to_date.to_s
+  click_button 'Next'
+end
+
+def fill_saving_and_investment
+  expect(page).to have_text 'Savings and investments'
+  choose 'Less than £3,000'
+  click_button 'Next'
+end
+
+def fill_benefits(benefits)
+  expect(page).to have_text 'Benefits'
+  choose (benefits) ? 'Yes' : 'No'
+
+  click_button 'Next'
+end
+
+def fill_benefit_evidence(benefits_options)
+  expect(page).to have_text 'Has the applicant provided paper evidence?'
+  if benefits_options[:paper_provided]
+    choose 'Yes, the applicant has provided paper evidence'
+    if benefits_options[:paper_correct]
+      choose 'Yes, the evidence is correct'
+    else
+      choose 'No'
+    end
+  else
+    choose 'benefit_override_evidence_false'
+  end
+
+  click_button 'Next'
+end
+
+def fill_income(supporting_children)
+  expect(page).to have_text 'Income'
+  if supporting_children
+   choose 'Yes'
+   fill_in 'Number of children', with: '2'
+  else
+    choose 'No'
+  end
+  fill_in 'Total monthly income', with: '1000'
+
+  click_button 'Next'
+end
+
+def has_evidence_check?
+  reference_number = find(:xpath, './/span[@class="reference-number"]').text
+  application = Application.where(reference: reference_number).last
+  application.try(:evidence_check).present?
+end
