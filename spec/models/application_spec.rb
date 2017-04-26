cbc09a764e0a5291b38034374856247083427a9f
Petr Zaparka <petr@zaparka.cz>
2017-04-26 15:27:54 +0100

    Adding application scope
    For pending check and mathcing ni number. Needed for testing if applciation should require evidence check.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -28,4 +28,53 @@ RSpec.describe Application, type: :model do
   it { is_expected.to validate_uniqueness_of(:reference).allow_blank }
 
   it { is_expected.to define_enum_for(:state).with([:created, :waiting_for_evidence, :waiting_for_part_payment, :processed, :deleted]) }
+
+  describe 'with_evidence_check_for_ni_number' do
+    let(:evidence_check) { create(:evidence_check, application: application) }
+
+    context 'pending evidence check' do
+      let(:application) { create(:application, :waiting_for_evidence_state, applicant: applicant) }
+      let(:applicant) { create(:applicant, ni_number: ni_number) }
+      let(:ni_number) { 'SN123456C' }
+
+      it "matching NI number" do
+        evidence_check
+        list = Application.with_evidence_check_for_ni_number(ni_number)
+        expect(list).to eq([application])
+      end
+
+      it "not matching NI number" do
+        evidence_check
+        list = Application.with_evidence_check_for_ni_number('SN123456D')
+        expect(list).to eq([])
+      end
+
+      it "missing evidence_check record" do
+        list = Application.with_evidence_check_for_ni_number(ni_number)
+        expect(list).to eq([])
+      end
+    end
+
+    context 'different state' do
+      let(:application) { create(:application, applicant: applicant) }
+      let(:applicant) { create(:applicant, ni_number: ni_number) }
+      let(:ni_number) { 'SN123456C' }
+      before { evidence_check }
+
+      it "matching NI number" do
+        list = Application.with_evidence_check_for_ni_number(ni_number)
+        expect(list).to eq([])
+      end
+
+      it "not matching NI number" do
+        list = Application.with_evidence_check_for_ni_number('SN123456D')
+        expect(list).to eq([])
+      end
+
+      it "missing evidence_check record" do
+        list = Application.with_evidence_check_for_ni_number(ni_number)
+        expect(list).to eq([])
+      end
+    end
+  end
 end
