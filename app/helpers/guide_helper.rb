d8b1e01d8f4a7d281c9ea796e644ed004462da55
Leonidas Apostolidis <leoapost@gmail.com>
2017-05-11 13:08:44 +0100

    add link in guides to complete staff guidance

---
+++ app/helpers/guide_helper.rb
@@ -0,0 +1,5 @@
+module GuideHelper
+  def staff_guidance_url
+    'http://hmcts.intranet.service.justice.gov.uk/hmcts/documents/lean/finance/help-with-fees/help-with-fees-staff-guidance.pdf'
+  end
+end
