66590e8be8e220c8ffc1eff2c509582bb0d30790
Petr Zaparka <petr@zaparka.cz>
2017-10-27 11:34:02 +0100

    Removing comment and adding another test.

--- spec/features/applications/processing_refund_application_spec.rb
+++ spec/features/applications/processing_refund_application_spec.rb
@@ -22,21 +22,51 @@ RSpec.feature 'Processing refund application with valid date received date', typ
       jurisdiction: jurisdiction)
   end
 
+  let(:online_application_2) do
+    create(:online_application, :completed, :with_reference,
+      married: false,
+      children: 3,
+      benefits: true,
+      fee: 1550,
+      form_name: 'D11',
+      income_min_threshold_exceeded: false,
+      refund: true,
+      date_fee_paid: 5.months.ago,
+      date_received: 3.months.ago,
+      jurisdiction: jurisdiction)
+  end
+
   before do
     login_as user
     dwp_api_response 'Yes'
   end
 
-  it "should not fail based on wrong date" do
+  it "should not fail based on invalid date" do
     visit '/'
     fill_in :online_search_reference, with: online_application_1.reference
     click_button 'Look up'
+    expect(page).to have_content "Application details"
     choose jurisdiction.name
     click_button 'Next'
+    expect(page).to have_content "Check details"
     click_button 'Complete processing'
-    # save_and_open_page
+
     expect(page).to have_content 'Savings and investments✓ Passed'
     expect(page).to have_content 'Benefits✓ Passed'
     expect(page).to have_content 'Eligible for help with fees'
+  end
+
+  it "should fail based on invalid date" do
+    visit '/'
+    fill_in :online_search_reference, with: online_application_2.reference
+    click_button 'Look up'
+    expect(page).to have_content "Application details"
+    choose jurisdiction.name
+    click_button 'Next'
+    expect(page).to have_content "Check details"
+    click_button 'Complete processing'
+
+    expect(page).to have_content 'Not eligible for help with fees'
+    expect(page).to have_content 'Savings and investments✓ Passed'
   end
 end
