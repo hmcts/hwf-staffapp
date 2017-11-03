ec742189ddc940d81631e8ef9e9d4a65d0603cc6
Petr Zaparka <petr@zaparka.cz>
2017-11-03 14:43:25 +0000

    Removing html format from notification mailer.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -5,7 +5,6 @@ class ApplicationMailer < ActionMailer::Base
   def dwp_is_down_notifier
     to = Settings.mail.dwp_notification_alert
     mail(to: to, subject: t('email.dwp_alert_notification..subject')) do |format|
-      format.html { render plain: t('email.dwp_alert_notification.message') }
       format.text { render plain: t('email.dwp_alert_notification.message') }
     end
   end
