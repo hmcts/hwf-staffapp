0489b7dcded3d0a37d3c3c31621baae61a796856
Petr Zaparka <petr@zaparka.cz>
2017-11-21 11:28:12 +0000

    Moving env value to .env file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,7 +7,6 @@ end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
-ENV['ENV'] ||= 'test'
 ENV['DWP_API_PROXY'] ||= 'http://localhost:9292'
 
 require File.expand_path('../../config/environment', __FILE__)
