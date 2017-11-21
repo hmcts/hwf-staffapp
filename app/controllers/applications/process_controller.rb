fb454988ce129cc19e2755fb8eed5f425bdbb8ad
Petr Zaparka <petr@zaparka.cz>
2017-11-21 16:39:13 +0000

    Asking for paper evidence when refund and no benefits and out of 3 months period.

--- app/controllers/applications/process_controller.rb
+++ app/controllers/applications/process_controller.rb
@@ -186,6 +186,8 @@ module Applications
       if benefits
         benefit_check_runner.run
         determine_override
+      elsif no_benefits_paper_evidence?
+        redirect_to application_benefit_override_paper_evidence_path(application)
       else
         redirect_to(action: :income)
       end
@@ -196,6 +198,12 @@ module Applications
         redirect_to application_benefit_override_paper_evidence_path(application)
       else
         redirect_to(action: :summary)
+      end
+    end
+
+    def no_benefits_paper_evidence?
+      if application.detail.refund?
+        !BenefitCheckRunner.new(application).benefit_check_date_valid?
       end
     end
   end
--- spec/features/applications/processing_refund_application_spec.rb
+++ spec/features/applications/processing_refund_application_spec.rb
@@ -79,4 +79,137 @@ RSpec.feature 'Processing refund application with valid date received date', typ
     end
 
   end
+
+  context 'Postal refund application' do
+    let(:applicant) { build :applicant_with_all_details }
+    let(:application) { build :application, applicant: applicant }
+
+    context 'with benefits' do
+      it "valid date" do
+        visit '/'
+        click_button 'Start now'
+        expect(page).to have_content "Personal details"
+        complete_page_as 'personal_information', application, true
+
+        expect(page).to have_content "Application details"
+        complete_page_as 'application_details', application, false
+        check "This is a refund case"
+        fill_in "application_date_fee_paid", with: 10.days.ago.to_s
+        click_button 'Next'
+
+        choose 'Less than £3,000'
+        fill_in 'application_amount', with: 0
+        click_button 'Next'
+
+        expect(page).to have_content "Does the applicant receive benefits?"
+        choose 'Yes'
+        click_button 'Next'
+
+        expect(page).to have_content "Check details"
+        click_button 'Complete processing'
+
+        expect(page).to have_content 'Savings and investments✓ Passed'
+        expect(page).to have_content 'Benefits✓ Passed'
+        expect(page).to have_content 'Eligible for help with fees'
+      end
+
+      it "paper evidence check if invalid date" do
+        visit '/'
+        click_button 'Start now'
+        expect(page).to have_content "Personal details"
+        complete_page_as 'personal_information', application, true
+
+        expect(page).to have_content "Application details"
+        complete_page_as 'application_details', application, false
+        check "This is a refund case"
+        fill_in "application_date_fee_paid", with: 4.months.ago.to_s
+        click_button 'Next'
+
+        choose 'Less than £3,000'
+        fill_in 'application_amount', with: 0
+        click_button 'Next'
+
+        expect(page).to have_content "Does the applicant receive benefits?"
+        choose 'Yes'
+        click_button 'Next'
+        expect(page).to have_content "Fees paid more than 3 months ago can’t be checked with the DWP. The applicant must provide paper evidence to show they were receiving eligible benefits on the date they paid"
+        choose "benefit_override_evidence_true"
+        choose "benefit_override_correct_true"
+        click_button 'Next'
+
+        click_button 'Complete processing'
+
+        expect(page).to have_content 'Savings and investments✓ Passed'
+        expect(page).to have_content 'Benefits✓ Passed'
+        expect(page).to have_content 'Eligible for help with fees'
+      end
+    end
+
+    context 'without benefits' do
+      it "valid date" do
+        visit '/'
+        click_button 'Start now'
+        expect(page).to have_content "Personal details"
+        complete_page_as 'personal_information', application, true
+
+        expect(page).to have_content "Application details"
+        complete_page_as 'application_details', application, false
+        check "This is a refund case"
+        fill_in "application_date_fee_paid", with: 10.days.ago.to_s
+        click_button 'Next'
+
+        choose 'Less than £3,000'
+        fill_in 'application_amount', with: 0
+        click_button 'Next'
+
+        expect(page).to have_content "Does the applicant receive benefits?"
+        choose 'No'
+        click_button 'Next'
+
+        expect(page).to have_content "In question 10, does the applicant financially support any children?"
+        choose 'No'
+        fill_in 'application_income', with: 1000
+        click_button 'Next'
+
+        expect(page).to have_content "Check details"
+        click_button 'Complete processing'
+
+        expect(page).to have_content 'Savings and investments✓ Passed'
+        expect(page).to have_content 'Eligible for help with fees'
+      end
+
+      it "paper evidence check if invalid date" do
+        visit '/'
+        click_button 'Start now'
+        expect(page).to have_content "Personal details"
+        complete_page_as 'personal_information', application, true
+
+        expect(page).to have_content "Application details"
+        complete_page_as 'application_details', application, false
+        check "This is a refund case"
+        fill_in "application_date_fee_paid", with: 4.months.ago.to_s
+        click_button 'Next'
+
+        choose 'Less than £3,000'
+        fill_in 'application_amount', with: 0
+        click_button 'Next'
+
+        expect(page).to have_content "Does the applicant receive benefits?"
+        choose 'No'
+        click_button 'Next'
+
+        expect(page).to have_content "Fees paid more than 3 months ago can’t be checked with the DWP. The applicant must provide paper evidence to show they were receiving eligible benefits on the date they paid"
+        choose "benefit_override_evidence_true"
+        choose "benefit_override_correct_true"
+        click_button 'Next'
+        binding.pry
+
+        click_button 'Complete processing'
+
+        expect(page).to have_content 'Savings and investments✓ Passed'
+        expect(page).to have_content 'Benefits✓ Passed'
+        expect(page).to have_content 'Eligible for help with fees'
+      end
+    end
+  end
 end
