875fb3cc714ae04e5364972b5e01e5e611c759c5
Petr Zaparka <petr@zaparka.cz>
2018-02-23 14:23:09 +0000

    Adding paper to Application model.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::Base
 
   include Pundit
   before_action :authenticate_user!
+  before_action :set_paper_trail_whodunnit
   after_action :verify_authorized
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
