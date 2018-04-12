156444c241930503b43a0ecb3f590ce5d6dac035
Petr Zaparka <petr@zaparka.cz>
2018-04-12 15:17:44 +0100

    Fixing rubocop warning.

--- spec/support/admin_application_steps.rb
+++ spec/support/admin_application_steps.rb
@@ -107,6 +107,9 @@ def fill_application_date_over_limit
 
   fill_in 'Date fee paid', with: 4.months.ago.to_date.to_s
   click_button 'Next'
+end
+
+def fill_no_discretion
   choose 'application_discretion_applied_false'
   click_button 'Next'
 end
