c5d91ff8a0f0eefe4dadd41a86746550e45f4859
Temi Oluwaseun <temi.oluwaseun@hmcts.net>
2018-04-20 15:03:52 +0100

    fixing rubocop error

--- spec/features/no_evidence_check/rst_985_spec.rb
+++ spec/features/no_evidence_check/rst_985_spec.rb
@@ -11,44 +11,44 @@ RSpec.feature 'Application is not evidence check when income is above threshold'
     login_as user
   end
 
-    context 'Always apply EV check for application with same NINO' do
+  context 'Always apply EV check for application with same NINO' do
 
-      let(:application) { create :application_full_remission }
+    let(:application) { create :application_full_remission }
 
-      scenario 'Check that evidence check is on for 2nd and 3rd application with same NINO' do
-        start_new_application
-        fill_personal_details('SN123456D')
-        fill_application_details
-        fill_saving_and_investment
-        fill_benefits(false)
-        fill_income(false)
-        expect(page).to have_text 'Check details'
-        click_button 'Complete processing'
+    scenario 'Check that evidence check is on for 2nd and 3rd application with same NINO' do
+      start_new_application
+      fill_personal_details('SN123456D')
+      fill_application_details
+      fill_saving_and_investment
+      fill_benefits(false)
+      fill_income(false)
+      expect(page).to have_text 'Check details'
+      click_button 'Complete processing'
 
-        visit home_index_url
-        create_flag_check('SN123456D')
+      visit home_index_url
+      create_flag_check('SN123456D')
 
-        click_button 'Start now'
-        fill_personal_details('SN123456D')
-        fill_application_details
-        fill_saving_and_investment
-        fill_benefits(false)
-        fill_income(false)
-        click_button 'Complete processing'
-        expect(page).to have_content('Evidence of income needs to be checked')
-        expect(page).not_to have_content('✓ Eligible for help with fees')
+      click_button 'Start now'
+      fill_personal_details('SN123456D')
+      fill_application_details
+      fill_saving_and_investment
+      fill_benefits(false)
+      fill_income(false)
+      click_button 'Complete processing'
+      expect(page).to have_content('Evidence of income needs to be checked')
+      expect(page).not_to have_content('✓ Eligible for help with fees')
 
-        visit home_index_url
+      visit home_index_url
 
-        click_button 'Start now'
-        fill_personal_details('SN123456D')
-        fill_application_details
-        fill_saving_and_investment
-        fill_benefits(false)
-        fill_income(false)
-        click_button 'Complete processing'
-        expect(page).to have_content('Evidence of income needs to be checked')
-        expect(page).not_to have_content('✓ Eligible for help with fees')
+      click_button 'Start now'
+      fill_personal_details('SN123456D')
+      fill_application_details
+      fill_saving_and_investment
+      fill_benefits(false)
+      fill_income(false)
+      click_button 'Complete processing'
+      expect(page).to have_content('Evidence of income needs to be checked')
+      expect(page).not_to have_content('✓ Eligible for help with fees')
     end
   end
 end
