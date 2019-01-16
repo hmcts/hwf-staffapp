3fffe0e447f5f309ef45cdd1d5d3c716334c1473
Leonidas Apostolidis <leoapost@users.noreply.github.com>
2019-01-16 14:34:07 +0000

    Bump Ruby from v2.3.3 to v2.5.3 (#870)

--- app/models/forms/application/applicant.rb
+++ app/models/forms/application/applicant.rb
@@ -51,7 +51,7 @@ module Forms
       end
 
       def validate_dob
-        if date_of_birth =~ /[a-zA-Z]/
+        if /[a-zA-Z]/.match?(date_of_birth.to_s)
           errors.add(:date_of_birth, "can't contain non numbers")
         elsif !date_of_birth.is_a?(Date)
           errors.add(:date_of_birth, :not_a_date)
