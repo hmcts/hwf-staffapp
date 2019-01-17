9fa556b1d51455f0593679331df09d788b007d25
Leonidas Apostolidis <leoapost@gmail.com>
2019-01-17 14:21:06 +0000

    Make the Staff Guidance pdf accessible to DOM1 users

--- spec/helpers/guide_helper_spec.rb
+++ spec/helpers/guide_helper_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe GuideHelper do
 
   describe '#staff_guidance_url' do
-    it { expect(helper.staff_guidance_url).to include('http://hmcts.intranet.service.justice.gov.uk/hmcts/documents/lean/finance/help-with-fees/help-with-fees-staff-guidance.pdf') }
+    it { expect(helper.staff_guidance_url).to include('https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-policy-guide.pdf') }
   end
 
   describe '#how_to_url' do
