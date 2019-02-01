a2873c8b0970cd1f486fb78aa93061bbe9524d5a
Leonidas Apostolidis <leoapost@gmail.com>
2019-02-01 13:36:45 +0000

    Create a transactional-level Finance report

---
+++ app/models/forms/report/finance_transactional_report.rb
@@ -0,0 +1,34 @@
+module Forms
+  module Report
+    class FinanceTransactionalReport
+      include Virtus.model(nullify_blank: true)
+      include ActiveModel::Model
+
+      attribute :date_from, Date
+      attribute :date_to, Date
+
+      validates :date_to, :date_from, presence: true
+
+      validates :date_to, date: {
+        after: :date_from, allow_blank: true
+      }
+
+      validates :date_to, date: {
+        before: proc { |obj| obj.date_from + 2.years },
+        message: "The date range can't be longer than 2 years"
+      }, if: :date_from
+
+      def i18n_scope
+        "activemodel.attributes.forms/report/finance_transactional_report".to_sym
+      end
+
+      def start_date
+        date_from.try(:strftime, Date::DATE_FORMATS[:gov_uk_long])
+      end
+
+      def end_date
+        date_to.try(:strftime, Date::DATE_FORMATS[:gov_uk_long])
+      end
+    end
+  end
+end
---
+++ spec/models/forms/report/finance_transactional_report_spec.rb
@@ -0,0 +1,58 @@
+require 'rails_helper'
+
+RSpec.describe Forms::Report::FinanceTransactionalReport do
+  subject { report }
+
+  let(:report) { described_class.new }
+
+  describe 'validations' do
+    before do
+      report.date_from = Time.zone.today.-1.month
+      report.date_to = Time.zone.today
+    end
+
+    describe 'date_from' do
+      it { is_expected.to validate_presence_of(:date_from) }
+
+      context 'when the date_from is less than date_to' do
+        before { report.date_from = Time.zone.tomorrow }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe 'date_to' do
+      it { is_expected.to validate_presence_of(:date_to) }
+
+      context 'when date_to is before date_from' do
+        before { report.date_to = Time.zone.today.-1.year }
+
+        it { is_expected.not_to be_valid }
+      end
+
+      context 'when date_to is longer than two years' do
+        before { report.date_to = 3.years.from_now }
+
+        it { is_expected.not_to be_valid }
+      end
+    end
+
+    describe '#i18n_scope' do
+      subject { report.i18n_scope }
+
+      it { is_expected.to eq :'activemodel.attributes.forms/report/finance_transactional_report' }
+    end
+
+    describe '#start_date' do
+      subject { report.start_date }
+
+      it { is_expected.to eq report.date_from.try(:strftime, Date::DATE_FORMATS[:gov_uk_long]) }
+    end
+
+    describe '#end_date' do
+      subject { report.end_date }
+
+      it { is_expected.to eq report.date_to.try(:strftime, Date::DATE_FORMATS[:gov_uk_long]) }
+    end
+  end
+end
