8cf53037cbd1f1d8d7946c05852e32eb79682f51
Alexa <alexa.ballantine@gmail.com>
2019-02-08 09:40:38 +0000

    removed WaitUntil and added config setup in capybara driver helper

--- features/support/generic_helpers.rb
+++ features/support/generic_helpers.rb
@@ -1,10 +1,3 @@
-module WaitUntil
-  def self.wait_until(timeout = 10, message = nil, &block)
-    wait = Selenium::WebDriver::Wait.new(timeout: timeout, message: message)
-    wait.until(&block)
-  end
-end
-
 def base_page
   @base_page ||= BasePage.new
 end
