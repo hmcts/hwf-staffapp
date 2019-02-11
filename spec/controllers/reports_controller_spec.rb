9611f357a26d8c102a14787c6003e7e9177032d7
Leonidas Apostolidis <leoapost@gmail.com>
2019-02-11 11:55:32 +0200

    Add controller specs for the finance transactional report

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -62,6 +62,47 @@ RSpec.describe ReportsController, type: :controller do
       end
     end
 
+    describe 'GET #finance_transactional_report' do
+      before { get :finance_transactional_report }
+
+      subject { response }
+
+      it { is_expected.to have_http_status(:success) }
+      it { is_expected.to render_template :finance_transactional_report }
+
+      describe 'assigns form object' do
+        subject { assigns(:form) }
+
+        it { is_expected.to be_a_kind_of Forms::Report::FinanceTransactionalReport }
+      end
+    end
+
+    describe 'PUT #finance_transactional_report' do
+
+      subject { response }
+
+      context 'with invalid data - nil date from' do
+        before { put :finance_transactional_report_generator, forms_report_finance_transactional_report: { date_from: nil, date_to: '2018-12-31' } }
+
+        it { is_expected.to have_http_status(:success) }
+        it { is_expected.to render_template :finance_transactional_report }
+      end
+
+      context 'with valid data - both from and to dates' do
+        before { put :finance_transactional_report_generator, forms_report_finance_transactional_report: { date_from: '2018-01-01', date_to: '2018-12-31' } }
+
+        it { is_expected.to have_http_status(:success) }
+
+        it 'sets the filename' do
+          expect(response.headers['Content-Disposition']).to include('finance-transactional-report-')
+        end
+
+        it 'sets the file type' do
+          expect(response.headers['Content-Type']).to include('text/csv')
+        end
+      end
+    end
+
     describe 'GET #graphs' do
       before { get :graphs }
 
