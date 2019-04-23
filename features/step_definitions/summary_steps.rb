34b3f1fad434d4e04052f309c80a45254d787010
Alexa <alexa.ballantine@gmail.com>
2019-04-23 09:48:16 +0100

    RST-1772 benefits test on summary page

--- features/step_definitions/summary_steps.rb
+++ features/step_definitions/summary_steps.rb
@@ -15,3 +15,15 @@ Then("I should be taken to the confirmation page") do
   expect(current_path).to include 'confirmation'
   expect(confirmation_page.content).to have_eligible
 end
+
+When("I see benefit summary") do
+  expect(summary_page.content.summary_section[3].summary_header.text).to eq 'Benefits'
+end
+
+Then("I should see I have declared benefits in this application") do
+  expect(summary_page.content.summary_section[3]).to have_benefit_declared_yes
+end
+
+Then("I have provided the correct evidence") do
+  expect(summary_page.content.summary_section[3]).to have_evidence_provided_yes
+end
--- features/support/page_objects/summary_page.rb
+++ features/support/page_objects/summary_page.rb
@@ -2,6 +2,11 @@ class SummaryPage < BasePage
   section :content, '#content' do
     element :header, 'h2', text: 'Check details'
     element :complete_processing_button, 'input[value="Complete processing"]'
+    sections :summary_section, '.summary-section' do
+      element :summary_header, 'h4'
+      element :benefit_declared_yes, '.grid-row', text: 'Benefits declared in application Yes'
+      element :evidence_provided_yes, '.grid-row', text: 'Correct evidence provided Yes'
+    end
   end
 
   def complete_processing
