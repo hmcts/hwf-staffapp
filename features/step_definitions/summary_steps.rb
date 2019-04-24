6d7c7587942bf4646f3f0fb42e1271b0df1a923b
Alexa <alexa.ballantine@gmail.com>
2019-04-24 15:22:28 +0100

    RST-1772 fix for benefit summary tests

--- features/step_definitions/summary_steps.rb
+++ features/step_definitions/summary_steps.rb
@@ -17,19 +17,21 @@ Then("I should be taken to the confirmation page") do
 end
 
 When("I see benefit summary") do
-  expect(summary_page.content.summary_section[3].row[0].text).to eq 'Benefits Change benefits'
+  expect(summary_page.content.summary_section[3]).to have_benefits_header
 end
 
 Then("I should see I have declared benefits in this application") do
-  expect(summary_page.content.summary_section[3].row[1].text).to eq 'Benefits declared in application Yes'
+  expect(summary_page.content.summary_section[3]).to have_benefits_question
+  expect(summary_page.content.summary_section[3]).to have_answer_yes
 end
 
 Then("I have provided the correct evidence") do
-  expect(summary_page.content.summary_section[3].row[2].text).to eq 'Correct evidence provided Yes'
+  expect(summary_page.content.summary_section[3]).to have_evidence_question
+  expect(summary_page.content.summary_section[3]).to have_answer_yes
 end
 
 When("I click on change benefits") do
-  summary_page.content.summary_section[3].row[0].change_benefits.click
+  summary_page.content.summary_section[3].change_benefits.click
 end
 
 When("I change my answer to no") do
@@ -38,5 +40,5 @@ end
 
 Then("I should see that my new answer is displayed in the benefit summary") do
   incomes_page.submit_incomes_no
-  expect(summary_page.content.summary_section[3].row[1].text).to eq 'Benefits declared in application No'
+  expect(summary_page.content.summary_section[3]).to have_answer_no
 end
