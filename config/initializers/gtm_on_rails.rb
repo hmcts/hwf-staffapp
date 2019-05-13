3276ff1d163467bb5678b48d04e37cd2bb78933d
Leonidas Apostolidis <leoapost@gmail.com>
2019-05-13 14:11:33 +0200

    Comply with our linter's rules

--- config/initializers/gtm_on_rails.rb
+++ config/initializers/gtm_on_rails.rb
@@ -7,10 +7,12 @@ GtmOnRails.configure do |config|
   config.data_layer_limit_byte_size = 7000
 
   # Settings that send google tag manager controller and action name by dataLayer.
-  # Be careful using this, if you enable this option, controller and action name output in html source code.
+  # Be careful using this, if you enable this option, controller and action name
+  # output in html source code.
   config.send_controller_and_action_in_data_layer = false
 
-  # If somthing error occurred when output tags, subsequent tag's output is stopped and run subsequent processing.
+  # If somthing error occurred when output tags, subsequent tag's output is stopped
+  # and run subsequent processing.
   # The point is, dataLayer error won't affect displaying website, if you enable this option.
   config.rescue_when_error_occurred = true
 
