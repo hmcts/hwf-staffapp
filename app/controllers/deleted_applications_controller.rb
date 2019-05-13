3cc184ede972c317d9f3d1efca7f2366bba9cf8e
Leonidas Apostolidis <leoapost@gmail.com>
2019-05-13 11:28:14 +0200

    Track deleted applications

--- app/controllers/deleted_applications_controller.rb
+++ app/controllers/deleted_applications_controller.rb
@@ -13,6 +13,17 @@ class DeletedApplicationsController < ApplicationController
     authorize application
 
     assign_views
+
+    event = GtmOnRails::DataLayer::Event.new(
+        'Application tracking',
+        medium:           application.medium || 'NA',
+        type:             application.application_type || 'NA',
+        office_id:        current_user.office.id,
+        jurisdiction_id:  application.detail.jurisdiction_id || 'NA',
+        rails_controller: controller_name,
+        rails_action:     action_name
+      )
+    data_layer.push(event)
   end
 
   private
