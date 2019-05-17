13f85613420f6d54cf139dd7e470ba79d04fa973
Petr Zaparka <petr@zaparka.cz>
2019-05-17 16:17:13 +0100

    Fixing errors in model folders specs
    Adding missing validation messages and removing belong_to validations since belongs_to will now trigger a validation error by default if the association is not present.

--- spec/models/detail_spec.rb
+++ spec/models/detail_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Detail, type: :model do
-  it { is_expected.to belong_to(:application) }
   it { is_expected.to validate_presence_of(:application) }
 
   it { is_expected.to belong_to(:jurisdiction) }
--- spec/models/evidence_check_spec.rb
+++ spec/models/evidence_check_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 describe EvidenceCheck, type: :model do
-  it { is_expected.to belong_to(:application) }
   it { is_expected.to validate_presence_of(:application) }
 
   it { is_expected.to validate_presence_of(:expires_at) }
