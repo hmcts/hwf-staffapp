5dfcf3ccc145a10481016beaf095af9981c9482b
Petr Zaparka <petr@zaparka.cz>
2019-06-25 13:29:12 +0100

    Adding more feature  tests around date fields

--- spec/features/applications/show_result_on_confirmation_spec.rb
+++ spec/features/applications/show_result_on_confirmation_spec.rb
@@ -32,19 +32,11 @@ RSpec.feature 'The result is shown on the confirmation page', type: :feature do
       fill_in 'application_ni_number', with: 'AB123456A'
       choose 'application_married_false'
       click_button 'Next'
-
-      expect(page).to have_xpath('//h2', text: 'Application details')
-      fill_in 'application_fee', with: '300'
-      find(:xpath, '(//input[starts-with(@id,"application_jurisdiction_id_")])[1]').click
-      fill_in 'application_day_date_received', with: date_received.day
-      fill_in 'application_month_date_received', with: date_received.month
-      fill_in 'application_year_date_received', with: date_received.year
-      fill_in 'Form number', with: 'ABC123'
-      click_button 'Next'
     end
 
     context 'exceeds the savings threshold' do
       before do
+        application_details_step
         choose :application_min_threshold_exceeded_true
         fill_in :application_amount, with: 3500
         click_button 'Next'
@@ -60,6 +52,7 @@ RSpec.feature 'The result is shown on the confirmation page', type: :feature do
 
     context 'has wrong DOB' do
       before do
+        application_details_step
         choose :application_min_threshold_exceeded_true
         fill_in :application_amount, with: 3500
         click_button 'Next'
@@ -88,6 +81,7 @@ RSpec.feature 'The result is shown on the confirmation page', type: :feature do
     context 'has wrong application received date' do
       let(:new_date_received) { date_received - 1.month }
       before do
+        application_details_step
         choose :application_min_threshold_exceeded_true
         fill_in :application_amount, with: 3500
         click_button 'Next'
@@ -115,8 +109,105 @@ RSpec.feature 'The result is shown on the confirmation page', type: :feature do
       end
     end
 
+    context 'Updating date of death' do
+      let(:deceased_date) { 3.months.ago }
+      let(:new_deceased_date) { 3.months.ago + 11.days }
+
+      before do
+        expect(page).to have_xpath('//h2', text: 'Application details')
+        fill_in 'application_fee', with: '300'
+        find(:xpath, '(//input[starts-with(@id,"application_jurisdiction_id_")])[1]').click
+        fill_in 'application_day_date_received', with: date_received.day
+        fill_in 'application_month_date_received', with: date_received.month
+        fill_in 'application_year_date_received', with: date_received.year
+        fill_in 'Form number', with: 'ABC123'
+
+        check('application_probate')
+        fill_in 'application_deceased_name', with: 'Jane'
+        fill_in 'application_day_date_of_death', with: deceased_date.day
+        fill_in 'application_month_date_of_death', with: deceased_date.month
+        fill_in 'application_year_date_of_death', with: deceased_date.year
+        click_button 'Next'
+
+        choose :application_min_threshold_exceeded_true
+        fill_in :application_amount, with: 3500
+        click_button 'Next'
+      end
+
+      scenario 'the summary page shows the benefit data' do
+        deceased_date_text = deceased_date.strftime('%-d %B %Y')
+        expect(page).to have_xpath('//h2', text: 'Check details')
+        expect(page).to have_content("Name of the deceasedJane")
+        expect(page).to have_content("Date of their death#{deceased_date_text}")
+
+        click_link 'Change application details'
+
+        expect(page).to have_xpath(".//input[@id='application_day_date_of_death'][@value='#{deceased_date.day}']")
+        expect(page).to have_xpath(".//input[@id='application_month_date_of_death'][@value='#{deceased_date.month}']")
+        expect(page).to have_xpath(".//input[@id='application_year_date_of_death'][@value='#{deceased_date.year}']")
+
+        fill_in 'application_day_date_of_death', with: new_deceased_date.day
+        fill_in 'application_month_date_of_death', with: new_deceased_date.month
+        fill_in 'application_year_date_of_death', with: new_deceased_date.year
+
+        click_button 'Next'
+        click_button 'Next'
+
+        new_deceased_date_text = new_deceased_date.strftime('%-d %B %Y')
+        expect(page).to have_content("Date of their death#{new_deceased_date_text}")
+      end
+    end
+
+    context 'Updating refund date' do
+      let(:refund_date) { 3.months.ago }
+      let(:new_refund_date) { 3.months.ago + 11.days }
+
+      before do
+        expect(page).to have_xpath('//h2', text: 'Application details')
+        fill_in 'application_fee', with: '300'
+        find(:xpath, '(//input[starts-with(@id,"application_jurisdiction_id_")])[1]').click
+        fill_in 'application_day_date_received', with: date_received.day
+        fill_in 'application_month_date_received', with: date_received.month
+        fill_in 'application_year_date_received', with: date_received.year
+        fill_in 'Form number', with: 'ABC123'
+
+        check('application_refund')
+        fill_in 'application_day_date_fee_paid', with: refund_date.day
+        fill_in 'application_month_date_fee_paid', with: refund_date.month
+        fill_in 'application_year_date_fee_paid', with: refund_date.year
+        click_button 'Next'
+
+        choose :application_min_threshold_exceeded_true
+        fill_in :application_amount, with: 3500
+        click_button 'Next'
+      end
+
+      scenario 'the summary page shows the benefit data' do
+        refund_date_text = refund_date.strftime('%-d %B %Y')
+        expect(page).to have_xpath('//h2', text: 'Check details')
+        expect(page).to have_content("Date fee paid#{refund_date_text}")
+
+        click_link 'Change application details'
+
+        expect(page).to have_xpath(".//input[@id='application_day_date_fee_paid'][@value='#{refund_date.day}']")
+        expect(page).to have_xpath(".//input[@id='application_month_date_fee_paid'][@value='#{refund_date.month}']")
+        expect(page).to have_xpath(".//input[@id='application_year_date_fee_paid'][@value='#{refund_date.year}']")
+
+        fill_in 'application_day_date_fee_paid', with: new_refund_date.day
+        fill_in 'application_month_date_fee_paid', with: new_refund_date.month
+        fill_in 'application_year_date_fee_paid', with: new_refund_date.year
+
+        click_button 'Next'
+        click_button 'Next'
+
+        new_refund_date_text = new_refund_date.strftime('%-d %B %Y')
+        expect(page).to have_content("Date fee paid#{new_refund_date_text}")
+      end
+    end
+
     context 'does not exceed the savings threshold' do
       before do
+        application_details_step
         choose 'application_min_threshold_exceeded_false'
         click_button 'Next'
       end
@@ -188,4 +279,17 @@ RSpec.feature 'The result is shown on the confirmation page', type: :feature do
       end
     end
   end
+
+  # rubocop:disable Metrics/AbcSize
+  def application_details_step
+    expect(page).to have_xpath('//h2', text: 'Application details')
+    fill_in 'application_fee', with: '300'
+    find(:xpath, '(//input[starts-with(@id,"application_jurisdiction_id_")])[1]').click
+    fill_in 'application_day_date_received', with: date_received.day
+    fill_in 'application_month_date_received', with: date_received.month
+    fill_in 'application_year_date_received', with: date_received.year
+    fill_in 'Form number', with: 'ABC123'
+    click_button 'Next'
+  end
+  # rubocop:enable Metrics/AbcSize
 end
