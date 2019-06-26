527220a84c3bb7b0244f66b0fe046c7a03a1fbfe
Petr Zaparka <petr@zaparka.cz>
2019-06-26 14:40:02 +0100

    Fixing rubocop complains

--- spec/repositories/application_form_repository_spec.rb
+++ spec/repositories/application_form_repository_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe ApplicationFormRepository do
   end
   let(:application_details_form) do
     instance_double('Forms::Application::Detail',
-      errors: errors, discretion_applied: discretion_applied, fee: fee)
+                    errors: errors, discretion_applied: discretion_applied, fee: fee)
   end
 
   let(:discretion_applied) { nil }
