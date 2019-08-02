1bcab42d183f6eef904e875d73a74eafd4868667
Alexa <alexa.ballantine@gmail.com>
2019-08-02 12:39:19 +0100

    full test for processing a paper application

--- performance-tests/src/test/scala/simulations/StaffAppPerformance.scala
+++ performance-tests/src/test/scala/simulations/StaffAppPerformance.scala
@@ -21,7 +21,7 @@ with HttpConfiguration
         .check(css("input[name='authenticity_token']", "value").saveAs("csrfCookie")))
 
     .exec(http("Sign in")
-        .get("/users/sign_in")
+        .put("/users/sign_in")
         .formParam("user[email]", "alexa.ballantine+1@digital.justice.gov.uk")
         .formParam("user[password]", "123456789")
         .formParam("authenticity_token", session => {
@@ -37,17 +37,64 @@ with HttpConfiguration
         .formParam("application[month_date_of_birth]", "02")
         .formParam("application[year_date_of_birth]", "1974")
         .formParam("application[married]", "false")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
         .check(status.is(200)))
 
     .exec(http("Paper application - Application details")
-        .get("/applications/733032/personal_informations")
+        .get("/applications/733032/details")
         .formParam("application[fee]", "500")
-        .formParam("application[jurisdiction_id]", "3")
+        .formParam("application[jurisdiction_id]", "1")
         .formParam("application[day_date_received]", "01")
         .formParam("application[month_date_received]", "06")
         .formParam("application[year_date_received]", "2019")
         .formParam("application[form_name]", "C100")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
         .check(status.is(200)))
+
+    .exec(http("Paper application - Savings and investments")
+        .get("/applications/733032/savings_investments")
+        .formParam("application[min_threshold_exceeded]", "false")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
+        .check(status.is(200)))
+
+    .exec(http("Paper application - Benefits")
+        .get("/applications/7330321/benefits")
+        .formParam("application[benefits]", "false")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
+        .check(status.is(200)))
+
+    .exec(http("Paper application - Income")
+        .get("/applications/7330321/incomes")
+        .formParam("application[dependents]", "false")
+        .formParam("application[income]", "500")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
+        .check(status.is(200)))
+
+    .exec(http("Paper application - Summary")
+        .get("/applications/7330321/summary")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
+        .check(status.is(200)))
+
+    .exec(http("Paper application - Confirmation")
+        .get("/applications/7330321/confirmation")
+        .formParam("authenticity_token", session => {
+              session("csrfCookie").as[String]
+        })
+        .check(status.is(200)))
+
+
 
   val userCount = conf.getInt("users")
   val durationInSeconds  = conf.getLong("duration")
