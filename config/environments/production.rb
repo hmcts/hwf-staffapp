f633e3985822e639f092620a3f7bab6a808edb79
Leonidas Apostolidis <leoapost@gmail.com>
2019-09-09 12:51:18 +0100

    Disable live asset compilation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
+
   config.assets.digest = true
 
   # `config.assets.precompile` and `config.assets.version`
