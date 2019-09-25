6300c6d7002bcbc8856cc5437855017f8aeac735
Leonidas Apostolidis <leoapost@users.noreply.github.com>
2019-09-25 08:35:02 +0100

    Update the Benefits question to be more accessible (#948)

--- features/step_definitions/benefits_steps.rb
+++ features/step_definitions/benefits_steps.rb
@@ -1,13 +1,14 @@
 Given("I am on the benfits part of the application") do
   benefits_page.go_to_benefits_page
-  expect(current_path).to include 'benefits'
+  expect(current_path).to end_with 'benefits'
   expect(benefits_page.content).to have_header
 end
 
 When("I answer yes to the benefits question") do
+  expect(benefits_page.content).to have_benefit_question
   benefits_page.submit_benefits_yes
 end
 
 Then("I should be asked about paper evidence") do
-  expect(current_path).to include 'benefit_override/paper_evidence'
+  expect(current_path).to end_with 'benefit_override/paper_evidence'
 end
