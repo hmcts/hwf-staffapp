b88cf16db11273980414b19f37ce0814e6cd73a6
Tomasz Cwik <47426645+tomaszcwikcs@users.noreply.github.com>
2019-10-09 14:28:10 +0100

    RST-2250 DB migration in a correct dir (#958)

--- run.sh
+++ run.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 
-case ${DOCKER_STATE} in
-migrate)
-    echo "running migrate"
-    bundle exec rake db:migrate
-    ;;
-seed)
-    echo "running seed"
-    bundle exec rake db:migrate
-    bundle exec rake db:seed
-    ;;
-vagrant)
-    echo "running vagrant"
-    bundle exec rake db:create
-    bundle exec rake db:migrate
-    bundle exec rake db:seed
-    ;;
-create)
-    echo "running create"
-    bundle exec rake db:create
-    bundle exec rake db:migrate
-    bundle exec rake db:seed
-    ;;
-setup)
-    echo 'running setup'
-    bundle exec rake db:setup
-    ;;
-esac
-
 ROLE1="${ROLE:-app}"
 PHUSION_SERVICE="${PHUSION:-false}"
 case ${PHUSION_SERVICE} in
 true)
     echo "running as service"
     cd /home/app/
+    case ${DOCKER_STATE} in
+    migrate)
+        echo "running migrate"
+        bundle exec rake db:migrate
+        ;;
+    seed)
+        echo "running seed"
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    vagrant)
+        echo "running vagrant"
+        bundle exec rake db:create
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    create)
+        echo "running create"
+        bundle exec rake db:create
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    setup)
+        echo 'running setup'
+        bundle exec rake db:setup
+        ;;
+    esac
         case ${ROLE1} in
         worker)
             echo "running worker"
@@ -46,15 +45,42 @@ true)
         esac
     ;;
 *)
-    case ${ROLE1} in
-    worker)
-        echo "running worker"
-        bundle exec rake jobs:work
+    case ${DOCKER_STATE} in
+    migrate)
+        echo "running migrate"
+        bundle exec rake db:migrate
         ;;
-    *)
-        echo "running app"
-        bundle exec unicorn -p ${UNICORN_PORT:-8080} -c config/unicorn.rb -E ${RAILS_ENV:-production}
+    seed)
+        echo "running seed"
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    vagrant)
+        echo "running vagrant"
+        bundle exec rake db:create
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    create)
+        echo "running create"
+        bundle exec rake db:create
+        bundle exec rake db:migrate
+        bundle exec rake db:seed
+        ;;
+    setup)
+        echo 'running setup'
+        bundle exec rake db:setup
         ;;
     esac
+        case ${ROLE1} in
+        worker)
+            echo "running worker"
+            bundle exec rake jobs:work
+            ;;
+        *)
+            echo "running app"
+            bundle exec unicorn -p ${UNICORN_PORT:-8080} -c config/unicorn.rb -E ${RAILS_ENV:-production}
+            ;;
+        esac
     ;;
 esac
