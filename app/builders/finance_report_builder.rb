8d23975b9261878279a1874bd2aeaf727de22114
Petr Zaparka <petr@zaparka.cz>
2019-10-29 16:03:17 +0000

    RST-1703 report filters (#957)
    * Adding filters to finance report page
    
    * Adding filters field to report generator page
    
    * Adding filters to finance report builder
    
    * Fixing failing tests
    
    clash with rails 6 upgrade. Fixing formatting.
    
    * Updating spec for report controller
    
    * Filters for finance transactional report
    
    * Filters for finance transactional report
    
    * Updating translations and form tags
    
    * Updating the layout for report form
    
    * Fixing issue of missing date from inputs
    
    When form was reloaded, due to an error, you lost the date you put in.
    
    * rst-1703 cucumber tests for filters

--- app/builders/finance_report_builder.rb
+++ app/builders/finance_report_builder.rb
@@ -20,9 +20,10 @@ class FinanceReportBuilder
     none_sum: 'granted basis amount'
   }.freeze
 
-  def initialize(start_date, end_date)
+  def initialize(start_date, end_date, filters = {})
     @date_from = DateTime.parse(start_date.to_s).utc
     @date_to = DateTime.parse(end_date.to_s).utc.end_of_day
+    @filters = filters
   end
 
   def to_csv
@@ -62,10 +63,45 @@ class FinanceReportBuilder
   end
 
   def distinct_offices_jurisdictions
+    list = offices_jurisdictions_query
+    list = filtered_query(list)
+    list.distinct { |s| s.values_at(:office, :jurisdiction) }
+  end
+
+  def offices_jurisdictions_query
     BusinessEntity.
       exclude_hq_teams.
       joins('LEFT JOIN applications ON business_entity_id = business_entities.id').
       where('decision_date BETWEEN :d1 AND :d2', d1: @date_from, d2: @date_to).
-      where('applications.state = 3').distinct { |s| s.values_at(:office, :jurisdiction) }
+      where('applications.state = 3')
+  end
+
+  def filtered_query(list)
+    return list if @filters.blank?
+    list = list.where(be_code: be_code_filter) if be_code_filter
+    list = list.where(jurisdiction_id: jurisdiction_filter) if jurisdiction_filter
+    list = list.where('applications.application_type = ?', app_type_filter) if app_type_filter
+
+    if refund_filter
+      list = list.joins('LEFT JOIN details ON applications.id = details.application_id').
+             where('details.refund = true')
+    end
+    list
+  end
+
+  def be_code_filter
+    @filters[:be_code]
+  end
+
+  def jurisdiction_filter
+    @filters[:jurisdiction_id]
+  end
+
+  def refund_filter
+    @filters[:refund]
+  end
+
+  def app_type_filter
+    @filters[:application_type]
   end
 end
--- spec/builders/finance_transactional_report_builder_spec.rb
+++ spec/builders/finance_transactional_report_builder_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe FinanceTransactionalReportBuilder do
-  subject(:frb) { described_class.new(start_date, end_date) }
+  subject(:frb) { described_class.new(start_date, end_date, filters) }
+
+  let(:filters) { {} }
 
   let(:current_time) { Time.zone.parse('2019-01-30 15:50:10') }
   let(:start_date) { Time.zone.parse('2018-1-05 12:30:40') }
   let(:end_date) { Time.zone.parse('2018-10-10 16:35:00') }
+
+  let(:jurisdiction1) { create :jurisdiction }
+  let(:jurisdiction2) { create :jurisdiction }
+  let(:business_entity1) { create :business_entity, be_code: 'abc134', jurisdiction: jurisdiction1 }
+  let(:business_entity2) { create :business_entity, be_code: 'efg142', jurisdiction: jurisdiction2 }
+  let(:business_entity3) { create :business_entity, be_code: 'hjk122', jurisdiction: jurisdiction1 }
+  let(:business_entity4) { create :business_entity, be_code: 'mop345', jurisdiction: jurisdiction2 }
 
   describe '#to_csv' do
     subject do
@@ -34,6 +43,93 @@ RSpec.describe FinanceTransactionalReportBuilder do
       application = create(:application_full_remission, :with_office, :with_business_entity, :processed_state, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
 
       is_expected.to include(application.reference)
+    end
+
+    context 'filters' do
+      context 'be_code' do
+        let(:filters) { { be_code: business_entity2.be_code } }
+
+        it 'contains data for distinct business entities' do
+          application1 = create(:application_full_remission, :with_office, :processed_state, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+          application2 = create(:application_full_remission, :with_office, :processed_state, business_entity_id: business_entity2.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+
+          is_expected.not_to include(application1.reference)
+          is_expected.to include(application2.reference)
+        end
+      end
+
+      context 'jurisdiction_id' do
+        let(:filters) { { jurisdiction_id: jurisdiction1.id } }
+
+        it 'contains data for distinct business entities' do
+          application1 = create(:application_full_remission, :with_office, :processed_state, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+          application2 = create(:application_full_remission, :with_office, :processed_state, business_entity_id: business_entity2.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+
+          is_expected.to include(application1.reference)
+          is_expected.not_to include(application2.reference)
+        end
+      end
+
+      context 'refund' do
+        let(:filters) { { refund: true } }
+
+        it 'contains data for distinct business entities' do
+          application1 = create(:application_full_remission, :with_office, :processed_state, :refund, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+          application2 = create(:application_full_remission, :with_office, :processed_state, fee: 500, business_entity_id: business_entity2.id, decision: 'full', decision_date: start_date + 10.seconds)
+
+          is_expected.to include(application1.reference)
+          is_expected.not_to include(application2.reference)
+        end
+      end
+
+      context 'application_type' do
+        context 'benefit' do
+          let(:filters) { { application_type: 'benefit' } }
+
+          it 'contains data for distinct business entities' do
+            application1 = create(:application_full_remission, :with_office, :processed_state, :income_type, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+            application2 = create(:application_full_remission, :with_office, :processed_state, :benefit_type, fee: 500, business_entity_id: business_entity2.id, decision: 'full', decision_date: start_date + 10.seconds)
+
+            is_expected.not_to include(application1.reference)
+            is_expected.to include(application2.reference)
+          end
+        end
+
+        context 'income' do
+          let(:filters) { { application_type: 'income' } }
+
+          it 'contains data for distinct business entities' do
+            application1 = create(:application_full_remission, :with_office, :processed_state, :income_type, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+            application2 = create(:application_full_remission, :with_office, :processed_state, :benefit_type, fee: 500, business_entity_id: business_entity2.id, decision: 'full', decision_date: start_date + 10.seconds)
+
+            is_expected.to include(application1.reference)
+            is_expected.not_to include(application2.reference)
+          end
+        end
+      end
+
+      context 'all filters' do
+        let(:filters) {
+          {
+            application_type: 'income',
+            refund: true,
+            jurisdiction_id: jurisdiction1.id,
+            be_code: business_entity3.be_code
+          }
+        }
+
+        it 'contains data for distinct business entities' do
+          application1 = create(:application_full_remission, :with_office, :processed_state, :income_type, business_entity_id: business_entity1.id, fee: 500, decision: 'full', decision_date: start_date + 10.seconds)
+          application2 = create(:application_full_remission, :with_office, :processed_state, :benefit_type, fee: 500, business_entity_id: business_entity2.id, decision: 'full', decision_date: start_date + 10.seconds)
+          application3 = create(:application_full_remission, :with_office, :processed_state, :income_type, :refund, fee: 500, business_entity_id: business_entity3.id, decision: 'full', decision_date: start_date + 10.seconds)
+          application4 = create(:application_full_remission, :with_office, :processed_state, :benefit_type, fee: 500, business_entity_id: business_entity4.id, decision: 'full', decision_date: start_date + 10.seconds)
+
+          is_expected.not_to include(application1.reference)
+          is_expected.not_to include(application2.reference)
+          is_expected.to include(application3.reference)
+          is_expected.not_to include(application4.reference)
+        end
+      end
     end
   end
 end
