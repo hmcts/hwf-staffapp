0c7757ade078d71090893d03b21f319d217c41a0
Petr Zaparka <petr@zaparka.cz>
2019-11-07 11:25:44 +0000

    Rounding income value with js
    It's alrady rounded in backend so this is more obvious to the user.

--- app/assets/javascripts/income.js
+++ app/assets/javascripts/income.js
@@ -7,12 +7,29 @@ window.moj.Modules.IncomeModule = {
   },
 
   bindEvents: function() {
+    var self = this;
     $('input[id*="application_dependents"]').on('click', function() {
       $('#income-input').show();
+    });
+
+    $('input[id="application_income"]').on('change', function(target){
+      self.roundIncomeValue(target);
+    });
+    $('input[id="evidence_income"]').on('change', function(target){
+      self.roundIncomeValue(target);
     });
   },
 
   checkStateOnLoad: function() {
     $('#income-input').toggle($('input[id*="application_dependents"]').is(':checked'));
-  }
+  },
+
+  roundIncomeValue: function(target) {
+    var income = $(target.currentTarget).val()
+    if(isNaN(income)) {
+      $(target.currentTarget).val('Value is not a number');
+    } else {
+      $(target.currentTarget).val(Math.round(income));
+    }
+  },
 };
