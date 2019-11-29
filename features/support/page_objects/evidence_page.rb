712a05fc8bcc4376c7a9efc9031132a212762658
Petr Zaparka <petr@zaparka.cz>
2019-11-29 11:09:56 +0000

    RST-2320 - Rejection letter is now based on the incorrect_reason value (#976)
    * Rejection letter is now based on the incorrect_reason value
    
    * rst-2320 moved evidence tests

--- features/support/page_objects/evidence_page.rb
+++ features/support/page_objects/evidence_page.rb
@@ -6,17 +6,14 @@ class EvidencePage < BasePage
     element :part_payment, 'h2', text: 'The applicant must pay £205 towards the fee'
     element :evidence_can_not_be_processed, 'summary.govuk-details__summary', text: 'What to do if the evidence can’t be processed'
     element :evidence_deadline, '.govuk-details__text'
-    element :personal_details, 'h2', text: 'Personal details'
-    element :application_details, 'h2', text: 'Application details'
-    element :benefits, 'h2', text: 'Benefits'
-    element :income, 'h2', text: 'Income'
-    element :result, 'h2', text: 'Result'
-    element :processing_summary, 'h2', text: 'Processing summary'
-    section :evidence_summary, '.govuk-summary-list' do
-      element :evidence_header, 'h2', text: 'Evidence'
-      element :change_application_evidence, '.govuk-summary-list__actions', text: 'Change'
-      elements :evidence_answer_key, '.govuk-summary-list__key'
-      elements :evidence_answer_value, '.govuk-summary-list__value'
+    sections :evidence_summary, '.govuk-summary-list' do
+      element :personal_details, 'h2', text: 'Personal details'
+      element :application_details, 'h2', text: 'Application details'
+      element :benefits, 'h2', text: 'Benefits'
+      element :income, 'h2', text: 'Income'
+      element :result, 'h2', text: 'Result'
+      elements :summary_row, '.govuk-summary-list__row'
     end
+    element :processing_summary, 'h2', text: 'Processing summary'
   end
 end
