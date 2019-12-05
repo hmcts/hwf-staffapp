a015758909dbb54a87976d2a2b815f53afc2dbf4
Petr Zaparka <petr@zaparka.cz>
2019-12-05 12:24:30 +0000

    Updating error message copy

--- features/support/page_objects/base_page.rb
+++ features/support/page_objects/base_page.rb
@@ -2,6 +2,6 @@ class BasePage < SitePrism::Page
   section :content, '#content' do
     element :next_button, 'input[value="Next"]'
     element :complete_processing_button, 'input[value="Complete processing"]'
-    element :select_from_list_error, '.error', text: 'Select from at least one of the following options'
+    element :select_from_list_error, '.error', text: 'Select a reason or reasons why you are rejecting the evidence'
   end
 end
