cc6100fba97e67cd845e0e9fded7228c6631c24e
Petr Zaparka <petr@zaparka.cz>
2020-01-10 10:47:07 +0000

    RST-2440 - CCMCC evidence check  5k rule (#1010)
    * Adding the ccmcc evidence check library
    
    And implementing the 5000 rule for non refund checks
    
    * Adding ccmccc_check_type to evidence checks table
    
    * Saving ccmcc check type to evidence check table
    
    When the ccmcc rules applies.
    
    * CCMCC rules now applies to all applications
    
    even the refund ones.

---
+++ app/services/ccmcc_evidence_check_rules.rb
@@ -0,0 +1,29 @@
+class CCMCCEvidenceCheckRules
+  OFFICE_CODE = 'DH403'.freeze
+  FIVE_K_RULE_FREQUENCY = 1
+  FIVE_K_RULE_ANNOTATION = 'over 5 thousand'.freeze
+
+  attr_reader :frequency, :check_type
+
+  def initialize(application)
+    @application = application
+  end
+
+  def rule_applies?
+    return false unless same_office?
+    over_five_thousand?
+  end
+
+  private
+
+  def same_office?
+    @application.office.try(:entity_code) == OFFICE_CODE
+  end
+
+  def over_five_thousand?
+    return false if @application.detail.fee < 5000
+    @frequency = FIVE_K_RULE_FREQUENCY
+    @check_type = FIVE_K_RULE_ANNOTATION
+    true
+  end
+end
