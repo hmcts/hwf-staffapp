7a9d8d871eb91906827fc110866da222303c9851
Petr Zaparka <petr@zaparka.cz>
2020-01-14 12:07:41 +0000

    Adding 2 percent rule (#1017)

--- spec/services/ccmcc_evidence_check_rules_spec.rb
+++ spec/services/ccmcc_evidence_check_rules_spec.rb
@@ -148,19 +148,49 @@ RSpec.describe CCMCCEvidenceCheckRules do
         ccmcc_check_rules.rule_applies?
         expect(ccmcc_check_rules.check_type).to eq('between 100 and 999 non refund')
       end
-
     end
   end
 
   describe 'under 100' do
     context 'refund' do
       let(:application) { create :application, office: ccmcc, fee: 99 }
-      it { expect(ccmcc_check_rules.rule_applies?).to be false }
+      it { expect(ccmcc_check_rules.rule_applies?).to be true }
+
+      it 'check query_type' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.query_type).to be CCMCCEvidenceCheckRules::QUERY_ALL
+      end
+
+      it 'has the frequency of 50 - as 2 percent' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.frequency).to be 50
+      end
+
+      it 'check type value' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.check_type).to eq('under 100')
+      end
     end
 
     context 'not a refund' do
       let(:application) { create :application, :refund, office: ccmcc, fee: 99 }
-      it { expect(ccmcc_check_rules.rule_applies?).to be false }
+      it { expect(ccmcc_check_rules.rule_applies?).to be true }
+
+      it 'check query_type' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.query_type).to be CCMCCEvidenceCheckRules::QUERY_ALL
+      end
+
+      it 'has the frequency of 50 - as 2 percent' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.frequency).to be 50
+      end
+
+      it 'check type value' do
+        ccmcc_check_rules.rule_applies?
+        expect(ccmcc_check_rules.check_type).to eq('under 100')
+      end
+
     end
   end
 
