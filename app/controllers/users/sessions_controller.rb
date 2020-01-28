b8a23dcfa639826997212c212c543dcbd12b0b9b
Petr Zaparka <petr@zaparka.cz>
2020-01-28 15:58:14 +0000

    Updating sign out url redirection

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,5 +6,11 @@ module Users
       @notification = Notification.first
       super
     end
+
+    private
+
+    def after_sign_out_path_for(resource_or_scope)
+      new_user_session_path
+    end
   end
 end
