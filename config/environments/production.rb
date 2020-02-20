7c5b949a67cd4397ba0e121ad9733449fd6d95fb
Petr Zaparka <petr@zaparka.cz>
2020-02-20 15:58:31 +0000

    disable automatic disabling buttons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "fr_staffapp_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_view.automatically_disable_submit_tag = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
