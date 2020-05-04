7afb9c2f5324cca3febc781fb2313ca46b3bc7a5
Petr Zaparka <petr@zaparka.cz>
2020-05-04 11:23:31 +0100

    Part payment banner fix
    When there was an evidence check and part payement the part payment banner was not working correctly.

--- app/models/views/application_result.rb
+++ app/models/views/application_result.rb
@@ -12,8 +12,19 @@ module Views
     end
 
     def amount_to_pay
-      if outcome_from.amount_to_pay.present?
+      if outcome_from.is_a?(PartPayment)
+        to_pay = amount_to_pay_for_part_payment
+        "£#{parse_amount_to_pay(to_pay)}"
+      elsif outcome_from.amount_to_pay.present?
         "£#{parse_amount_to_pay(outcome_from.amount_to_pay)}"
+      end
+    end
+
+    def amount_to_pay_for_part_payment
+      if @application.evidence_check
+        @application.evidence_check.amount_to_pay
+      else
+        @application.amount_to_pay
       end
     end
 
@@ -42,6 +53,8 @@ module Views
       case outcome_from
       when EvidenceCheck
         outcome_from.outcome
+      when PartPayment
+        outcome_from_application
       when Application
         outcome_from_application
       end
@@ -62,7 +75,7 @@ module Views
     end
 
     def outcome_from
-      @application.evidence_check || @application
+      @application.part_payment || @application.evidence_check || @application
     end
 
     def format_locale(suffix)
