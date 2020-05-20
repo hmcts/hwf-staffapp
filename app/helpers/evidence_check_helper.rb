39ee31901d8f846abaf112fe02e570aca4c2bf35
Petr Zaparka <petr@zaparka.cz>
2020-05-20 10:34:16 +0100

    Updating income rejection letter

--- app/helpers/evidence_check_helper.rb
+++ app/helpers/evidence_check_helper.rb
@@ -3,4 +3,11 @@ module EvidenceCheckHelper
     thresholds = IncomeThresholds.new(application.applicant.married?, application.children)
     thresholds.max_threshold
   end
+
+  def income_increase?(application)
+    if application.income.positive? && application.evidence_check.income.positive?
+      return true if application.income < application.evidence_check.income
+    end
+    false
+  end
 end
