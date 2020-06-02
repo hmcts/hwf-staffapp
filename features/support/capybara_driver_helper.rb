6e3482e5fa6c85fdecbd1d342ea6aff3a8601754
Alexa <alexa.ballantine@gmail.com>
2020-06-02 14:50:11 +0100

    fix for rubocop error

--- features/support/capybara_driver_helper.rb
+++ features/support/capybara_driver_helper.rb
@@ -11,7 +11,7 @@ Capybara.configure do |config|
 end
 
 Capybara.register_driver :headless do |app|
-  chrome_options = Selenium::WebDriver::Chrome::Options.new(args: %w[headless disable-gpu])
+  chrome_options = Selenium::WebDriver::Chrome::Options.new(args: ['headless', 'disable-gpu'])
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
 end
 
