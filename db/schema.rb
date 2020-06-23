7b195b0ea68f42ce3eff3b2294e5468a3c92b780
Petr Zaparka <petr@zaparka.cz>
2020-06-23 07:30:31 +0100

    Adding API response column to benefit checks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_21_073854) do
+ActiveRecord::Schema.define(version: 2020_06_23_062753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_05_21_073854) do
     t.integer "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string "api_response"
     t.index ["application_id"], name: "index_benefit_checks_on_application_id"
     t.index ["user_id"], name: "index_benefit_checks_on_user_id"
   end
