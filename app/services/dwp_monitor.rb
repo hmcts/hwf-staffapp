d9a5354c6cdf7525ca5dcfb6b9eb50237fb15ee1
Petr Zaparka <petr@zaparka.cz>
2020-06-25 11:35:48 +0100

    RST-2716 - Reducing number of application to be checked (#1107)
    so the DWP monitor reacts more quickly

--- app/services/dwp_monitor.rb
+++ app/services/dwp_monitor.rb
@@ -17,7 +17,7 @@ class DwpMonitor
   private
 
   def dwp_results
-    @checks = BenefitCheck.order('id desc').limit(20).pluck(:dwp_result, :error_message)
+    @checks = BenefitCheck.order('id desc').limit(10).pluck(:dwp_result, :error_message)
   end
 
   def percent
@@ -30,8 +30,16 @@ class DwpMonitor
 
   def error_total
     internal_server_error = @checks.flatten.count('500 Internal Server Error').to_f
-    bad_request = @checks.flatten.count('BadRequest').to_f
+    bad_request = bad_request_count.to_f
     server_broke = @checks.flatten.count('Server broke connection').to_f
     bad_request + server_broke + internal_server_error
+  end
+
+  def bad_request_count
+    # i = 0
+    @checks.count do |a|
+      next if a[0] != 'BadRequest'
+      a[1].include?('LSCBC959')
+    end
   end
 end
