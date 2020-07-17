fd5b818c21e155cd130f2116372e44f4f149b73d
Alexa <alexa.ballantine@gmail.com>
2020-07-17 13:14:29 +0100

    added visible false to Look up button

--- features/step_definitions/process_online_application_steps.rb
+++ features/step_definitions/process_online_application_steps.rb
@@ -4,7 +4,7 @@ Given("I have looked up an online application") do
   sign_in_page.user_account
   reference = OnlineApplication.last.reference
   fill_in 'Reference', with: reference
-  click_on 'Look up'
+  click_on 'Look up', visible: false
 end
 
 When("I see the application details") do
