b9792d33f0e7b0a5fbab950049a1861bb4caf8c1
Petr Zaparka <petr@zaparka.cz>
2020-07-24 08:58:59 +0100

    Finding link before opening it in new window

--- features/step_definitions/confirmation_steps.rb
+++ features/step_definitions/confirmation_steps.rb
@@ -39,6 +39,8 @@ Then("I should see the next steps") do
 end
 
 When("I can view the guides in a new window") do
+  guides_link = page.find_link('See the guides')
+  expect(guides_link['target']).to eql('blank')
   new_window = window_opened_by { click_on 'See the guides', visible: false }
   within_window new_window do
     expect(guide_page).to be_displayed
