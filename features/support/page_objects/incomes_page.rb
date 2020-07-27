f3185c67472c8c4a6531f454da51f366c845e154
Petr Zaparka <petr@zaparka.cz>
2020-07-27 14:31:39 +0100

    Waiting for form to load
    on income page

--- features/support/page_objects/incomes_page.rb
+++ features/support/page_objects/incomes_page.rb
@@ -18,20 +18,24 @@ class IncomesPage < BasePage
   end
 
   def submit_incomes_50
+    incomes_page.content.wait_until_question_visible
     find_field('Total monthly income', visible: false).set('50')
     next_page
   end
 
   def submit_incomes_1200
+    incomes_page.content.wait_until_question_visible
     find_field('Total monthly income', visible: false).set('1200')
     next_page
   end
 
   def submit_incomes_no
+    incomes_page.content.wait_until_question_visible
     incomes_page.content.radio[0].click
   end
 
   def submit_incomes_yes_3
+    incomes_page.content.wait_until_question_visible
     incomes_page.content.radio[1].click
     find_field('Number of children', visible: false).set('3')
     find_field('Total monthly income', visible: false).set('1900')
