cf62f780456ef005d1ac4e76d4b68dcd077e7c13
Petr Zaparka <petr@zaparka.cz>
2020-08-28 12:53:02 +0100

    Adding method for checking benefit check reusult
    to check if DWP bad response is involved

--- app/models/application.rb
+++ app/models/application.rb
@@ -81,4 +81,10 @@ class Application < ActiveRecord::Base
     days = Settings.part_payment.expires_in_days
     Time.zone.today + days
   end
+
+  def failed_because_dwp_error?
+    return false if self.benefit_checks.blank?
+    benefit_check = benefit_checks.last
+    benefit_check.dwp_result == 'BadRequest' && benefit_check.error_message.include?('LSCBC959: Service unavailable')
+  end
 end
