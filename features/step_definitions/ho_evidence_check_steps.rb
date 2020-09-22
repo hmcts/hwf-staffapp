d4eb434070592451a3928d55cc8d25186a360a87
Shaun Francis <shaun@mapingo.co.uk>
2020-09-22 16:16:18 +0100

    Feature/rst 2660 rejection letter cucumber (#1135)
    * Implemented cucumber for rst 2660
    
    * We don’t need to create 40 users
    
    * squashed commit: new scenerio for income rejection and page load waiting
    
    Co-authored-by: AhsanZX97 <ahsan97@hotmail.co.uk>
    Co-authored-by: Petr Zaparka <petr@zaparka.cz>

--- features/step_definitions/ho_evidence_check_steps.rb
+++ features/step_definitions/ho_evidence_check_steps.rb
@@ -1,13 +1,20 @@
 
 When("a second application is processed with the same home office number") do
   dashboard_page.process_application
+  expect(personal_details_page).to have_current_path(%r{/personal_informations})
   personal_details_page.submit_all_personal_details_ho
+  expect(application_details_page).to have_current_path(%r{/details})
   application_details_page.submit_as_refund_case
+  expect(savings_investments_page).to have_current_path(%r{/savings_investments})
   savings_investments_page.submit_less_than
+  expect(benefits_page).to have_current_path(%r{/benefits})
   benefits_page.submit_benefits_no
+  expect(incomes_page).to have_current_path(%r{/incomes})
   incomes_page.submit_incomes_no
   incomes_page.submit_incomes_50
+  expect(page).to have_current_path(%r{/summary})
   complete_and_back_to_start
+  expect(page).to have_current_path('/')
 end
 
 Then("the first application will be processed") do
--- features/support/page_objects/part_payment_page.rb
+++ features/support/page_objects/part_payment_page.rb
@@ -2,6 +2,7 @@ class PartPaymentPage < BasePage
   section :content, '#content' do
     element :no, 'label', text: 'No'
     element :yes, 'label', text: 'Yes'
+    element :header, 'h1', text: 'Is the part-payment ready to process?'
     element :evidence_confirmation_letter, '.evidence-confirmation-letter', text: 'We have received your part-payment towards your fee. However we are unable to accept it because:'
     element :part_payment_fee, '#result h2', text: 'The applicant must pay £40 towards the fee'
   end
