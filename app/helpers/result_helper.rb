a3ccc378f310a0e604176d70fd1230b058c0dd9e
Petr Zaparka <petr@zaparka.cz>
2020-09-23 08:00:43 +0100

    RST-2660 income rejection letter (#1133)
    * Saving income threshold exceeded if they can pay not just if the income is above threshold
    
    * Displaying income based rejection letter if income threshold exceeded
    
    * Adding income value if the max threshold exceeded calculated on income_max_threshold
    
    * Implemented cucumber for rst 2660
    
    * squashed commit: new scenerio for income rejection and page load waiting
    
    Co-authored-by: Shaun Francis <shaun@mapingo.co.uk>
    Co-authored-by: AhsanZX97 <ahsan97@hotmail.co.uk>

---
+++ app/helpers/result_helper.rb
@@ -0,0 +1,32 @@
+module ResultHelper
+
+  def display_savings?(application)
+    application.detail.discretion_applied != false
+  end
+
+  def display_savings_failed_letter?(application)
+    return false if application.saving.blank?
+    !application.saving.passed?
+  end
+
+  def display_income_failed_letter?(application)
+    return false if application.income_max_threshold_exceeded.nil?
+    application.income_max_threshold_exceeded
+  end
+
+  def display_benefit_failed_letter?(application)
+    checks = application.benefit_checks
+    return false if application.benefits != true || checks.blank?
+    !checks.last.benefits_valid?
+  end
+
+  def income_value(application)
+    return number_to_currency(application.income, precision: 2) unless application.income.nil?
+    if application.income_min_threshold_exceeded == true &&
+       application.income_max_threshold_exceeded == true
+      income = number_to_currency(application.income_max_threshold, precision: 0)
+      return "#{income} or more"
+    end
+  end
+
+end
--- app/services/income_calculation_runner.rb
+++ app/services/income_calculation_runner.rb
@@ -6,13 +6,18 @@ class IncomeCalculationRunner
   def run
     income_calculation_result = IncomeCalculation.new(@application).calculate
     if income_calculation_result
-      @application.update(
-        application_type: 'income',
-        outcome: income_calculation_result[:outcome],
-        amount_to_pay: income_calculation_result[:amount_to_pay],
-        income_min_threshold: income_calculation_result[:min_threshold],
-        income_max_threshold: income_calculation_result[:max_threshold]
-      )
+      update_application(income_calculation_result)
     end
+  end
+
+  def update_application(income_calculation_result)
+    @application.update(
+      application_type: 'income',
+      outcome: income_calculation_result[:outcome],
+      amount_to_pay: income_calculation_result[:amount_to_pay],
+      income_min_threshold: income_calculation_result[:min_threshold],
+      income_max_threshold: income_calculation_result[:max_threshold],
+      income_max_threshold_exceeded: income_calculation_result[:income_max_threshold_exceeded]
+    )
   end
 end
