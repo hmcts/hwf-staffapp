b1f5f431226d12a23962aeacebea630396f7d756
Daniel Bell <daniel.bell@solirius.com>
2020-10-14 09:17:33 +0100

    Fix rubocop problems

--- features/step_definitions/override_steps.rb
+++ features/step_definitions/override_steps.rb
@@ -1,14 +1,14 @@
 Given("I have completed an ineligible paper application") do
   expect(dashboard_page).to have_current_path('/')
   dashboard_page.process_application
-  expect(personal_details_page).to have_current_path(%r{personal_informations})
+  expect(personal_details_page).to have_current_path(/personal_informations/)
   personal_details_page.submit_all_personal_details_ni
-  expect(application_details_page).to have_current_path(%r{details})
+  expect(application_details_page).to have_current_path(/details/)
   application_details_page.submit_fee_100
-  expect(savings_investments_page).to have_current_path(%r{savings_investments})
+  expect(savings_investments_page).to have_current_path(/savings_investments/)
   savings_investments_page.submit_more_than
   savings_investments_page.submit_exact_amount
-  expect(summary_page).to have_current_path(%r{/summary})
+  expect(summary_page).to have_current_path(/summary/)
   complete_processing
   expect(confirmation_page.content).to have_ineligible
 end
@@ -24,11 +24,11 @@ When("Click Update application without selecting an option") do
 end
 
 Then("I should see an error telling me to select an option") do
-  expect(confirmation_page).to have_content(%r{Please select a reason for granting help with fees})
+  expect(confirmation_page).to have_content(/Please select a reason for granting help with fees/)
 end
 
 Then("I should see an error telling me to enter a reason for granting help with fees") do
-  expect(confirmation_page).to have_content(%r{Please enter a reason for granting help with fees})
+  expect(confirmation_page).to have_content(/Please enter a reason for granting help with fees/)
 end
 
 When("I check the Other option") do
@@ -73,4 +73,3 @@ end
 Then("I should not see a message telling me the application passed by manager's decision") do
   expect(confirmation_page.content).not_to have_passed_by_manager
 end
-
