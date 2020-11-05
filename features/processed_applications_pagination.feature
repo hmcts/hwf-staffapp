6e225f1fd13910754ade590ffc8a015c5d61fc1a
Daniel Bell <daniel.bell@solirius.com>
2020-11-05 14:22:14 +0000

    updated cucumber tests for no per page functionality

--- features/processed_applications_pagination.feature
+++ features/processed_applications_pagination.feature
@@ -3,7 +3,6 @@ Feature: Pagination on Processed applications page
   Background: Signed in and many applications on Processed applications page
     Given I am signed in as a user that has 50 processed applications
     And I click on processed applications
-    And I click 15 per page
 
   Scenario: Next page and Previous page functionality
     When I click Next page button
@@ -12,7 +11,7 @@ Feature: Pagination on Processed applications page
 
   Scenario: Go directly to last page from first page
     When I click on the number representing the last page
-    Then I should be on page 4
+    Then I should be on page 3
 
   Scenario: Go directly to first page from last page
     When I click on the number representing the last page
--- features/step_definitions/processed_applications_pagination_steps.rb
+++ features/step_definitions/processed_applications_pagination_steps.rb
@@ -5,34 +5,29 @@ Given("I am signed in as a user that has 50 processed applications") do
   expect(dashboard_page).to have_current_path('/')
 end
 
-Given("I click 15 per page") do
-  processed_applications_page.select_fifteen_per_page
-  expect(processed_applications_page).to have_current_path(/per_page=15/)
-end
-
 When("I click on the number representing the last page") do
   processed_applications_page.click_last_page_number
-  expect(processed_applications_page).to have_current_path(/page=4/)
-  expect(processed_applications_page.content.which_page.text).to eq 'Page 4 of 4'
+  expect(processed_applications_page).to have_current_path(/page=3/)
+  expect(processed_applications_page.content.which_page.text).to eq 'Page 3 of 3'
 end
 
 When("I click on the number representing the first page") do
   processed_applications_page.click_first_page_number
 end
 
-Then("I should be on page 4") do
-  expect(processed_applications_page).to have_current_path(/page=4/)
-  expect(processed_applications_page.content.which_page.text).to eq 'Page 4 of 4'
+Then("I should be on page 3") do
+  expect(processed_applications_page).to have_current_path(/page=3/)
+  expect(processed_applications_page.content.which_page.text).to eq 'Page 3 of 3'
 end
 
 Then("I should be on page 1") do
   expect(processed_applications_page).to have_current_path(/page=1/)
-  expect(processed_applications_page.content.which_page.text).to eq 'Page 1 of 4'
+  expect(processed_applications_page.content.which_page.text).to eq 'Page 1 of 3'
 end
 
 When("I click Next page button") do
   processed_applications_page.click_next_page_button
-  expect(processed_applications_page.content.which_page.text).to eq 'Page 2 of 4'
+  expect(processed_applications_page.content.which_page.text).to eq 'Page 2 of 3'
 end
 
 When("I click Previous page button") do
