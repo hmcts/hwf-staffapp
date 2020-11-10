78df1572140ef31c1a8d424e52945ee5032a411b
Petr Zaparka <petr@zaparka.cz>
2020-11-10 15:35:17 +0000

    Updating js triggers for discretion

--- app/assets/javascripts/refund.js
+++ app/assets/javascripts/refund.js
@@ -3,13 +3,14 @@
 window.moj.Modules.RefundModule = {
   init: function() {
     this.bindEvents();
+    this.preload();
   },
 
   bindEvents: function() {
     var self = this;
-    var day = 0;
-    var month = 0;
-    var year = 0;
+    var day = null;
+    var month = null;
+    var year = null;
 
     $('input[id="application_day_date_fee_paid"]').on('keyup', function(t){
       self.loadFeePaidDate();
@@ -17,7 +18,7 @@ window.moj.Modules.RefundModule = {
     $('input[id="application_month_date_fee_paid"]').on('keyup', function(t){
       self.loadFeePaidDate();
     });
-    $('input[id="application_month_date_fee_paid"]').on('keyup', function(t){
+    $('input[id="application_year_date_fee_paid"]').on('keyup', function(t){
       self.loadFeePaidDate();
     });
   },
@@ -27,8 +28,7 @@ window.moj.Modules.RefundModule = {
     // 3 months ago
     latest_date.setMonth(latest_date.getMonth() - 3)
     self.date_paid = new Date(`${month} ${day} ${year}`)
-
-    if(date_paid == 'Invalid Date'){
+    if(day.length < 1 || month.length < 1 || year.length < 1 || date_paid == 'Invalid Date'){
       // invalid date
     } else {
       this.toggleDiscretionBlock();
@@ -46,8 +46,25 @@ window.moj.Modules.RefundModule = {
   loadFeePaidDate: function() {
     self.day = $('input[id="application_day_date_fee_paid"]').val();
     self.month = $('input[id="application_month_date_fee_paid"]').val();
-    self.year = $('input[id="application_year_date_fee_paid"]').val();
+    var year_value = $('input[id="application_year_date_fee_paid"]').val();
+
+    // if you pass just 2 digit for year Date() makes it full year
+    if(year_value.length == 2) {
+      self.year = 0;
+    } else {
+      self.year = year_value;
+    }
+
     this.compareDates();
+  },
+
+  preload: function() {
+    var self = this;
+    if($('input[id="application_day_date_fee_paid"]').length == 1){
+      $('input[id="application_day_date_fee_paid"]').ready(function() {
+        self.loadFeePaidDate();
+      });
+    }
   },
 
 };
