358b941d7233b56f9fb9ad5466b78e79034e31fc
Petr Zaparka <petr@zaparka.cz>
2020-12-14 09:14:04 +0000

    RST-3044 Job cards on guideline (#1178)
    * Job cards on guideline
    
    * updated staffguide feature& steps
    
    * added signed scenario & updated locators
    
    Co-authored-by: rramisetty <ramisR01@heiway.net>

--- features/step_definitions/staff_guides_steps.rb
+++ features/step_definitions/staff_guides_steps.rb
@@ -10,6 +10,10 @@ Then("I can view how to guide") do
   expect(guide_page.content.how_to_guide['href']).to end_with '/documents/2017/10/help-with-fees-how-to-guide.pdf'
 end
 
+Then("I can view the Job Cards") do
+  expect(guide_page.content.job_cards['href']).to end_with '/about-hmcts/my-work/help-with-fees/'
+end
+
 Then("I can view the training course") do
   expect(guide_page.content.training_course['href']).to eq 'https://rise.articulate.com/share/GcGp9iZ04MHyNte5WUue7ukH4dwXlfwK'
 end
@@ -68,6 +72,25 @@ end
 
 Then("I should be taken to the suspected fraud guide") do
   expect(suspected_fraud_guide_page).to have_header
+end
+
+Then("I can view Staff guides link on footer") do
+  expect(sign_in_page.footer).to have_see_the_guides
+end
+
+When("I click on Staff guides link") do
+  sign_in_page.footer.see_the_guides.click
+end
+
+Then("I should be taken to the guide page") do
+  guide_page.load_page
+  expect(guide_page).to be_displayed
+  expect(guide_page.content).to have_guide_header
+end
+
+Then("I will see a Job Cards link") do
+  expect(guide_page.content).to have_job_cards
+  expect(guide_page.content.job_cards['href']).to eql 'https://intranet.justice.gov.uk/about-hmcts/my-work/help-with-fees/'
 end
 
 Then("I can view guides by clicking on the link in the footer") do
