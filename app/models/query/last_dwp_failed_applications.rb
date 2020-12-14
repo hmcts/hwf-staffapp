801817c3a65169dd6950eafe637699cc0e269ab4
Petr Zaparka <petr@zaparka.cz>
2020-12-14 13:52:54 +0000

    Admin sees all dwp pending applicaitons

--- app/models/query/last_dwp_failed_applications.rb
+++ app/models/query/last_dwp_failed_applications.rb
@@ -5,9 +5,13 @@ module Query
     end
 
     def find
-      applications = @user.office.applications.where(benefits: true, state: 0).
-                     where('applications.created_at between ? AND ?', 3.months.ago, Time.zone.now)
-      apps_with_failed_checks(applications)
+      unless @user.admin?
+        applications = @user.office.applications.where(benefits: true, state: 0).
+                       where('applications.created_at between ? AND ?', 3.months.ago, Time.zone.now)
+        apps_with_failed_checks(applications)
+      else
+        dwp_faild_for_admin
+      end
     end
 
     private
@@ -19,6 +23,12 @@ module Query
 
         benefit_check.dwp_result == 'BadRequest' && benefit_check.error_message == 'LSCBC959: Service unavailable.'
       end
+    end
+
+    def dwp_faild_for_admin
+      Application.joins(:benefit_checks).
+        where('applications.created_at between ? AND ?', 3.months.ago, Time.zone.now).
+        where('benefit_checks.dwp_result = ? AND benefit_checks.error_message = ?', 'BadRequest', 'LSCBC959: Service unavailable.')
     end
   end
 end
