66e06df11e2326d42b304958607c548721c62cbd
rramisetty <ramisR01@heiway.net>
2020-12-15 22:18:46 +0000

    added empty form scenario

--- features/step_definitions/your_feedback_steps.rb
+++ features/step_definitions/your_feedback_steps.rb
@@ -20,3 +20,17 @@ Then("I can email if I have an urgent question or something isn't working") do
   expect(feedback_page.content).to have_email_us
   expect(feedback_page.content.email['href']).to eq 'mailto:helpwithfees.feedback@digital.justice.gov.uk'
 end
+
+When("I click on Send feedback") do
+  feedback_page.content.send_feedback_button.click
+end
+
+Then("I should see an error summary message") do
+  expect(feedback_page.content).to have_error_summary
+  expect(feedback_page.content).to have_error_summary_title
+  expect(feedback_page.content).to have_error_summary_list
+end
+
+And("I should see a rating error") do
+  expect(feedback_page.content).to have_rating_error
+end
