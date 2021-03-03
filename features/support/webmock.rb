02c39ae02833be7c1de8966d0889a3da1dd20096
Petr Zaparka <zaparka@petrs-mac-mini.home>
2021-03-03 17:37:40 +0000

    fixing too many connection errors

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -3,4 +3,5 @@ require_relative './capybara_driver_helper'
 
 selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
 app_host_url = URI.parse Capybara.app_host
-WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'ondemand.saucelabs.com', 'chromedriver.storage.googleapis.com'])
+# WebMock.disable_net_connect!(allow_localhost: true, allow: [selenium_url.host, app_host_url.host, 'ondemand.saucelabs.com', 'chromedriver.storage.googleapis.com'])
+WebMock.allow_net_connect!(net_http_connect_on_start: true, allow_localhost: true)
