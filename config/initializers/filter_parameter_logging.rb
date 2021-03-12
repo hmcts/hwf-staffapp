04bd7bbe40448e567c8c63301fef4ec10efa9855
Petr Zaparka <petr@zaparka.cz>
2021-03-12 11:06:02 +0000

    Fixing spacing

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :ni_number, :ho_number, :date_of_birth, :first_name, :last_name,
-  :address, :postcode, :email_address]
+Rails.application.config.filter_parameters += [:password, :ni_number, :ho_number, :date_of_birth, :first_name,
+                                               :last_name, :address, :postcode, :email_address]
