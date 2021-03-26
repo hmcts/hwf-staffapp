ba82d459d9564d4e96e50c967151ece92e3a4c97
Petr Zaparka <petr@zaparka.cz>
2021-03-26 15:41:33 +0000

    Loading the state based on DWP warning

--- spec/controllers/dwp_failed_applications_controller_spec.rb
+++ spec/controllers/dwp_failed_applications_controller_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe DwpFailedApplicationsController do
   let(:office) { create :office }
   let(:user) { create :staff, office: office }
+  let(:dwp_state) { DwpWarning::STATES[:online] }
 
   before do
     sign_in user
@@ -10,6 +11,7 @@ RSpec.describe DwpFailedApplicationsController do
 
   describe 'GET #index' do
     before do
+      allow(controller).to receive(:dwp_checker_state).and_return dwp_state
       allow(LoadApplications).to receive(:load_users_last_dwp_failed_applications).with(user).and_return ['waiting apps']
       get :index
     end
@@ -26,6 +28,16 @@ RSpec.describe DwpFailedApplicationsController do
       it 'loads waiting_for_part_payment application for current user' do
         expect(assigns(:list)).to eql(['waiting apps'])
       end
+    end
+
+    context 'is ready_to_process' do
+      let(:dwp_state) { DwpWarning::STATES[:warning] }
+      it { expect(assigns(:ready_to_process)).to be(true) }
+    end
+
+    context 'is not ready_to_process' do
+      let(:dwp_state) { DwpWarning::STATES[:offline] }
+      it { expect(assigns(:ready_to_process)).to be(false) }
     end
 
     describe 'authorize' do
