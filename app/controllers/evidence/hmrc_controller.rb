384c78955b39864f42c6ad329d87f2ab6593f2f3
Petr Zaparka <petr@zaparka.cz>
2021-09-21 14:03:12 +0100

    RST-3470 hmrc error response (#1250)
    * Displaying next step after income check
    
    * Raising an error when response is empty
    
    * Calcuating income and managing errors
    
    * Updating timeout error message

--- app/controllers/evidence/hmrc_controller.rb
+++ app/controllers/evidence/hmrc_controller.rb
@@ -1,5 +1,6 @@
 module Evidence
   class HmrcController < ApplicationController
+    before_action :load_hmrc_check, only: :show
 
     def new
       authorize evidence
@@ -11,12 +12,17 @@ module Evidence
       @form = Forms::Evidence::HmrcCheck.new(HmrcCheck.new)
       @form.update_attributes(hmrc_params)
 
-      if @form.valid?
-        hmrc_service_call
-        render html: 'good'
+      if @form.valid? && hmrc_service_call
+        redirect_to evidence_check_hmrc_path(evidence, @hmrc_check)
       else
         render :new
       end
+    end
+
+    def show
+      authorize evidence
+      check_hmrc_data
+      render :show
     end
 
     private
@@ -32,8 +38,25 @@ module Evidence
     def hmrc_service_call
       hmrc_service = HmrcApiService.new(evidence.application)
       hmrc_service.income(@form.from_date, @form.to_date)
-      @hmrc_service = hmrc_service.hmrc_check
+      @hmrc_check = hmrc_service.hmrc_check
+    rescue HwfHmrcApiError => e
+      @form.errors.add(:request, e.message)
+      false
+    rescue Net::ReadTimeout
+      message = "HMRC income checking failed. Submit this form for HMRC income checking"
+      @form.errors.add(:timout, message)
+      false
     end
-  end
 
+    def load_hmrc_check
+      @hmrc_check = HmrcCheck.find(params[:id])
+    end
+
+    def check_hmrc_data
+      return if @hmrc_check.total_income != 0
+      message = "There might be an issue with HMRC data. Please contact technical support."
+      @hmrc_check.errors.add(:income_calculation, message)
+    end
+
+  end
 end
--- spec/controllers/evidence/hmrc_controller_spec.rb
+++ spec/controllers/evidence/hmrc_controller_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Evidence::HmrcController, type: :controller do
   let(:applicant) { create :applicant_with_all_details }
   let(:application) { create :application, office: office, applicant: applicant }
   let(:evidence) { create :evidence_check, application_id: application.id }
+  let(:hmrc_check) { create :hmrc_check, evidence_check: evidence }
 
   before do
     allow(EvidenceCheck).to receive(:find).with(evidence.id.to_s).and_return(evidence)
@@ -54,6 +55,7 @@ RSpec.describe Evidence::HmrcController, type: :controller do
     context 'as a signed in user' do
       let(:form) { instance_double(Forms::Evidence::HmrcCheck) }
       let(:api_service) { instance_double(HmrcApiService) }
+      let(:hmrc_check) { instance_double(HmrcCheck) }
 
       let(:dates) {
         {
@@ -80,6 +82,13 @@ RSpec.describe Evidence::HmrcController, type: :controller do
         expect(form).to have_received(:update_attributes).with(dates)
       end
 
+      context 'not valid' do
+        it 'render new page again' do
+          post_call
+          expect(Forms::Evidence::HmrcCheck).to have_received(:new)
+        end
+      end
+
       context 'valid' do
         let(:valid) { true }
         before do
@@ -87,28 +96,129 @@ RSpec.describe Evidence::HmrcController, type: :controller do
           allow(form).to receive(:to_date).and_return '2002-01-03'
           allow(HmrcApiService).to receive(:new).and_return api_service
           allow(api_service).to receive(:income)
-          allow(api_service).to receive(:hmrc_check)
-          post_call
+          allow(api_service).to receive(:hmrc_check).and_return hmrc_check
         end
 
         it 'validate' do
+          post_call
           expect(form).to have_received(:valid?)
         end
 
         describe 'service call' do
-          it "calls service with application" do
-            expect(HmrcApiService).to have_received(:new).with(application)
+          context 'success' do
+            before { post_call }
+
+            it "calls service with application" do
+              expect(HmrcApiService).to have_received(:new).with(application)
+            end
+
+            it "load income" do
+              expect(api_service).to have_received(:income).with('2001-01-03', '2002-01-03')
+            end
+
+            it "load hmrc_check" do
+              expect(api_service).to have_received(:hmrc_check)
+            end
+
+            it "redirects to show page" do
+              expect(response).to redirect_to(evidence_check_hmrc_path(evidence, hmrc_check))
+            end
           end
 
-          it "load income" do
-            expect(api_service).to have_received(:income).with('2001-01-03', '2002-01-03')
+          context 'fail' do
+            let(:errors) { instance_double(ActiveModel::Errors) }
+            before do
+              allow(api_service).to receive(:income).and_raise(HwfHmrcApiError.new('Error message'))
+              allow(form).to receive(:errors).and_return errors
+              allow(errors).to receive(:add)
+              post_call
+            end
+
+            it 'add error' do
+              expect(errors).to have_received(:add).with(:request, 'Error message')
+            end
+
+            it 'render new template' do
+              expect(response).to render_template('new')
+            end
           end
 
-          it "load hmrc_check" do
-            expect(api_service).to have_received(:hmrc_check)
+          context 'fail - timeout' do
+            let(:errors) { instance_double(ActiveModel::Errors) }
+            before do
+              allow(api_service).to receive(:income).and_raise(Net::ReadTimeout.new('Error message'))
+              allow(form).to receive(:errors).and_return errors
+              allow(errors).to receive(:add)
+              post_call
+            end
+
+            it 'add error' do
+              expect(errors).to have_received(:add).with(:timout, 'HMRC income checking failed. Submit this form for HMRC income checking')
+            end
+
+            it 'render new template' do
+              expect(response).to render_template('new')
+            end
           end
         end
+      end
+    end
+  end
 
+  describe 'GET #show' do
+    context 'as a signed out user' do
+      before { get :show, params: { evidence_check_id: evidence.id, id: hmrc_check.id } }
+
+      it { expect(response).to have_http_status(:redirect) }
+
+      it { expect(response).to redirect_to(user_session_path) }
+    end
+
+    context 'as a signed in user' do
+      context 'success' do
+        before do
+          allow(HmrcCheck).to receive(:find).and_return hmrc_check
+          allow(hmrc_check).to receive(:total_income).and_return 100
+          sign_in user
+          get :show, params: { evidence_check_id: evidence.id, id: hmrc_check.id }
+        end
+
+        it 'returns the correct status code' do
+          expect(response).to have_http_status(200)
+        end
+
+        it 'renders the correct template' do
+          expect(response).to render_template('show')
+        end
+
+        it 'load check' do
+          expect(HmrcCheck).to have_received(:find).with(hmrc_check.id.to_s)
+        end
+      end
+
+      context 'data issue' do
+        let(:errors) { instance_double(ActiveModel::Errors) }
+        before do
+          allow(HmrcCheck).to receive(:find).and_return hmrc_check
+          allow(hmrc_check).to receive(:total_income).and_return 0
+          allow(hmrc_check).to receive(:errors).and_return errors
+          allow(errors).to receive(:add)
+          sign_in user
+          get :show, params: { evidence_check_id: evidence.id, id: hmrc_check.id }
+        end
+
+        it 'add error message' do
+          message = "There might be an issue with HMRC data. Please contact technical support."
+          expect(errors).to have_received(:add).with(:income_calculation, message)
+        end
+
+        it 'renders the correct template' do
+          expect(response).to render_template('show')
+        end
+
+        it 'load check' do
+          expect(HmrcCheck).to have_received(:find).with(hmrc_check.id.to_s)
+        end
       end
     end
   end
