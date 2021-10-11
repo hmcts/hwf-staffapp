70e0bead427fa90280c113266345aa9d12a802bf
Petr Zaparka <petr@zaparka.cz>
2021-10-11 14:59:58 +0100

    Adding missing migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_133617) do
+ActiveRecord::Schema.define(version: 2021_10_11_135412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -238,7 +238,7 @@ ActiveRecord::Schema.define(version: 2021_09_30_133617) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "purged_at"
-    t.integer "additional_income", default: 0
+    t.integer "additional_income"
   end
 
   create_table "hmrc_tokens", force: :cascade do |t|
