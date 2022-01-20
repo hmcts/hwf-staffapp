7d74f4b6e94075ee70321246dcb17f6f8ca0308d
Petr Zaparka <petr@zaparka.cz>
2022-01-20 11:27:26 +0000

    RST-4198 Decimal point and updating tax credit calculation (#1287)

--- spec/lib/hmrc_income_parser_spec.rb
+++ spec/lib/hmrc_income_parser_spec.rb
@@ -12,37 +12,85 @@ RSpec.describe HmrcIncomeParser do
   end
 
   describe 'tax_credit' do
+    let(:request_range) { { from: "2021-12-01", to: "2021-12-31" } }
     let(:tax_credit_hash) {
       [
         { "payProfCalcDate" => "2020-08-18",
-          "totalEntitlement" => 18765.23,
+          "totalEntitlement" => 1876523,
           "childTaxCredit" => {
-            "childCareAmount" => 930.98,
-            "ctcChildAmount" => 730.49,
-            "familyAmount" => 100.49,
-            "babyAmount" => 100, "paidYTD" => 8976.34
+            "childCareAmount" => 93098,
+            "ctcChildAmount" => 73049,
+            "familyAmount" => 10049,
+            "babyAmount" => 100, "paidYTD" => 897634
           },
           "payments" => [
-            { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 76.34 },
-            { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 56.24 }
+            { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 7634 },
+            { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5624 }
           ] },
         { "payProfCalcDate" => "2020-09-18",
-          "totalEntitlement" => 18765.23,
+          "totalEntitlement" => 1876523,
           "childTaxCredit" => {
-            "childCareAmount" => 930.98,
-            "ctcChildAmount" => 730.49,
-            "familyAmount" => 100.49,
-            "babyAmount" => 100, "paidYTD" => 8976.34
+            "childCareAmount" => 93098,
+            "ctcChildAmount" => 73049,
+            "familyAmount" => 10049,
+            "babyAmount" => 100, "paidYTD" => 897634
           },
           "payments" => [
-            { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 70.34 },
-            { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 50.24 }
+            { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 7034 },
+            { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5024 }
           ] }
       ]
     }
 
     it "returns income" do
-      expect(described_class.tax_credit(tax_credit_hash)).to eq(253.16)
+      expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(253.16)
+    end
+
+    describe 'frequency' do
+      let(:tax_credit_hash) {
+        [
+          {
+            "payments" => [
+              { "startDate" => '2021-06-24', "endDate" => '2022-03-31', "frequency" => frequency, "tcType" => "ICC", "amount" => 17059 },
+              { "startDate" => '2021-05-06', "endDate" => '2022-03-31', "frequency" => frequency, "tcType" => "ICC", "amount" => 17478 }
+            ]
+          }
+        ]
+      }
+
+      context '1' do
+        let(:frequency) { 1 }
+        it { expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(345.37) }
+      end
+
+      context '7' do
+        let(:frequency) { 7 }
+        it { expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(1726.85) }
+        context 'different frequency per payment' do
+          let(:tax_credit_hash) {
+            [
+              {
+                "payments" => [
+                  { "startDate" => '2021-06-24', "endDate" => '2022-03-31', "frequency" => 7, "tcType" => "ICC", "amount" => 17059 },
+                  { "startDate" => '2021-05-06', "endDate" => '2022-03-31', "frequency" => 14, "tcType" => "ICC", "amount" => 17478 }
+                ]
+              }
+            ]
+          }
+          it { expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(1377.29) }
+        end
+      end
+
+      context '14' do
+        let(:frequency) { 14 }
+        it { expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(865.52) }
+      end
+
+      context '28' do
+        let(:frequency) { 28 }
+        it { expect(described_class.tax_credit(tax_credit_hash, request_range).to_f).to eq(345.37) }
+      end
+
     end
   end
 end
--- spec/models/hmrc_check_spec.rb
+++ spec/models/hmrc_check_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe HmrcCheck, type: :model do
   describe 'serialized attributes' do
-    subject(:hmrc_check) { described_class.new(evidence_check: evidence_check, user: user) }
+    subject(:hmrc_check) { described_class.new(evidence_check: evidence_check, user: user, request_params: date_range) }
     let(:evidence_check) { create :evidence_check }
+    let(:date_range) { { date_range: { from: "2021-12-01", to: "2021-12-31" } } }
     let(:user) { create :user }
 
     context 'address' do
@@ -122,7 +123,10 @@ RSpec.describe HmrcCheck, type: :model do
 
       context 'with tax credit' do
         before do
-          hmrc_check.tax_credit = { child: [{ "payments" => [{ "amount" => 10 }] }], work: [{ "payments" => [{ "amount" => 10 }] }] }
+          hmrc_check.tax_credit = {
+            child: [{ "payments" => [{ "amount" => 1000, "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1 }] }],
+            work: [{ "payments" => [{ "amount" => 1000, "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1 }] }]
+          }
           hmrc_check.save
         end
         it { expect(hmrc_check.hmrc_income.to_f).to be 54021.45 }
@@ -187,8 +191,8 @@ RSpec.describe HmrcCheck, type: :model do
               "babyAmount" => 100, "paidYTD" => 8976.34
             },
             "payments" => [
-              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 76.34 },
-              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 56.24 }
+              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 7634 },
+              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5624 }
             ] },
           { "payProfCalcDate" => "2020-09-18",
             "totalEntitlement" => 18765.23,
@@ -199,8 +203,8 @@ RSpec.describe HmrcCheck, type: :model do
               "babyAmount" => 100, "paidYTD" => 8976.34
             },
             "payments" => [
-              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 70.34 },
-              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 50.24 }
+              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 7034 },
+              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5024 }
             ] }
         ]
 
@@ -223,8 +227,8 @@ RSpec.describe HmrcCheck, type: :model do
               paidYTD: 8976.34
             },
             "payments" => [
-              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 86.34 },
-              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 56.24 }
+              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 8634 },
+              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5624 }
             ] },
           { payProfCalcDate: "1996-08-01",
             totalEntitlement: 18765.23,
@@ -233,8 +237,8 @@ RSpec.describe HmrcCheck, type: :model do
               paidYTD: 8976.34
             },
             "payments" => [
-              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 7, "tcType" => "ICC", "amount" => 50.34 },
-              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 7, "tcType" => "ICC", "amount" => 55.24 }
+              { "startDate" => "1996-01-01", "endDate" => "1996-02-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5034 },
+              { "startDate" => "1996-03-01", "endDate" => "1996-04-01", "frequency" => 1, "tcType" => "ICC", "amount" => 5524 }
             ] }
         ]
 
@@ -249,9 +253,10 @@ RSpec.describe HmrcCheck, type: :model do
   end
 
   describe 'calculate_evidence_income!' do
-    subject(:hmrc_check) { described_class.new(evidence_check: evidence_check) }
+    subject(:hmrc_check) { described_class.new(evidence_check: evidence_check, request_params: date_range) }
     let(:evidence_check) { create :evidence_check, income: nil, application: application }
     let(:application) { create :single_applicant_under_61 }
+    let(:date_range) { { date_range: { from: "2021-12-01", to: "2021-12-31" } } }
 
     context 'no income data' do
       before { hmrc_check.calculate_evidence_income! }
@@ -260,7 +265,7 @@ RSpec.describe HmrcCheck, type: :model do
 
     context 'income present' do
       let(:income) { [{ "grossEarningsForNics" => { "inPayPeriod1" => 12000.04 } }] }
-      subject(:hmrc_check) { described_class.new(evidence_check: evidence_check, income: income) }
+      subject(:hmrc_check) { described_class.new(evidence_check: evidence_check, income: income, request_params: date_range) }
 
       before { hmrc_check.calculate_evidence_income! }
       it { expect(evidence_check.income).to eq(12000) }
