02e9723607acbe33167c46e85acb40e447b504ba
Petr Zaparka <petr@zaparka.cz>
2022-02-02 10:35:35 +0000

    Adding maintenance page

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,10 @@ module FrStaffapp
     end
     config.exceptions_app = routes
 
+    config.maintenance_enabled = ENV.fetch('MAINTENANCE_ENABLED', 'false').downcase == 'true'
+    config.maintenance_allowed_ips = ENV.fetch('MAINTENANCE_ALLOWED_IPS', '').split(',').map(&:strip)
+    config.maintenance_end = ENV.fetch('MAINTENANCE_END', nil)
+
   end
   WillPaginate.per_page = 20
 end
