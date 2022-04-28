7a5de96b2d6d4e7bc6ad6c9e54c777eb7163d078
Petr Zaparka <petr@zaparka.cz>
2022-04-28 10:09:12 +0100

    Moving dwp alert email to delayed jobs

---
+++ app/jobs/dwp_report_status_job.rb
@@ -0,0 +1,32 @@
+class DwpReportStatusJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    log_task_run
+    run_dwp_check
+  end
+
+  private
+
+  def run_dwp_check
+    send_email_notifications if DwpMonitor.new.state == 'offline'
+  end
+
+  def send_email_notifications
+    ApplicationMailer.dwp_is_down_notifier.deliver_now
+    log_notification
+  end
+
+  def log_task_run
+    tc = ApplicationInsights::TelemetryClient.new ENV['AZURE_APP_INSIGHTS_INSTRUMENTATION_KEY']
+    tc.track_event("Running DWP status check at #{Time.zone.now.to_s(:db)}")
+    tc.flush
+  end
+
+  def log_notification
+    tc = ApplicationInsights::TelemetryClient.new ENV['AZURE_APP_INSIGHTS_INSTRUMENTATION_KEY']
+    tc.track_event("Sending DWP status is offline notication at #{Time.zone.now.to_s(:db)}")
+    tc.flush
+  end
+
+end
--- lib/tasks/healtcheck_alert.rake
+++
@@ -1,6 +0,0 @@
-namespace :healtcheck_alert do
-  desc "check DWP status and report if it's down"
-  task report_dwp_offline_status: :environment do
-    AlertNotifier.run!
-  end
-end
