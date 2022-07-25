f2ccb2037cc357500bfd9a5e7e658a26e750ce73
Petr Zaparka <petr@zaparka.cz>
2022-07-25 14:18:15 +0100

    Hnmrc report controller and view

---
+++ app/controllers/report/hmrc_controller.rb
@@ -0,0 +1,39 @@
+module Report
+  class HmrcController < ReportsController
+    before_action :authorize_hmrc_purged
+
+    def show
+      render 'reports/hmrc'
+    end
+
+    def data_export
+      build_and_send_data
+    rescue StandardError => e
+      Raven.tags_context(task: "hmrc_purged_export") do
+        Raven.capture_message(e.message)
+      end
+      Rails.logger.debug { "Error in hmrc_purged_export export task: #{e.message}" }
+      render 'reports/hmrc'
+    end
+
+    private
+
+    def hmrc_purged_data
+      @date_from = 1.year.ago.beginning_of_day
+      @date_to = Time.now
+      Views::Reports::HmrcPurgedExport.new(@date_from, @date_to).to_csv
+    end
+
+    def authorize_hmrc_purged
+      authorize :report, :hmrc_purged?
+    end
+
+    def build_and_send_data
+      send_data hmrc_purged_data,
+          filename: "help-with-fees-#{@date_from}-#{@date_to}-hmrc-data-purged-history.csv",
+          type: 'text/csv',
+          disposition: 'attachment'
+    end
+
+  end
+end
---
+++ spec/controllers/report/hmrc_controller_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Report::HmrcController do
+
+  context 'mi user' do
+    let(:user) { create :user }
+    before { sign_in user }
+
+    describe 'GET #hmrc' do
+      before { get :show }
+
+      subject { response }
+      it { is_expected.to have_http_status(:redirect) }
+    end
+
+    describe 'PUT #hmrc' do
+      before { put :data_export }
+
+      subject { response }
+      it { is_expected.to have_http_status(:redirect) }
+    end
+  end
+
+  context 'admin' do
+    let(:admin) { create :admin_user }
+    before { sign_in admin }
+
+    describe 'GET #hmrc' do
+      before { get :show }
+
+      subject { response }
+
+      it { is_expected.to have_http_status(:success) }
+
+      it { is_expected.to render_template :hmrc }
+    end
+
+    describe 'PUT #data_export' do
+      subject { response }
+      let(:hmrc_export_class) { instance_double(Views::Reports::HmrcPurgedExport) }
+      let(:report) { 'csv_file' }
+      let(:current_time) { Time.zone.parse('2020-03-01 10:20:30') }
+
+      before {
+        allow(Views::Reports::HmrcPurgedExport).to receive(:new).and_return hmrc_export_class
+        allow(hmrc_export_class).to receive(:to_csv).and_return report
+        Timecop.freeze(current_time) do
+          put :data_export
+        end
+      }
+
+      it { is_expected.to have_http_status(:success) }
+
+      it 'generate report' do
+        date_from = Time.zone.parse('2019-03-01 0:00:00')
+        date_to = Time.zone.parse('2020-03-01 10:20:30')
+
+        expect(Views::Reports::HmrcPurgedExport).to have_received(:new).with(date_from, date_to)
+      end
+
+      it 'generate report' do
+        expect(hmrc_export_class).to have_received(:to_csv)
+      end
+
+    end
+  end
+end
