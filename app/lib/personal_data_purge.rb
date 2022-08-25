ce8e0983474b3f8450ade49d58c3784031488055
Petr Zaparka <petr@zaparka.cz>
2022-08-25 11:19:42 +0100

    Refactoring test to make if faster

--- app/lib/personal_data_purge.rb
+++ app/lib/personal_data_purge.rb
@@ -1,5 +1,4 @@
 class PersonalDataPurge
-  # include deleted applications
   # completed_at < 7.year.ago
   attr_reader :applications_to_purge
 
@@ -26,14 +25,10 @@ class PersonalDataPurge
       online_application_purge!(application)
       hmrc_check_purge!(application)
       benefit_check_purge!(application)
-      online_benefit_check_purge!(application)
       application_purge!(application)
     end
 
-    # deceased_name, date_of_death, case_number, ho_number, ni_number, title, first_name, last_name
-    # address, email_address, phone
-
-    # log_data_purge
+    log_data_purge
   end
 
   def log_data_purge
@@ -44,7 +39,6 @@ class PersonalDataPurge
   def application_purge!(application)
     application.update(purged: true)
   end
-
 
   def applicant_purge!(application)
     applicant = application.applicant
@@ -59,12 +53,18 @@ class PersonalDataPurge
   def online_application_purge!(application)
     online_application = application.online_application
     return unless online_application
+    online_benefit_check_purge!(online_application)
 
-    online_application.update(purged: true, case_number: PURGE_STRING, date_of_death: PURGE_STRING,
-      deceased_name: PURGE_STRING, title: PURGE_STRING, first_name: PURGE_STRING,
-      last_name: PURGE_STRING, ni_number: PURGE_STRING, ho_number: PURGE_STRING,
-      phone: PURGE_STRING, email_address: PURGE_STRING, address: PURGE_STRING, date_of_death: nil)
+    online_application.update(purged: true, case_number: PURGE_STRING,
+                              deceased_name: PURGE_STRING, title: PURGE_STRING, first_name: PURGE_STRING,
+                              last_name: PURGE_STRING, ni_number: PURGE_STRING, ho_number: PURGE_STRING,
+                              phone: PURGE_STRING, email_address: PURGE_STRING, address: PURGE_STRING,
+                              date_of_death: nil)
   end
+
+  # rubocop:disable Rails/SkipsModelValidations
+  # Update_all skips validations but following changes are not affecting any vailation
+  # in those models.
 
   def hmrc_check_purge!(application)
     hmrc_checks = application.evidence_check.try(:hmrc_checks)
@@ -78,12 +78,11 @@ class PersonalDataPurge
     benefit_checks.update_all(parameter_hash: nil, our_api_token: nil, last_name: nil, ni_number: nil)
   end
 
-  def online_benefit_check_purge!(application)
-    online_application = application.online_application
-    return unless online_application
+  def online_benefit_check_purge!(online_application)
     online_benefit_checks = online_application.online_benefit_checks
     return unless online_benefit_checks
     online_benefit_checks.update_all(parameter_hash: nil, our_api_token: nil, last_name: nil, ni_number: nil)
   end
+  # rubocop:enable Rails/SkipsModelValidations
 
 end
--- spec/lib/personal_data_purge_spec.rb
+++ spec/lib/personal_data_purge_spec.rb
@@ -11,28 +11,30 @@ RSpec.describe PersonalDataPurge do
   let(:online_benefit_check2) { build :online_benefit_check, parameter_hash: 'personal_data2', our_api_token: 'includes data too2' }
 
   describe 'load data' do
-
     let(:application1) { create :application }
     let(:application2) { create :application }
+    let(:application3) { create :application, :deleted_state }
+
     before do
       Timecop.freeze(7.years.ago) do
         application1
+        application3
       end
       application2
     end
 
-    it { expect(purge_object.applications_to_purge.count).to be(1) }
-    it { expect(purge_object.applications_to_purge.first.id).to eql(application1.id) }
-
+    it { expect(purge_object.applications_to_purge).to eq([application1, application3]) }
   end
 
   describe 'purge data' do
-    let(:application1) { create :application, applicant_traits: [:ho_number],
-      benefit_checks: [benefit_check1, benefit_check2], detail_traits: [:probate], online_application: online_application,
-      completed_at: 8.years.ago }
+    subject(:purge) { purge_object.purge! }
+    let(:application1) {
+      create :application, applicant_traits: [:ho_number],
+                           benefit_checks: [benefit_check1, benefit_check2], detail_traits: [:probate], online_application: online_application,
+                           completed_at: 8.years.ago
+    }
     let(:online_application) { create :online_application_with_all_details, online_benefit_checks: [online_benefit_check1, online_benefit_check2] }
 
-    subject(:purge) { purge_object.purge! }
     before {
       application1
       purge
@@ -42,56 +44,70 @@ RSpec.describe PersonalDataPurge do
 
     context 'applicant' do
       let(:applicant) { application1.applicant }
-      it { expect(applicant.reload.title).to be_nil }
-      it { expect(applicant.reload.first_name).to be_nil }
-      it { expect(applicant.reload.last_name).to be_nil }
-      it { expect(applicant.reload.ni_number).to be_nil }
-      it { expect(applicant.reload.ho_number).to be_nil }
+      let(:keys) { [:title, :first_name, :last_name, :ni_number, :ho_number] }
+      it {
+        applicant.reload
+        values = keys.map { |key| applicant[key] }
+        expect(values).to eq [nil, nil, nil, nil, nil]
+      }
     end
 
     context 'benefit_checks' do
-      it { expect(benefit_check1.reload.parameter_hash).to be_nil }
-      it { expect(benefit_check1.reload.our_api_token).to be_nil }
-      it { expect(benefit_check1.reload.last_name).to be_nil }
-      it { expect(benefit_check1.reload.ni_number).to be_nil }
-      it { expect(benefit_check2.reload.parameter_hash).to be_nil }
-      it { expect(benefit_check2.reload.our_api_token).to be_nil }
-      it { expect(benefit_check2.reload.last_name).to be_nil }
-      it { expect(benefit_check2.reload.ni_number).to be_nil }
+      let(:keys) { [:parameter_hash, :our_api_token, :last_name, :ni_number] }
+      it {
+        benefit_check1.reload
+        values = keys.map { |key| benefit_check1[key] }
+        expect(values).to eq [nil, nil, nil, nil]
+      }
+
+      it {
+        benefit_check2.reload
+        values = keys.map { |key| benefit_check2[key] }
+        expect(values).to eq [nil, nil, nil, nil]
+      }
     end
 
     context 'online_benefit_checks' do
-      it { expect(online_benefit_check1.reload.parameter_hash).to be_nil }
-      it { expect(online_benefit_check1.reload.our_api_token).to be_nil }
-      it { expect(online_benefit_check1.reload.last_name).to be_nil }
-      it { expect(online_benefit_check1.reload.ni_number).to be_nil }
-      it { expect(online_benefit_check2.reload.parameter_hash).to be_nil }
-      it { expect(online_benefit_check2.reload.our_api_token).to be_nil }
-      it { expect(online_benefit_check2.reload.last_name).to be_nil }
-      it { expect(online_benefit_check2.reload.ni_number).to be_nil }
+      let(:keys) { [:parameter_hash, :our_api_token, :last_name, :ni_number] }
+      it {
+        online_benefit_check1.reload
+        values = keys.map { |key| online_benefit_check1[key] }
+        expect(values).to eq [nil, nil, nil, nil]
+      }
+
+      it {
+        online_benefit_check2.reload
+        values = keys.map { |key| online_benefit_check2[key] }
+        expect(values).to eq [nil, nil, nil, nil]
+      }
     end
 
     context 'detail' do
       let(:detail) { application1.detail }
-      it { expect(detail.reload.deceased_name).to be_nil }
-      it { expect(detail.reload.date_of_death).to be_nil }
-      it { expect(detail.reload.case_number).to be_nil }
+      let(:keys) { [:deceased_name, :date_of_death, :case_number] }
+      it {
+        detail.reload
+        values = keys.map { |key| detail[key] }
+        expect(values).to eq [nil, nil, nil]
+      }
     end
 
     context 'online_application' do
-      it { expect(online_application.reload.deceased_name).to eq 'data purged' }
-      it { expect(online_application.reload.date_of_death).to be_nil }
-      it { expect(online_application.reload.case_number).to eq 'data purged' }
-      it { expect(online_application.reload.title).to eq 'data purged' }
-      it { expect(online_application.reload.first_name).to eq 'data purged' }
-      it { expect(online_application.reload.last_name).to eq 'data purged' }
-      it { expect(online_application.reload.ni_number).to eq 'data purged' }
-      it { expect(online_application.reload.ho_number).to eq 'data purged' }
-      it { expect(online_application.reload.address).to eq 'data purged' }
-      it { expect(online_application.reload.email_address).to eq 'data purged' }
-      it { expect(online_application.reload.phone).to eq 'data purged' }
+      let(:keys) {
+        [:deceased_name, :date_of_death, :case_number,
+         :title, :first_name, :last_name, :ni_number, :ho_number,
+         :address, :email_address, :phone]
+      }
+      let(:expected_values) {
+        ['data purged', nil, 'data purged', 'data purged', 'data purged', 'data purged',
+         'data purged', 'data purged', 'data purged', 'data purged', 'data purged']
+      }
+      it {
+        online_application.reload
+        values = keys.map { |key| online_application[key] }
+        expect(values).to eq expected_values
+      }
     end
-    it { expect(application1.evidence_check).to be_nil }
 
     context 'hmrc_check' do
       let(:hmrc_check1) { create :hmrc_check }
@@ -99,10 +115,18 @@ RSpec.describe PersonalDataPurge do
       let(:evidence_check) { create :evidence_check, hmrc_checks: [hmrc_check1, hmrc_check2] }
       let(:application1) { create :application, evidence_check: evidence_check, completed_at: 8.years.ago }
 
-      it { expect(hmrc_check1.reload.address).to be_nil }
-      it { expect(hmrc_check1.reload.ni_number).to be_nil }
-      it { expect(hmrc_check2.reload.address).to be_nil }
-      it { expect(hmrc_check2.reload.ni_number).to be_nil }
+      let(:keys) { [:address, :ni_number] }
+      it {
+        hmrc_check1.reload
+        values = keys.map { |key| hmrc_check1[key] }
+        expect(values).to eq [nil, nil]
+      }
+
+      it {
+        hmrc_check2.reload
+        values = keys.map { |key| hmrc_check2[key] }
+        expect(values).to eq [nil, nil]
+      }
     end
 
   end
