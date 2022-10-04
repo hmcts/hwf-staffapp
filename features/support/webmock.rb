d0c825fe43dbb9e7aa7b4ac3ea71c7797626e59f
JakeCG <112622305+JakeCG@users.noreply.github.com>
2022-10-04 09:59:15 +0100

    RST-4696 Cucumber reports (#1349)
    * RST-4696 Cucumber reports
    
    Co-authored-by: Petr Zaparka <petr@zaparka.cz>

--- features/support/webmock.rb
+++ features/support/webmock.rb
@@ -1,7 +1,10 @@
-require 'webmock/cucumber'
+require 'webmock'
+# rubocop:disable Style/MixinUsage
+include WebMock::API
+# rubocop:enable Style/MixinUsage
 require_relative './capybara_driver_helper'
 
 selenium_url = URI.parse ENV.fetch('SELENIUM_URL', 'http://localhost:4444/wd/hub')
 app_host_url = URI.parse Capybara.app_host
 
-WebMock.disable_net_connect!(allow_localhost: true, net_http_connect_on_start: true, allow: [selenium_url.host, app_host_url.host, 'ondemand.saucelabs.com', 'chromedriver.storage.googleapis.com'])
+WebMock.disable_net_connect!(allow_localhost: true, net_http_connect_on_start: true, allow: [selenium_url.host, app_host_url.host, 'ondemand.saucelabs.com', 'chromedriver.storage.googleapis.com', 'https://messages.cucumber.io/api/reports'])
