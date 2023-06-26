e685fade7d707431f10697045b77038d1f4edc5b
Petr Zaparka <petr@zaparka.cz>
2023-06-26 13:21:49 +0100

    different regex test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,11 +41,12 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+
   config.ssl_options = {
     hsts: { expires: 1.year, preload: true },
-    redirect: { exclude: ->(request) { request.path.include?('submissions') } }
+    redirect: { exclude: ->(request) { return true if ((request.path =~ /ping|submissions/) != nil)  } }
   }
+  config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
