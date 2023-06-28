d222fbc93d908731bd5991faf6fcd2e23185a945
Petr Zaparka <petr@zaparka.cz>
2023-06-28 13:07:56 +0100

    This should ignor the puma error

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -3,7 +3,6 @@ EXCLUDE_PATHS = ['/ping', '/ping.json', '/health', '/health.json'].freeze
 Sentry.init do |config|
   config.dsn = Settings.sentry.dsn
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
-
   config.release = ENV.fetch('APPVERSION', 'unknown')
 
   config.traces_sampler = lambda do |sampling_context|
@@ -11,5 +10,14 @@ Sentry.init do |config|
     transaction_name = transaction_context[:name]
 
     transaction_name.in?(EXCLUDE_PATHS) ? 0.0 : 0.01
+  end
+
+  config.before_send = lambda do |event, hint|
+    # note: hint[:exception] would be a String if you use async callback
+    if hint[:exception].is_a?(Puma::HttpParserError)
+      nil
+    else
+      event
+    end
   end
 end
