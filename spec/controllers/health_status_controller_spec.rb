860a891e35ecb827f99e7d79e672f7ee726092c5
Damon Green <damon.green@btinternet.com>
2023-09-12 15:37:19 +0100

    add show to health status spec

--- spec/controllers/health_status_controller_spec.rb
+++ spec/controllers/health_status_controller_spec.rb
@@ -32,6 +32,41 @@ RSpec.describe HealthStatusController do
     end
   end
 
+  describe '#show' do
+    let(:json) { { status: 'ok' } }
+    let(:health_check) { instance_double(HealthStatus::HealthCheck, as_json: json, healthy?: healthy?) }
+
+    before do
+      allow(HealthStatus::HealthCheck).to receive(:new).and_return(health_check)
+
+      get :show
+    end
+
+    context 'when the health check reports as healthy' do
+      let(:healthy?) { true }
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'renders the health check json' do
+        expect(response.body).to eql(json.to_json)
+      end
+    end
+
+    # context 'when the health check reports as unhealthy' do
+    #   let(:healthy?) { false }
+
+    #   it 'responds with 500 status' do
+    #     expect(response).to have_http_status(:internal_server_error)
+    #   end
+
+    #   it 'renders the health check json' do
+    #     expect(response.body).to eql(json.to_json)
+    #   end
+    # end
+  end
+
   describe 'GET #raise_exception' do
     it { expect { get :raise_exception }.to raise_exception(RuntimeError) }
   end
