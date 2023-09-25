45bf32f348f89a38984c7916f995af998e302cff
Petr Zaparka <petr@zaparka.cz>
2023-09-25 14:52:56 +0100

    Removing the depreceation warning

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ require "action_view/railtie"
 # require "action_cable/engine"
 # require "rails/test_unit/railtie"
 
+ENV['RAILS_DISABLE_DEPRECATED_TO_S_CONVERSION'] = 'true'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
