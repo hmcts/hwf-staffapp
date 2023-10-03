4afb7899e0ff324e3a52bcb7384c280ca5332696
Petr Zaparka <petr@zaparka.cz>
2023-10-03 16:46:41 +0100

    No special chars in company name (#1504)

--- spec/factories/jurisdictions.rb
+++ spec/factories/jurisdictions.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :jurisdiction do
-    sequence(:name)   { "#{Faker::Company.name.delete(',')}#{Random.rand(1000)}" }
+    sequence(:name)   { "#{Faker::Company.name.gsub(/\W/, '')}#{Random.rand(1000)}" }
     sequence(:abbr)   { "#{Random.rand(1000)} #{Faker::Hacker.abbreviation} #{Random.rand(1000)}" }
     active { true }
     factory :invalid_jurisdiction do
