1b0fef8d1b40323b0e1b855fb01e37cfc0e2ecc7
Petr Zaparka <petr@zaparka.cz>
2023-11-14 13:58:28 +0000

    Updating flaky test

--- spec/factories/representatives.rb
+++ spec/factories/representatives.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :representative do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
+    first_name { Faker::Name.first_name.gsub(/\W/, '') }
+    last_name { Faker::Name.last_name.gsub(/\W/, '') }
     organisation { Faker::Company.name.gsub(/\W/, '') }
   end
 end
