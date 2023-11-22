d83ec018aed291f70dddfcaf4dc8e53446e4a2aa
kalvinlyu <kalvin.lyu@solirius.com>
2023-11-22 21:26:57 +0000

    RST-6055 guide helper tests

--- spec/helpers/guide_helper_spec.rb
+++ spec/helpers/guide_helper_spec.rb
@@ -2,8 +2,16 @@ require 'rails_helper'
 
 RSpec.describe GuideHelper do
 
-  describe '#staff_guidance_url' do
-    it { expect(helper.staff_guidance_url).to include('https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-policy-guide.pdf') }
+  describe '#old_staff_guidance_url' do
+    it { expect(helper.old_staff_guidance_url).to include('https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-policy-guide.pdf') }
+  end
+
+  describe '#new_staff_guidance_url' do
+    it { expect(helper.new_staff_guidance_url).to include('https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-policy-guide.pdf') }
+  end
+
+  describe '#training_course_url' do
+    it { expect(helper.training_course_url).to include('https://mydevelopment.org.uk/course/view.php?id=9824') }
   end
 
   describe '#how_to_url' do
@@ -17,4 +25,17 @@ RSpec.describe GuideHelper do
   describe '#fraud_awareness_url' do
     it { expect(helper.fraud_awareness_url).to include('https://intranet.justice.gov.uk/documents/2018/05/help-with-fees-fraud-awareness-pdf.pdf') }
   end
+
+  describe '#old_job_cards_url' do
+    it { expect(helper.old_job_cards_url).to include('https://intranet.justice.gov.uk/about-hmcts/my-work/help-with-fees/job-cards/') }
+  end
+
+  describe '#new_job_cards_url' do
+    it { expect(helper.new_job_cards_url).to include('https://intranet.justice.gov.uk/about-hmcts/my-work/help-with-fees/job-cards/') }
+  end
+
+  describe '#rrds_url' do
+    it { expect(helper.rrds_url).to include('https://www.gov.uk/government/publications/record-retention-and-disposition-schedules') }
+  end
+
 end
