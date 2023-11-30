6581cb8ba996271aa319c12c82f995f066e940d1
Petr Zaparka <petr@zaparka.cz>
2023-11-30 15:34:01 +0000

    RST-6056 Adding another cost of living exception (#1555)

--- app/lib/hmrc_cost_of_living.rb
+++ app/lib/hmrc_cost_of_living.rb
@@ -5,6 +5,7 @@ module HmrcCostOfLiving
     return true if september_match?(request_range, payment)
     return true if november_match?(request_range, payment)
     return true if may_match?(request_range, payment)
+    return true if november_23_match?(request_range, payment)
     false
   end
 
@@ -12,6 +13,14 @@ module HmrcCostOfLiving
   def included_in_may_date_range?(payment)
     list = [Date.parse(payment['startDate']), Date.parse(payment['endDate'])]
     parsed_list = list_parsed_by_may_cost_of_living(list, payment)
+
+    parsed_list.size != 2
+  end
+
+  # RST-6056 for more info
+  def included_in_november_23_date_range?(payment)
+    list = [Date.parse(payment['startDate']), Date.parse(payment['endDate'])]
+    parsed_list = list_parsed_by_november_23_cost_of_living(list, payment)
 
     parsed_list.size != 2
   end
@@ -28,6 +37,20 @@ module HmrcCostOfLiving
       end
     end
 
+    parsed_list
+  end
+
+  def list_parsed_by_november_23_cost_of_living(list, payment)
+    parsed_list = []
+
+    list.each do |date|
+      case date
+      when Date.parse('10 November 2023')..Date.parse('19 November 2023')
+        next if payment['amount'].to_i == 300
+      else
+        parsed_list << date
+      end
+    end
     parsed_list
   end
 
@@ -50,6 +73,13 @@ module HmrcCostOfLiving
       request_range[:to] == "2023-05-31" &&
       payment['frequency'] == 1 &&
       payment['amount'].to_i == 301 && included_in_may_date_range?(payment)
+  end
+
+  def november_23_match?(request_range, payment)
+    request_range[:from] == "2023-11-01" &&
+      request_range[:to] == "2023-11-30" &&
+      payment['frequency'] == 1 &&
+      payment['amount'].to_i == 300 && included_in_november_23_date_range?(payment)
   end
 
 end
