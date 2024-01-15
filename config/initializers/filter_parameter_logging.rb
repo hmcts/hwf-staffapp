1fca10ee05f1840f8e118a7be6ef30cac27ba509
Petr Zaparka <petr@zaparka.cz>
2024-01-15 10:29:26 +0000

    Rails update script

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :ni_number, :ho_number, :date_of_birth, :first_name,
-                                               :last_name, :address, :postcode, :email_address, :encrypted_access_token]
+# Configure parameters to be partially matched (e.g. passw matches password) and filtered from the log file.
+# Use this to limit dissemination of sensitive information.
+# See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn,
+  :password, :ni_number, :ho_number, :date_of_birth, :first_name,
+  :last_name, :address, :postcode, :email_address, :encrypted_access_token
+]
