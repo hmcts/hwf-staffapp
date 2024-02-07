9007403df03e07e4d2669f8a54acd4ea9630a534
Aaron Lambley <aaron.lambley@gmail.com>
2024-02-07 12:46:52 +0000

    Update guide process urls for old and new applications

--- features/step_definitions/process_online_application_steps.rb
+++ features/step_definitions/process_online_application_steps.rb
@@ -97,7 +97,8 @@ Then("I see digital check the fee") do
   expect(application_details_digital_page.content.guidance.guidance_sub_heading[1].text).to eq 'Check the fee:'
   expect(application_details_digital_page.content.guidance.guidance_list[1].text).to have_text 'was not processed through the money claim online (MCOL)'
   expect(application_details_digital_page.content.guidance.guidance_text[1].text).to eq 'What to do if the fee is one of these'
-  expect(application_details_digital_page.content.guidance.guidance_link[1]['href']).to end_with '/guide/process_application#check-the-fee'
+  expect(application_details_digital_page.content.guidance.guidance_link[1]['href']).to eq 'https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-how-to-guide.pdf'
+
 end
 
 Then("I see Remember for the case details") do
@@ -114,7 +115,7 @@ Then("I see digital examples of emergency cases") do
   expect(application_details_digital_page.content.guidance.guidance_sub_heading[3].text).to eq 'Example of emergency cases:'
   expect(application_details_digital_page.content.guidance.guidance_list[3].text).to have_text 'suspending an eviction debtor insolvency petition children or vulnerable adults domestic violence injunctions ‘out of hours’ provisions at the Royal Courts of Justice'
   expect(application_details_digital_page.content.guidance.guidance_text[3].text).to eq 'What to do if the application can’t be processed before the emergency application is heard'
-  expect(application_details_digital_page.content.guidance.guidance_link[2]['href']).to end_with '/guide/process_application#emergency'
+  expect(application_details_digital_page.content.guidance.guidance_link[2]['href']).to eq how_to_url
 end
 
 When("I click emergency checkbox") do
