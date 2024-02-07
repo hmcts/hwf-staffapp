874c692ea2b60dc4fe0ad406fb3e9e1a7b94edbb
kalvinlyu <kalvin.lyu@solirius.com>
2024-02-07 14:57:12 +0000

    RST-6169 added test for income period helper

---
+++ spec/helpers/income_period_helper_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe IncomePeriodHelper do
+
+  describe '#income_period' do
+    subject(:income_period) { helper.income_period(application) }
+
+    context 'when application has no income period' do
+      let(:application) { build_stubbed(:application, income_period: nil) }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when application has an income period' do
+      context 'and income period is last month' do
+        let(:application) { build_stubbed(:application, income_period: 'last_month') }
+
+        it { is_expected.to eq 'last calendar month' }
+      end
+
+      context 'and income period is average' do
+        let(:application) { build_stubbed(:application, income_period: 'average') }
+
+        it { is_expected.to eq 'average income for last three calendar months' }
+      end
+    end
+
+
+  end
+end
