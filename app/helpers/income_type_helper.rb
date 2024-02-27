97d1612f1fda1857d56647f8fb0d1449f91e4d74
kalvinlyu <kalvin.lyu@solirius.com>
2024-02-27 15:39:19 +0000

    RST-6102 added missing text and preucd income type

--- app/helpers/income_type_helper.rb
+++ app/helpers/income_type_helper.rb
@@ -1,6 +1,8 @@
 module IncomeTypeHelper
 
   def income_duration_type(application)
+    return 'Your income total' unless show_ucd_changes?(application.detail.calculation_scheme)
+
     if application.income_period == 'last_month'
       'Your total monthly income'
     else
