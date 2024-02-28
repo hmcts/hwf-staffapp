eebf2d6936479374ab55dd7ef88fa0f95e886a41
Kalvin Lyu <104897244+kalvinSolirius@users.noreply.github.com>
2024-02-28 13:20:39 +0000

    RST-6259 Checkboxes checked after save and return (#1644)
    * RST-6259 Checkboxes checked after save and return
    
    * Update Chart.yaml

---
+++ spec/helpers/income_kind_helper_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe IncomeKindHelper do
+
+  describe '#kind_checked' do
+    subject(:kind_checked) { helper.kind_checked(application, form, claimant, kind) }
+
+    before do
+      allow(form).to receive(:i18n_scope).and_return(:'activemodel.attributes.forms/application/income_kind_applicant')
+    end
+
+    context 'when application has kind' do
+      let(:application) { build_stubbed(:application, income_kind: { applicant: ['Wages'] }) }
+      let(:form) { Forms::Application::IncomeKindApplicant }
+      let(:claimant) { :applicant }
+      let(:kind) { 1 }
+
+      it { is_expected.to be true }
+    end
+
+    context 'when application does not contain kind' do
+      let(:application) { build_stubbed(:application, income_kind: { applicant: [] }) }
+      let(:form) { Forms::Application::IncomeKindApplicant }
+      let(:claimant) { :applicant }
+      let(:kind) { 1 }
+
+      it { is_expected.to be false }
+    end
+  end
+end
