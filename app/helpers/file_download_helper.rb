584d7a54109b1faa74c7c182ea4dd46a276f4972
Petr Zaparka <petr@zaparka.cz>
2024-04-10 11:15:46 +0100

    Load wording from translation file

--- app/helpers/file_download_helper.rb
+++ app/helpers/file_download_helper.rb
@@ -1,6 +1,6 @@
 module FileDownloadHelper
   def download_file_page_header(storage)
-    return 'Raw Data' if storage.name == 'raw_data'
-    'Finance transactional' if storage.name == 'finance_transactional'
+    return I18n.t('.users.file_download.raw_data') if storage.name == 'raw_data'
+    I18n.t('.users.file_download.finance_transactional') if storage.name == 'finance_transactional'
   end
 end
