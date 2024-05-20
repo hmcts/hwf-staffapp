77f5c998b2bd5a7f979efebb4c644f64fd032f03
Petr Zaparka <petr@zaparka.cz>
2024-05-20 10:30:09 +0100

    RST-5659 date received (#1720)
    * Updating test
    * Digital has different validation then paper

---
+++ app/models/validators/date_received_validator.rb
@@ -0,0 +1,67 @@
+module Validators
+  class DateReceivedValidator < ActiveModel::Validator
+
+    def validate(record)
+      @validate_record = record
+      @date_received_value = record.date_received
+
+      if @date_received_value.blank?
+        add_error(I18n.t("#{translation_prefix}.blank"))
+      elsif @date_received_value.is_a?(String)
+        parse_string_date
+      else
+        validate_ranges
+      end
+    end
+
+    def validate_ranges
+      if after_or_equal_min_date
+        add_error(I18n.t("#{translation_prefix}.date_after_or_equal_to"))
+      elsif before_tomorrow
+        add_error(I18n.t("#{translation_prefix}.date_before"))
+      elsif before_or_equal_to_submitt_date
+        add_error(I18n.t("#{translation_prefix}.before_submit"))
+      end
+    end
+
+    def parse_string_date
+      Date.parse(@date_received_value)
+    rescue StandardError
+      add_error(I18n.t("#{translation_prefix}.not_a_date"))
+    end
+
+    def add_error(message)
+      @validate_record.errors.add(:date_received, message)
+    end
+
+    def min_date
+      3.months.ago.midnight
+    end
+
+    def tomorrow
+      Time.zone.tomorrow
+    end
+
+    def submitted_date
+      @validate_record.submitted_at.to_date
+    end
+
+    def after_or_equal_min_date
+      @date_received_value <= min_date
+    end
+
+    def before_tomorrow
+      tomorrow < @date_received_value
+    end
+
+    def before_or_equal_to_submitt_date
+      return true if @date_received_value < submitted_date
+      (@date_received_value - 3.months) >= submitted_date
+    end
+
+    def translation_prefix
+      '.activemodel.errors.models.forms/application/detail.attributes.date_received'
+    end
+
+  end
+end
