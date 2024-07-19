de14f7ce66b9230713b483127828601399a4426f
Petr Zaparka <petr@zaparka.cz>
2024-07-19 16:22:41 +0100

    Fixing bug

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -104,7 +104,7 @@ module HomeHelper
   def hmrc_evidence_check_link(application)
     evidence_check = application.evidence_check
 
-    if evidence_check.hmrc_check.try(:total_income).try(:positive?)
+    if evidence_check.total_income.try(:positive?)
       evidence_check_hmrc_path(evidence_check, evidence_check.hmrc_check)
     else
       new_evidence_check_hmrc_path(evidence_check)
