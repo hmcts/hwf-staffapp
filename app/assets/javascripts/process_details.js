dd570616dbee96255447743253c81a17474059d8
Aaron Lambley <aaron.lambley@gmail.com>
2024-08-05 16:12:27 +0100

    preserve choices from check your answers page

--- app/assets/javascripts/process_details.js
+++ app/assets/javascripts/process_details.js
@@ -4,7 +4,7 @@ window.moj.Modules.JurisdictionModule = {
     init: function(countyJurisdictionId) {
         this.countyJurisdictionId = countyJurisdictionId || 1;
         this.bindEvents();
-        this.toggleClaimType();
+        this.loadState();
     },
 
     bindEvents: function() {
@@ -18,6 +18,15 @@ window.moj.Modules.JurisdictionModule = {
         });
     },
 
+    loadState: function() {
+        const countyJurisdictionElement = document.querySelector(`#jurisdiction_${this.countyJurisdictionId}`);
+        const formTypeN1Radio = document.querySelector('#form_type_n1_radio');
+        const isCountyJurisdiction = countyJurisdictionElement?.checked;
+        if (formTypeN1Radio) {
+            formTypeN1Radio.disabled = !isCountyJurisdiction;
+        }
+    },
+
     toggleClaimType: function() {
         const countyJurisdictionElement = document.querySelector(`#jurisdiction_${this.countyJurisdictionId}`);
 
@@ -29,14 +38,12 @@ window.moj.Modules.JurisdictionModule = {
             document.querySelector('#application_claim_type_personal_injury')
         ];
 
-        const isCountyJurisdiction = countyJurisdictionElement.checked;
+        const isCountyJurisdiction = countyJurisdictionElement?.checked;
         formTypeN1Radio.disabled = !isCountyJurisdiction;
 
         claimTypes.forEach(claimType => {
             claimType.disabled = !isCountyJurisdiction;
             claimType.checked = false;
         });
-        formTypeN1Radio.checked = false;
-        otherRadioRadio.checked = false;
     }
 };
