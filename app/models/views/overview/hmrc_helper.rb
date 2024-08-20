9a9e9edc447250ff7181e4c1014888340647e023
Petr Zaparka <petr@zaparka.cz>
2024-08-20 10:25:47 +0100

    Income period check update

--- app/models/views/overview/hmrc_helper.rb
+++ app/models/views/overview/hmrc_helper.rb
@@ -3,12 +3,12 @@ module Views
     module HmrcHelper
 
       def hmrc_view_total_monthly_income
-        return total_monthly_income if @application.income_period == 'last_month'
+        return total_monthly_income if @application.income_period_last_month?
         'N/A'
       end
 
       def average_monthly_income
-        return total_monthly_income if @application.income_period == 'average'
+        return total_monthly_income if @application.income_period_three_months_average?
         'N/A'
       end
 
