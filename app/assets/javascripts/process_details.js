67a1e4431d136367c49f568cbbfa4ea5a722f7e1
Aaron Lambley <aaron.lambley@gmail.com>
2024-08-30 09:56:40 +0100

    update js to toggle required attribute on claim_type radio buttons

--- app/assets/javascripts/process_details.js
+++ app/assets/javascripts/process_details.js
@@ -10,12 +10,23 @@ window.moj.Modules.JurisdictionModule = {
 
     bindEvents: function() {
         const jurisdictionRadios = document.querySelectorAll(`#jurisdiction_radios input[name="${this.applicationType}[jurisdiction_id]"]`);
+        const formTypeN1Radio = document.querySelector('#form_type_n1_radio');
+        const formTypeOther = document.querySelector('#other_radio');
 
         const self = this;
+
         jurisdictionRadios.forEach(function (radio) {
             radio.addEventListener('change', function() {
                 self.toggleClaimType();
             });
+        });
+
+        formTypeN1Radio.addEventListener('change', function() {
+            self.toggleRequiredAttributes(formTypeN1Radio.checked);
+        });
+
+        formTypeOther.addEventListener('change', function() {
+            self.toggleRequiredAttributes(false);
         });
     },
 
@@ -23,14 +34,15 @@ window.moj.Modules.JurisdictionModule = {
         const countyJurisdictionElement = document.querySelector(`#jurisdiction_${this.countyJurisdictionId}`);
         const formTypeN1Radio = document.querySelector('#form_type_n1_radio');
         const isCountyJurisdiction = countyJurisdictionElement?.checked;
+
         if (formTypeN1Radio) {
             formTypeN1Radio.disabled = !isCountyJurisdiction;
+            this.toggleRequiredAttributes(formTypeN1Radio.checked);
         }
     },
 
     toggleClaimType: function() {
         const countyJurisdictionElement = document.querySelector(`#jurisdiction_${this.countyJurisdictionId}`);
-
         const formTypeN1Radio = document.querySelector('#form_type_n1_radio');
         const formTypeOther = document.querySelector('#other_radio');
         const claimTypes = [
@@ -40,17 +52,34 @@ window.moj.Modules.JurisdictionModule = {
         ].filter(el => el.id.match(/^(application|online_application)_claim_type/));
 
         const isCountyJurisdiction = countyJurisdictionElement?.checked;
-        // added to ensure the correct radio is selected on county change
+
         if (!isCountyJurisdiction) {
             formTypeOther.click();
         } else {
             formTypeN1Radio.click();
         }
+
         formTypeN1Radio.disabled = !isCountyJurisdiction;
-        // claim_type should be deselected/reset when form_type or county changes
+
         claimTypes.forEach(claimType => {
             claimType.disabled = !isCountyJurisdiction;
             claimType.checked = false;
+        });
+    },
+
+    toggleRequiredAttributes: function(isRequired) {
+        const claimTypes = [
+            ...document.querySelectorAll('[id$="_claim_type_specified"]'),
+            ...document.querySelectorAll('[id$="_claim_type_unspecified"]'),
+            ...document.querySelectorAll('[id$="_claim_type_personal_injury"]')
+        ].filter(el => el.id.match(/^(application|online_application)_claim_type/));
+
+        claimTypes.forEach(claimType => {
+            if (isRequired) {
+                claimType.setAttribute('required', 'required');
+            } else {
+                claimType.removeAttribute('required');
+            }
         });
     }
 };
