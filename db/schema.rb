e09eb63506275e11c851beecca7fdc6198fc5722
kalvinlyu <kalvin.lyu@solirius.com>
2024-10-22 10:24:07 +0100

    RST-6779 forgot schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_10_093923) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_10_134831) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
