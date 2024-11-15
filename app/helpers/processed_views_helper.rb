a5bf52c9a7aa5dcc95041eda7ea5545a6b6eba5d
Petr Zaparka <petr@zaparka.cz>
2024-11-15 10:30:49 +0000

    Fixing failing tests

--- app/helpers/processed_views_helper.rb
+++ app/helpers/processed_views_helper.rb
@@ -4,6 +4,7 @@ module ProcessedViewsHelper
     @application = application
     @fee_status = Views::Overview::FeeStatus.new(application)
     @applicant = Views::Overview::Applicant.new(application)
+    @online_applicant = Views::Overview::OnlineApplicant.new(application)
     @details = Views::Overview::Details.new(application)
     @savings = Views::Overview::SavingsAndInvestments.new(application.saving)
     @children = Views::Overview::Children.new(application)
@@ -12,7 +13,7 @@ module ProcessedViewsHelper
     @application_view = Views::Overview::Application.new(application)
     @result = Views::ApplicationResult.new(application)
     @declaration = Views::Overview::Declaration.new(application)
-    @representative = Views::Overview::Representative.new(application.representative)
+    @representative = Views::Overview::Representative.new(build_representative(application))
     @processing_details = Views::ProcessedData.new(application)
   end
   # rubocop:enable Rails/HelperInstanceVariable,Metrics/AbcSize,Metrics/MethodLength
@@ -53,6 +54,19 @@ module ProcessedViewsHelper
 
   def evidence_not_received(evidence)
     evidence.try(:incorrect_reason) == 'not_arrived_or_late'
+  end
+
+  def build_representative(build_from)
+    if build_from.is_a?(Application) || !ucd_changes_apply?(build_from)
+      build_from.representative
+    else
+      Representative.new(
+        first_name: build_from.legal_representative_first_name,
+        last_name: build_from.legal_representative_last_name,
+        organisation: build_from.legal_representative_organisation_name,
+        position: build_from.legal_representative_position
+      )
+    end
   end
 
   private
