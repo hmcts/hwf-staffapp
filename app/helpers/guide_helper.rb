b73ccd819e91756d98a9eadb77db2c446aac4d4d
Azarren <daniellekushnir22@gmail.com>
2024-11-19 10:41:48 +0000

    RST-6994 - Updated staff guides with new links

--- app/helpers/guide_helper.rb
+++ app/helpers/guide_helper.rb
@@ -21,7 +21,11 @@ module GuideHelper
   end
 
   def new_process_application_url
-    'https://intranet.justice.gov.uk/documents/2023/11/processing-a-help-with-fees-application-post-27th-november-23.docx'
+    'https://intranet.justice.gov.uk/documents/2024/11/process-a-paper-help-with-fees-application.pdf/'
+  end
+
+  def new_online_process_application_url
+    'https://intranet.justice.gov.uk/documents/2024/11/processing-an-online-help-with-fees-application.pdf/'
   end
 
   def old_evidence_checks_url
@@ -29,11 +33,11 @@ module GuideHelper
   end
 
   def new_evidence_checks_url
-    'https://intranet.justice.gov.uk/documents/2023/11/processing-evidence-post-27th-november-23.docx'
+    'https://intranet.justice.gov.uk/documents/2024/11/process-a-help-with-fees-evidence.pdf/'
   end
 
   def part_payment_url
-    'https://intranet.justice.gov.uk/documents/2020/12/help-with-fees-processing-a-part-payment-job-card.pdf'
+    'https://intranet.justice.gov.uk/documents/2024/11/process-a-help-with-fees-part-payment.pdf/'
   end
 
   def fraud_awareness_url
@@ -42,5 +46,9 @@ module GuideHelper
 
   def rrds_url
     'https://www.gov.uk/government/publications/record-retention-and-disposition-schedules'
+  end
+
+  def datashare_url
+    'https://intranet.justice.gov.uk/my-work/help-with-fees/job-cards/'
   end
 end
