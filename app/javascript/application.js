a3ad6f81f3dcaf956504442eb03fa4a9c2b989c3
Petr Zaparka <petr@zaparka.cz>
2025-01-30 14:35:12 +0000

    Moving js structure around

---
+++ app/javascript/application.js
@@ -0,0 +1,28 @@
+
+console.log('start');
+import { initAll } from 'govuk-frontend'
+initAll();
+
+import 'jquery'
+
+global.$ = require("jquery");
+global.jQuery = global.$;
+
+import './module_loader'
+import './radio_checkbox_module'
+import './income'
+import './radio_checkbox_module'
+import './income'
+import './savings'
+import './evidence_confirmation'
+import './equal_height_boxes'
+import './cookies'
+import './login'
+import './close_alerts'
+import './data_layer_push'
+import './gtm_track_timestamps'
+import './error_class_loader'
+import './refund'
+import './date_received_online'
+
+console.log('end');
