12dc49304521c1e61221a847bbac515fcd245b58
kalvinlyu <kalvin.lyu@solirius.com>
2025-04-23 13:03:55 +0100

    RST-7284 missing space

--- spec/models/query/waiting_for_evidence_spec.rb
+++ spec/models/query/waiting_for_evidence_spec.rb
@@ -43,7 +43,7 @@ RSpec.describe Query::WaitingForEvidence do
 
     context 'when order is Ascending' do
       it 'returns applications in form_name, then fee then ascending order by completed_at' do
-        expect(query.find(false, false,{}, 'Ascending')).to eq([application1, application3, application2])
+        expect(query.find(false, false, {}, 'Ascending')).to eq([application1, application3, application2])
       end
     end
 
