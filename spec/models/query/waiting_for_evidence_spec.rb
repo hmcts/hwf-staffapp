792cdac5163c2dacb5a4c26fe422d4fdde892521
kalvinlyu <kalvin.lyu@solirius.com>
2025-04-23 15:48:25 +0100

    RST-7284 sonarqube

--- spec/models/query/waiting_for_evidence_spec.rb
+++ spec/models/query/waiting_for_evidence_spec.rb
@@ -31,31 +31,47 @@ RSpec.describe Query::WaitingForEvidence do
              detail: create(:detail, form_name: 'AA', fee: 200))
     end
 
-    before do
-      application1
-      application2
-      application3
+    context 'default sorting by completed_at DESC' do
+      it 'returns applications ordered by completed_at descending' do
+        expect(query.find(false, false)).to eq([application2, application3, application1])
+      end
     end
 
-    it 'returns applications in default (Descending) order by completed_at, then form_name, then fee' do
-      expect(query.find(false, false)).to eq([application2, application3, application1])
-    end
-
-    context 'when order is Ascending' do
-      it 'returns applications in form_name, then fee then ascending order by completed_at' do
+    context 'sorting by completed_at ASC' do
+      it 'returns applications ordered by completed_at ascending' do
         expect(query.find(false, false, {}, 'Ascending')).to eq([application1, application3, application2])
       end
     end
 
-    context 'empty jurisdiction value' do
-      it 'ignores the filter and returns all' do
-        expect(query.find(false, false, jurisdiction_id: '')).to eq([application2, application3, application1])
+    context 'sorting by form_name' do
+      it 'orders by form_name DESC then completed_at DESC' do
+        expect(query.find(true, false)).to eq([application2, application3, application1])
+      end
+
+      it 'orders by form_name DESC then completed_at ASC' do
+        expect(query.find(true, false, {}, 'Ascending')).to eq([application2, application1, application3])
       end
     end
 
-    context 'nil filter' do
+    context 'sorting by fee' do
+      it 'orders by fee DESC then completed_at DESC' do
+        expect(query.find(false, true)).to eq([application3, application1, application2])
+      end
+
+      it 'orders by fee DESC then completed_at ASC' do
+        expect(query.find(false, true, {}, 'Ascending')).to eq([application3, application1, application2])
+      end
+    end
+
+    context 'with empty jurisdiction filter' do
+      it 'ignores the filter and returns all' do
+        expect(query.find(false, false, { jurisdiction_id: '' })).to match_array([application1, application2, application3])
+      end
+    end
+
+    context 'with nil filter' do
       it 'returns all applications without filtering' do
-        expect(query.find(false, false, nil)).to eq([application2, application3, application1])
+        expect(query.find(false, false, nil)).to match_array([application1, application2, application3])
       end
     end
   end
