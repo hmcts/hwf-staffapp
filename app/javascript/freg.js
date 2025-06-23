a2805a13ff144fb464d4d5d381937ec9eacc3dda
Petr Zaparka <petr@zaparka.cz>
2025-06-23 16:23:02 +0100

    more filters

--- app/javascript/freg.js
+++ app/javascript/freg.js
@@ -4,6 +4,7 @@ window.moj.Modules.JsonSearcherModule = (function() {
   // Make codes accessible to both init and findMatches
   let codes = [];
 
+  // Ideally search for fee (amount), jurisdictions, service type and event type.
   return {
     init: function() {
       console.log('JsonSearcherModule initialized');
@@ -11,8 +12,9 @@ window.moj.Modules.JsonSearcherModule = (function() {
     },
     findMatches: function (term) {
       return codes.filter(item =>
-        (item.code && item.code.toLowerCase().includes(term.toLowerCase())) ||
-        (item.jurisdiction2 && item.jurisdiction2.name && item.jurisdiction2.name.toLowerCase().includes(term.toLowerCase()))
+      (item.code && item.code.toLowerCase().includes(term.toLowerCase())) ||
+      (item.jurisdiction2 && item.jurisdiction2.name && item.jurisdiction2.name.toLowerCase().includes(term.toLowerCase())) ||
+      (item.current_version && item.current_version.flat_amount && typeof item.current_version.flat_amount.amount === 'number' && item.current_version.flat_amount.amount.toString().includes(term))
       );
     }
   };
