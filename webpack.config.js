edec1ba51b6b3bfb252e5c3f5b2b2dd07b2b9ef6
Petr Zaparka <petr@zaparka.cz>
2025-06-24 10:55:49 +0100

    Frontend reload fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,8 @@ module.exports = {
     application: "./app/javascript/application.js"
   },
   output: {
-    filename: "bundle-3.js",
-    sourceMapFilename: "bundle-3.map",
+    filename: "bundle-4.js",
+    sourceMapFilename: "bundle-4.map",
     chunkFormat: "module",
     path: path.resolve(__dirname, "app/assets/builds"),
   },
