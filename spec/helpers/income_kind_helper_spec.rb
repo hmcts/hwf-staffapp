baebce70e34d1e984a3887f822b1e9d89c04680e
kalvinlyu <kalvin.lyu@solirius.com>
2025-10-16 14:49:11 +0100

    RST-6779 test

--- spec/helpers/income_kind_helper_spec.rb
+++ spec/helpers/income_kind_helper_spec.rb
@@ -3,14 +3,10 @@ require 'rails_helper'
 RSpec.describe IncomeKindHelper do
 
   describe '#kind_checked' do
-    subject(:kind_checked) { helper.kind_checked(application, form, claimant, kind) }
-
-    before do
-      allow(form).to receive(:i18n_scope).and_return(:'activemodel.attributes.forms/application/income_kind_applicant')
-    end
+    subject(:kind_checked) { helper.kind_checked(application, claimant, kind) }
 
     context 'when application has kind' do
-      let(:application) { build_stubbed(:application, income_kind: { applicant: ['Wages before tax and National Insurance are taken off'] }) }
+      let(:application) { build_stubbed(:application, income_kind: { applicant: ['wage'] }) }
       let(:form) { Forms::Application::IncomeKindApplicant }
       let(:claimant) { :applicant }
       let(:kind) { :wage }
