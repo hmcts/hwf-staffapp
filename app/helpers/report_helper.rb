e19680b6d49084559c43984272e2c2ffc549f0ef
Petr Zaparka <petr@zaparka.cz>
2025-12-10 14:06:35 +0000

    RST-7766 removing income claims export

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -1,9 +1,4 @@
 module ReportHelper
-  def income_claims_courts
-    codes = Views::Reports::IncomeClaimsDataExport::ENTITY_CODES
-    Office.where(entity_code: codes).order(name: 'ASC')
-  end
-
   def ocmc_courts
     Office.sorted.non_digital
   end
