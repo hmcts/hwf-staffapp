5a8c2c7399ca732967f467778c2dcea116b20387
Petr Zaparka <petr@zaparka.cz>
2026-03-02 15:06:14 +0000

    Storing fee valid from

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_02_141700) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_02_145057) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pg_trgm"
@@ -231,6 +231,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_02_141700) do
     t.string "fee_code"
     t.string "fee_manager_firstname"
     t.string "fee_manager_lastname"
+    t.date "fee_version_valid_from"
     t.string "form_name"
     t.integer "jurisdiction_id"
     t.boolean "probate"
