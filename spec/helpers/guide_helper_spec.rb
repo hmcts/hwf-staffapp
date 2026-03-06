24e06fe9b1cd1462a1aad141828588824e90816d
Petr Zaparka <petr@zaparka.cz>
2026-03-06 13:39:24 +0000

    Updating guide links

--- spec/helpers/guide_helper_spec.rb
+++ spec/helpers/guide_helper_spec.rb
@@ -3,19 +3,19 @@ require 'rails_helper'
 RSpec.describe GuideHelper do
 
   describe '#how_to_url' do
-    it { expect(helper.how_to_url).to include('https://intranet.justice.gov.uk/documents/2017/10/help-with-fees-how-to-guide.pdf') }
+    it { expect(helper.how_to_url).to include('sourcedoc=%7B5D999F01-6E39-4EB6-8703-E3E4A0262580%7D') }
   end
 
   describe '#training_course_url' do
-    it { expect(helper.training_course_url).to include('https://mydevelopment.org.uk/course/view.php?id=9824') }
+    it { expect(helper.training_course_url).to include('sourcedoc=%7BB3128FFC-3EF6-4648-B2BB-8A3D8A29E9BA%7D') }
   end
 
   describe '#key_control_checks_url' do
-    it { expect(helper.key_control_checks_url).to include('https://intranet.justice.gov.uk/documents/2017/02/help-with-fees-kccs.docx') }
+    it { expect(helper.key_control_checks_url).to include('sourcedoc=%7BF13EB074-F2F7-4349-BC0A-BA191503BBE9%7D') }
   end
 
   describe '#staff_guidance_url' do
-    it { expect(helper.staff_guidance_url).to include('https://intranet.justice.gov.uk/documents/2023/12/help-with-fees-policy-guide-for-applications-post-27th-november-23.pdf/') }
+    it { expect(helper.staff_guidance_url).to include('sourcedoc=%7BDD4BE471-9B43-43D4-9E71-0FB28A9B41EE%7D') }
   end
 
   describe '#old_process_application_url' do
@@ -23,11 +23,11 @@ RSpec.describe GuideHelper do
   end
 
   describe '#new_process_application_url' do
-    it { expect(helper.new_process_application_url).to include('https://intranet.justice.gov.uk/documents/2024/11/process-a-paper-help-with-fees-application.pdf/') }
+    it { expect(helper.new_process_application_url).to include('sourcedoc=%7BB62AF5DB-DF50-4415-A261-A4598E61B298%7D') }
   end
 
   describe '#new_online_process_application_url' do
-    it { expect(helper.new_online_process_application_url).to include('https://intranet.justice.gov.uk/documents/2024/11/processing-an-online-help-with-fees-application.pdf/') }
+    it { expect(helper.new_online_process_application_url).to include('sourcedoc=%7B1ADE6338-5A41-4D11-8047-ACBB1A070C19%7D') }
   end
 
   describe '#old_evidence_checks_url' do
@@ -35,26 +35,26 @@ RSpec.describe GuideHelper do
   end
 
   describe '#new_evidence_checks_url' do
-    it { expect(helper.new_evidence_checks_url).to include('https://intranet.justice.gov.uk/documents/2024/11/process-a-help-with-fees-evidence.pdf/') }
+    it { expect(helper.new_evidence_checks_url).to include('sourcedoc=%7B40446293-A8A9-4003-B09E-F228F727A441%7D') }
   end
 
   describe '#part_payment_url' do
-    it { expect(helper.part_payment_url).to include('https://intranet.justice.gov.uk/documents/2024/11/process-a-help-with-fees-part-payment.pdf/') }
+    it { expect(helper.part_payment_url).to include('sourcedoc=%7BA5D5C042-C211-45A6-B1C5-22275AC6E0C5%7D') }
   end
 
   describe '#fraud_awareness_url' do
-    it { expect(helper.fraud_awareness_url).to include('https://intranet.justice.gov.uk/documents/2018/05/help-with-fees-fraud-awareness-pdf.pdf') }
+    it { expect(helper.fraud_awareness_url).to include('sourcedoc=%7B03E83158-CD55-45F0-9D99-FC7B02BF2343%7D') }
   end
 
   describe '#rrds_url' do
-    it { expect(helper.rrds_url).to include('https://www.gov.uk/government/publications/record-retention-and-disposition-schedules') }
+    it { expect(helper.rrds_url).to include('sourcedoc=%7B34D2BF32-AEB1-41D7-BF74-9C76A6E8042B%7D') }
   end
 
   describe '#datashare_url' do
-    it { expect(helper.datashare_url).to include('https://intranet.justice.gov.uk/my-work/help-with-fees/help-with-fees-guidance-documents/') }
+    it { expect(helper.datashare_url).to include('sourcedoc=%7BD130FF65-E19C-4A4D-9F02-5E2FF579B229%7D') }
   end
 
   describe '#faq_url' do
-    it { expect(helper.faq_url).to eq('https://intranet.justice.gov.uk/documents/2025/11/help-with-fees-frequently-asked-questions.pdf') }
+    it { expect(helper.faq_url).to include('HWF%5FFAQ%20%5FOriginal%5FVersion1%2E0%5F28%2E10%2E2025%2Epdf') }
   end
 end
