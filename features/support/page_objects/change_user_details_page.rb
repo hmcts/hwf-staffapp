ea66002e76d61a9f0690cfca7862ace9c335f3de
Jack Burke <jack.burke@solirius.com>
2026-07-09 10:47:25 +0100

    Fix selector for cross-browser tests

--- features/support/page_objects/change_user_details_page.rb
+++ features/support/page_objects/change_user_details_page.rb
@@ -4,7 +4,7 @@ class ChangeUserDetailsPage < BasePage
   section :content, '#content' do
     element :header, 'h1', text: 'Change details'
     elements :radio, '.govuk-radios__item'
-    element :reader_radio, '.govuk-radios__item', text: 'Reader'
+    element :reader_radio, '#user_role_reader', visible: false
     element :role, 'p', text: 'Manager'
     element :county_radio, '.govuk-radios__item', text: 'County'
     element :save_changes_button, 'input[value="Save changes"]'
