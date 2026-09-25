b7513701545978dd84032cc440ede75b8cc3b870
Petr Zaparka <petr@zaparka.cz>
2026-09-25 14:09:37 +0100

    Missed the other summary pages for postcode.

--- spec/features/online_applications/online_application_postcode_spec.rb
+++ spec/features/online_applications/online_application_postcode_spec.rb
@@ -34,4 +34,23 @@ RSpec.feature 'Online application check details - UK postcode' do
       expect(page).to have_css('.govuk-summary-list__value', text: 'SW1H 9AJ')
     end
   end
+
+  context 'after completing processing' do
+    let(:scheme) { nil }
+    let(:online_application) do
+      create(:online_application, :completed, :with_reference, :income,
+             income: 600, postcode: 'SW1H 9AJ', jurisdiction: jurisdiction)
+    end
+
+    scenario 'the processed page shows the postcode copied to the applicant' do
+      click_button 'Complete processing'
+      application = Application.find_by(online_application: online_application)
+
+      expect(application.applicant.postcode).to eq 'SW1H 9AJ'
+
+      visit processed_application_path(application)
+      expect(page).to have_css('.govuk-summary-list__key', text: 'UK postcode')
+      expect(page).to have_css('.govuk-summary-list__value', text: 'SW1H 9AJ')
+    end
+  end
 end
